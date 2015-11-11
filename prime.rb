5798cd3a53738ffab8c2a0e4d1452232e0e74cde
onyoo <roberto9m9@gmail.com>
2015-11-11 19:09:31 +0100

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+require 'pry'
+
+def prime?(int)
+  x = (2..int/2).to_a
+  puts "#{x}"
+  if int == 0 || int == 1
+    false
+  else 
+    x.none?{|num| int%num == 0}
+  end
+end

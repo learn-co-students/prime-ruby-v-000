d9854172a1109d05acf113da174ceaf96ca992b0
Tracy <tracetrace@gmail.com>
2015-11-23 22:36:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+
+def prime?(integer)
+  test_divisors = (2..(integer - 1)).to_a
+  if test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1 
+    false
+  else
+    true
+  end
+end

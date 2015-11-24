c346c2afa9e543d115402ef6f194711349987e1d
authorbeard <msnolan@gmail.com>
2015-11-24 17:25:43 -0500

    better version of crappy trial division method

--- prime.rb
+++ prime.rb
@@ -1,12 +1,16 @@
 require 'pry'
 
-def prime?(test_number)
-  test_array = (2...test_number).to_a
-  unless test_number > 1 && test_array.none?{|num| test_number % num == 0}
-    return false
-  else
-    return true
-  end
-end
+# def prime?(test_number)
+#   test_array = (2...test_number).to_a
+#   unless test_number > 1 && test_array.none?{|num| test_number % num == 0}
+#     return false
+#   else
+#     return true
+#   end
+# end
 
-#needs to return a boolean (true for prime)
+
+def prime?(num)
+  test_array = (2..num).select{|x| x*x <= num}
+  (num > 1 && test_array.none?{|z| num % z == 0}) ? true : false   
+end

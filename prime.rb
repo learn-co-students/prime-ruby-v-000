f040ef0864df8346748c070b72b83d5a5b17e9b6
authorbeard <msnolan@gmail.com>
2015-11-24 15:39:02 -0500

    All tests passing

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'pry'
+
+def prime?(test_number)
+  test_array = (2...test_number).to_a
+  unless test_number > 1 && test_array.none?{|num| test_number % num == 0}
+    return false
+  else
+    return true
+  end
+end
+
+#needs to return a boolean (true for prime)

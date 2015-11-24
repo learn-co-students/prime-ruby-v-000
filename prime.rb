12954d23ed4d61521448bd292293738ba1e40d3c
Tracy <tracetrace@gmail.com>
2015-11-24 18:18:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,9 +2,5 @@
 
 def prime?(integer)
   test_divisors = (2..(integer - 1)).to_a
-  if test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1 
-    false
-  else
-    true
-  end
+  (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1) ? false : true
 end

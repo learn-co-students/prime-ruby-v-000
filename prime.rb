e6455529fe857d92236132a2e316d42f4f828632
ed-karabinus <ejkarabin@gmail.com>
2015-12-01 17:07:46 -0600

    Finished Prime? Lab; added reasonably efficient implementation of #prime to prime.rb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer)
+  if greater_than_one?(integer) && prime_candidate?(integer)
+    (2...integer).all?{|number| integer % number != 0}
+  else
+    false
+  end
+end
+
+def greater_than_one?(integer)
+  integer > 1
+end
+
+def prime_candidate?(integer)
+  integer == 2 || integer == 3 || integer % 6 == 1 || integer % 6 == 5
+end

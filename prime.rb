205e60bd136ef4b61b53161f48303d52f7accbd1
yoshikot <ytamaoki@gmail.com>
2015-12-02 13:17:40 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,4 @@
 def prime?(number)
-  if number > 1
-    testers = (2..(number**0.5).round).to_a
-    testers.none? {|tester| number % tester == 0}
-  else
-    false
-  end
+  testers = (2..(number**0.5).round).to_a
+  number > 1 ? testers.none? {|tester| number % tester == 0} : false
 end

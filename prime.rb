d2b413ca6118e2e7be376bbc46022085624eff2c
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-06 01:11:07 +0000

    uses benchmark to test different solutions to the prime?(num) method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,52 @@
-# Add  code here!
+require 'benchmark'
+
+def prime?(num)
+  if num <= 1
+    false
+  elsif num % 1 == 0 
+    (2..(num/2).floor).to_a.none? do |factor|
+        num % factor == 0
+    end
+  else
+    puts "#{num} is not an integer."
+    false
+  end
+end
+
+def prime_with_any?(num)
+  if num <= 1
+    false
+  elsif num % 1 == 0 
+    divisible = (2..(num/2).floor).to_a.any? do |factor|
+        num % factor == 0
+    end
+    divisible ? false : true
+  else
+    puts "#{num} is not an integer."
+    false
+  end
+end
+
+#much faster!!!
+def prime_with_while?(num)
+  if num <= 1
+    false
+  elsif num % 1 == 0
+    i = 2
+    while i < num/2 
+      if num % i == 0
+        return false
+      end
+      i += 1
+    end
+  else
+    puts "#{num} is not an integer."
+  end
+end
+
+Benchmark.bm do |bm|
+  bm.report {prime?(9999999)}
+  bm.report {prime_with_any?(9999999)}
+  bm.report {prime_with_while?(9999999)}
+end
+  

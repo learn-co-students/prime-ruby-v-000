fbbaa8495bf7e678ad82b01e996d2851863e9b6a
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2015-12-15 17:48:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  number=num
+  array_number=Array(1..num)
+  divisible_tester=[]
+until number==0
+if num%number==0
+  divisible_tester << number
+end
+number-=1
+end
+if divisible_tester[0]==num && divisible_tester[1]==1
+  return true
+else
+  return false
+end
+end

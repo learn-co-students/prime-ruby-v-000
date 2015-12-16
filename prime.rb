870e67e0ce7c4c731d7bdf1e4509be15e0a38f95
lizdenhup <lizdenhup@gmail.com>
2015-12-15 21:40:16 -0600

    prime? method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+ if number == 0 || number == 1
+  return false 
+elsif number == 3
+  return true 
+ elsif (2...number).any? {|i| number % i == 0 }
+  return false
+ else 
+  return true 
+  end 
+end
+
+
+#a number is prime if it is divisible by one and itself
+#only 

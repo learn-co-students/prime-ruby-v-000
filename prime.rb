6ef6cb05e18f568c89bbb34d9a20d140eddc1d4f
eric-an <erican@me.com>
2015-12-17 11:11:12 -0800

    define method to find prime number without math module or prime class

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
+require 'pry'
 # Add  code here!
+def prime?(number)
+  prime = true
+  if number == 0 || number == 1
+    prime = false
+  else
+    (2...number).to_a.collect do |num| 
+      if number % num == 0
+        prime = false
+      end
+    end
+  end
+  prime
+end  

b2eb956c4bd7acf464d648d33aeb125899748f63
Jason McElrath <jmcelrath84@gmail.com>
2015-12-22 13:47:16 -0500

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -9,3 +9,21 @@ def prime?(n)
     true
   end
 end
+
+
+# def prime? (n)
+#   if n <=1
+#     false
+#   elsif n <= 3
+#     true
+#   elsif n % 2 == 0 || n % 3 == 0
+#     false
+#   end
+#   i = 5
+#   while i*i <= n
+#     if n % i == 0 || n % (i+2) == 0
+#       false
+#       i = i + 6
+#     end
+#   end
+# end

137bbeeb44242d2dc08aeeb2a21ad103038f61d5
rluftw <luxh93@gmail.com>
2015-12-23 16:31:24 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(number)
+	if number < 2 
+		false
+	else        
+		multiples = []
+
+		Math.sqrt(number).floor.downto(1) do |num| 
+			if number % num == 0 
+				multiples << num 
+			end 
+		end 
+		multiples.length == 1
+	end 
+end
+

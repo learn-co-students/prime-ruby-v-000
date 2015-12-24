40f6657835ef00352e75d0e0565630db880d06aa
empireofryan <rjohnson2001@gmail.com>
2015-12-24 13:04:55 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(num)
+  if num == 0 || num == 1
+    return false
+  elsif (2..(num-1)).any? do |n|
+    (num % n) == 0 
+    end
+      return false
+  else
+    return true
+  end
+end

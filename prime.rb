830dc64e07a0e27161977194bcf4dd936d3e4975
Brad1974 <danieltbrad@gmail.com>
2015-12-26 15:00:50 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,12 @@
+require 'benchmark'
+require 'bigdecimal/math'
+
 def prime?(n)
 range = [*1..n]
 multiples = range.select {|x| n % x == 0}
 multiples.length == 2 ? true : false
 end
+
+prime?(2000)
+
+puts Benchmark.measure { BigMath.PI(10_000) }

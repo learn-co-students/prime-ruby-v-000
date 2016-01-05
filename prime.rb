2287b3180522a4882b91f39b69e3356cd509c91a
adamgfraser <adam.fraser@gmail.com>
2016-01-04 17:22:52 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+
+require 'benchmark'
+
+def prime?(number)
+  return false if number == 0 || number == 1
+  potential_divisors = (2..(number ** 0.5).floor).to_a
+  prime = true
+  while potential_divisors.empty? == false
+    current_divisor = potential_divisors.first
+    if number % current_divisor == 0
+      prime = false
+      break
+    else
+      potential_divisors.select! do |potential_divisor|
+        potential_divisor % current_divisor != 0
+      end
+    end
+  end
+  prime
+end

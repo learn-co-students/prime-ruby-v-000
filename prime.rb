82211a0ccf35d8ed7af2017c8db488e3da219d26
johnram528 <johnram528@gmail.com>
2016-01-06 23:02:56 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  elsif number <= 3
+    true
+  elsif number % 2 == 0 || number % 3 == 0
+    false
+  else
+    true
+  end
+end
+    

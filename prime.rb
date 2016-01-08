75fd835254ce3ec902e65d566a7e37a7b26915dc
Chen Yang <yangc5@mail.uc.edu>
2016-01-08 22:25:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(int)
+  if int<=1 || int==4
+    return false
+  elsif int == 2 || int == 3 || int == 5
+    return true
+  else
+    array = (2..int/2).to_a
+    !divisable(array, int)
+  end
+end
+
+def divisable(array, int)
+    array.any? do |num|
+      int%num == 0
+    end
+end

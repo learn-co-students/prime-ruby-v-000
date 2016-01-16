e553421cac39043fd91e58ccbe1b0431ab39c610
simque <simon.duncanson@gmail.com>
2016-01-15 21:09:52 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
 # Add  code here!
+def prime?(num)
+  divisors = []
+  (1..num).to_a.each do |divisor|
+    divisors << divisor if num % divisor == 0
+  end
+  divisors.length == 2 ? true : false
+end

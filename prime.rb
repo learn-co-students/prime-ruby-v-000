5afcec23e8a140314ca750d0dfcaa2d13f85ec73
michaelsimon <michael.simon@gmail.com>
2016-01-16 14:41:34 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+require 'benchmark'
+def prime?(number)
+  return false if number == 0 || number == 1
+  (2..(number-1)).to_a.each do |checknum|
+    return false if (number % checknum) == 0 || (number % 2) == 0    
+  end
+  true
+end
+

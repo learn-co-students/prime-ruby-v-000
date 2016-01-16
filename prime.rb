0ffeb7b9bd7cb03bff513a4b83baf177cc1c9660
michaelsimon <michael.simon@gmail.com>
2016-01-16 14:43:47 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(number)
-  return false if number == 0 || number == 1
+  return false if number < 2
   array = (2..(number-1)).to_a
   array.each do |checknum|
     return false if (number % checknum) == 0 || (number % 2) == 0    
   end
-  true
+  return true
 end
 

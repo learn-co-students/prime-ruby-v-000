c1f7c1bb4b634ab87e258b02e692cc00aec4dcf7
simque <simon.duncanson@gmail.com>
2016-01-15 21:36:53 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -3,6 +3,7 @@ def prime?(num)
   divisors = []
   (1..num).to_a.each do |divisor|
     divisors << divisor if num % divisor == 0
+    break if divisors.length > 2
   end
   divisors.length == 2 ? true : false
 end

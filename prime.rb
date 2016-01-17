1dc5f8a61b8f08d8dc0ce577a1e122c1ec67fb8e
thegands <thegands@gmail.com>
2016-01-17 01:10:40 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,19 +1,27 @@
 # Add  code here!
+require 'benchmark'
 def prime?(number)
   if number < 2
     false
   else
-    possible_factors = (2..Math.sqrt(number)).to_a
+    possible_factors = (2..Math.sqrt(number).floor).to_a
     !possible_factors.any? { |e| number % e == 0 }
   end
-  # return false if number < 2
-  # prime = true
-  # 2.upto(Math.sqrt(number).floor) do |i|
-  #   if number % i == 0
-  #     prime = false
-  #   end
-  # end
-  # prime
+end
+
+def primed?(number)
+  return false if number < 2
+  prime = true
+  2.upto(Math.sqrt(number).floor) do |i|
+    if number % i == 0
+      prime = false
+    end
+  end
+  prime
 end
 
 # (0..50).to_a.each_with_index { |number, i| puts "#{i} is prime? #{prime?(number)}" }
+# (0..50).to_a.each_with_index { |number, i| puts "#{i} is prime? #{primed?(number)}" }
+
+# puts Benchmark.measure{(0..50000).to_a.each_with_index { |number, i| prime?(number) }}
+# puts Benchmark.measure{(0..50000).to_a.each_with_index { |number, i| primed?(number) }}

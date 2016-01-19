25bd6a96c3f2ef6ae053178f121d5714a44dcb82
lcorr8 <l.corr.v@gmail.com>
2016-01-19 16:32:11 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+ 
+def prime?(integer)
+
+  return false if integer < 2
+
+  (2...integer).each do |x|
+    return false if integer % x == 0
+  end  
+
+  return true
+
+end  
+
+         
+    

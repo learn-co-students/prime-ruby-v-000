6f875e216e5a6292688788e358212dd89c004e70
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-01-20 22:37:32 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num == 0 || num == 1
+    return false
+  end
+  range = (2..num-1).to_a
+  result = false
+  range.each_with_index do |element, index| 
+
+    if (num % element) == 0 || num < 1
+      return result = false 
+    end
+  end
+      return result = true
+end
+
+

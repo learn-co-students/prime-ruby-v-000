1c322481445dd038dff408a4fbb8cd6eba4f1bdf
mhnguyen289 <mhnguyen289@gmail.com>
2016-01-21 17:10:52 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  array = (2..num-1).to_a
+
+  if num == 0 || num == 1
+    return false
+  elsif
+    array.each { |element|
+      if num % element == 0
+        return false
+      end
+    }
+  end
+  return true
+end
+
+puts prime?(0)

c452c04f8ea31329c8dc759aee03539a0cf9bc99
Tom Johns <tommyskies@hotmail.com>
2016-01-25 14:58:42 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
-# Add  code here!
+require'benchmark'
+
+#my no Math lib solution
+def prime?(n)
+	return true if n == 2 || n == 3
+	return false if n <= 1 || n % 2 == 0 || n % 3 == 0 
+	
+	!(2..n-1).to_a.any? {|x| n % x == 0}
+	
+end
+
+# range up to square root
+def prime2?(n)
+	return true if n == 2 || n == 3
+	return false if n <= 1 || n % 2 == 0 || n % 3 == 0 
+	
+	!2.upto(Math.sqrt(n).to_i).to_a.any? {|x| n % x == 0}
+	
+end
+
+#tests
+#http://www.bigprimes.net/archive/prime/100001/
+testPrime = 15487469
+puts Benchmark.measure{prime?(testPrime)}
+puts Benchmark.measure{prime2?(testPrime)}
+
+#notes
+#using .find instead of any? was slower
+#look into AKS implementation

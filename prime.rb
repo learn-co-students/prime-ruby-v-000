fe5098509fb6dc3972e7ac152e2ae2f7db0f6bb1
lamass <hadzy@yahoo.com>
2016-01-25 02:53:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,22 @@
 
 
-def prime?(number)
-  (2...number).each do |x|
-    return false if number % x == 0
-  end
-  true
-end
+# def prime?(number)
+#   (2...number).each do |x|
+#     return false if number % x == 0
+#   end
+#   true
+# end
 
+def prime?(number)
+  if number == 0 or number == 1
+    return false
+  end
+  i = 2
+  while i < number
+    if number % i == 0
+      return false
+    end
+  i += 1
+  end
+  return true
+end

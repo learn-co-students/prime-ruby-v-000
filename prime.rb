21c6ac3a5cd4e0f38192500d33041319dbc4768e
zachnewburgh <zach.newburgh@gmail.com>
2016-01-26 10:04:46 -0500

    created method to determine if number is prime, return true/false

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(num)
+  if (num > 2 && num % 2 == 0) || (num > 3 && num % 3 == 0) || (num > 5 && num % 5 == 0)
+    return false
+  elsif num == 0 || num == 1
+    return false
+  else
+    return true
+  end
+end

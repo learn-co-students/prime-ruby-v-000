3dab2b24ca071a01adf17bd7ab2ec0a2573ab846
jjlippman <jjlippman@earthlink.net>
2016-02-02 13:33:26 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,39 @@
-# Add  code here!
+def prime?(num)
+  num_range = (2..num)
+  
+  num_array=num_range.to_a
+  arr=[]
+  
+  if num==1 || num == 0
+    return false
+  end
+  
+  num_array.each do |n|
+     if num != n
+        arr << (num % n == 0)
+      end
+  end
+   
+   arr.all? do |i|
+    i==false
+
+   end
+ end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

53fa137c5f173176aa5874656a8e1a52a3c74b09
thelucasmoore <lucasedwardmoore@gmail.com>
2016-02-10 18:15:18 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+
+	if integer == 1 || integer == 0
+		return false
+	end
+
+	range = (2..integer-1).to_a
+	range.each do |test|
+		if integer % test == 0
+			return false
+		end
+	end
+	return true
+end

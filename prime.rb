b22c183e0dc975b0e6f54bc5e414d534b4b4d870
mcglaser <michaelcharlesglaser@gmail.com>
2016-02-11 14:37:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  elsif number % 2 == 0 || number % 3 == 0
+    return false
+  else
+    true
+  end
+end

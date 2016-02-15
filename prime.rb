3f63dc0dd28158013a173a148e204b8d12cc73d2
ShiLi007 <omgshi888@gmail.com>
2016-02-15 15:40:26 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+# Add  code here!
+def prime?(num)
+  i=2
+  while i<=num
+    
+  if num % i ==0
+    return false
+  else 
+   return true
+   i+=1
+        end 
+    end
+  end
+  
+  

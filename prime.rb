43d2c31d34c79e35a7dde040e511f073495423d9
ShiLi007 <omgshi888@gmail.com>
2016-02-15 16:09:35 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -2,16 +2,9 @@
 
 # Add  code here!
 def prime?(num)
-  i=2
-  while i<=num
-    
-  if num % i ==0
-    return false
-  else 
-   return true
-   i+=1
-        end 
-    end
+  return false if num <=1
+  (2...num).each do |divisor|
+    return false if num % divisor == 0
   end
-  
-  
+  true
+end

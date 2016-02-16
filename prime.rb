8551fe24fd38b60c06dc70470e921ad6f854244e
swdavies7 <swdavies7@gmail.com>
2016-02-16 03:16:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(integer)
+  if integer == 0
+    return false
+  elsif integer == 1
+    return false
+  elsif integer == 2
+    return true
+  elsif (2...integer).any? {|num| integer % num == 0}
+      return false
+  else 
+      return true
+  end
+end

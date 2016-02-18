18e4aa58284d104bb73c587899d03ea2e339ca30
jsbenning <jbenninghoff1970@yahoo.com>
2016-02-18 18:30:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+
+def prime?(int)
+  numb = 2
+  ans = true
+  if int < 2
+    ans = false
+  end
+  while numb < int
+    if int%numb == 0
+     ans = false
+      break
+    elsif int%numb != 0
+      numb += 1
+    end
+  end
+  return ans
+end
+
+
+

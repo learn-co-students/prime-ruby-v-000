5a57f110b8ff1a41bec82b63fcaf11d4a555983d
cernanb <cernanb@gmail.com>
2016-02-22 23:04:00 -0800

    updated prime? method to look for prime numbers

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  prime = true
+  if num < 2
+    return false
+  else
+    i = num - 1
+    while i > 1
+      if num % i == 0
+        return false
+      end
+      i -= 1
+    end
+  end
+  prime
+end
+
+prime?(4)

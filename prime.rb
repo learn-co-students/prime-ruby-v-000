5cf0bc9184e245ff1121c07314c144b4d77dad58
perritanom <perritanom@gmail.com>
2016-02-25 13:19:03 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(integer)
+  counter = 0
+  array = (1..integer).to_a
+  array.each {|num| if integer % num == 0 then counter += 1 end}
+  
+  if counter == 2 
+    return true
+  end
+
+  false
+  
+end

c2cf2fbb323aa42f07fba003fd7d5740be0efb85
bradleyoashburn <bradleyoashburn@gmail.com>
2016-03-04 13:51:04 -1000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  i = 2
+  return false if num == 0
+  return false if num == 1
+  while i <= Math.sqrt(num)
+    return false if num%i==0
+    i += 1
+  end
+
+  true
+end

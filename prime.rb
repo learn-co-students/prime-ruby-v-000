ebd875be5c8a303a18976cdbee6876e8b0ae642d
gnfisher <me@gnfisher.com>
2016-03-04 12:29:52 -0300

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+require 'benchmark'
+
+def prime?(num)
+  # prime numbers up to 110,000 detected
+  return false if (0..1).include? num
+
+  a=*(2..110000)
+  a.delete(num)
+  !a.any? { |i| num % i == 0 }
+end

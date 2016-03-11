025797197098f9a528f607ad6c51c10d058c95a9
Robert Koeze <rkoeze@gmail.com>
2016-03-11 14:40:23 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+
+  (2...num).to_a.each do |factor|
+    if num % factor == 0
+      return false
+    end
+  end
+  true
+end

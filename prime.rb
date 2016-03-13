7f6b0310a6e14b93bde225575eacbf2119d08160
RMcNeely <rmcneely23@gmail.com>
2016-03-13 02:54:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'pry'
+def prime?(number)
+  prime_array = []
+  (1..number.to_i).each do |n|
+    if number % n == 0
+      prime_array.push(n)
+    end
+  end
+    prime_array.delete_if {|n| n == 1 }
+    if prime_array.length == 1
+      true
+    else
+      false
+  end
+end

25e18f359d562b37ff42de237c14700f77d3941f
PeteAJ <peter.jacobson@gmail.com>
2016-03-15 14:18:04 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(n)
+  return self == 2 if self % 2 == 0
+
+    (3..Math.sqrt(self)).step(2) do |x|
+      return false if self % x == 0
+    end
+
+    true
+  end
+    end

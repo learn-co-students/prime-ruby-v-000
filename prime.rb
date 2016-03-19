a667bee49db1db916f28fc328a44e3ac5bcba9c1
nicolefederici <nicolefederici@yahoo.com>
2016-03-19 17:13:55 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
+def prime?(n)
+    if n == 1 || n == 0
+        return false
+    else
+        clean_div = (2..n-1).find {|i| n % i == 0}
+        if clean_div == nil
+            return true
+        else
+            return false
+        end        
+    end
+end
+
+
 # Add  code here!

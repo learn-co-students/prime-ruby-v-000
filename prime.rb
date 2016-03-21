e7362a1335ac97b8073380450aeaf339c3e29c4a
viparthasarathy <viparthasarathy@gmail.com>
2016-03-20 22:45:04 -0700

    refactor

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
+
 def prime?(n)
   return false if n <= 1
-  Math.sqrt(n).to_i.downto(2).each {|i| return false if n % i == 0}
+  (2..Math.sqrt(n)).each {|i| return false if n % i == 0}
   true
 end

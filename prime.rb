fc8d8215958bf52ecdbdfd4b0a534fb37a86cd7d
eliyooy <ebshimon@gmail.com>
2016-03-22 22:43:16 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  elsif int == 2 || int == 3 
+    return true
+  elsif int % 2 == 0 || int % 3 == 0
+    return false
+  else
+    return true
+  end
+end

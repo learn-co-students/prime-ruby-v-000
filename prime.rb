301161aa51da874d1b02c6beb8bea48b1227118a
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-03-23 23:30:32 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+# def prime?(integer)
+# a = []
+# (2..integer-1).to_a.collect {|i| a << i if integer%i == 0}
+# if a != [] || integer == 0 || integer == 1
+# 	return false
+# else 
+# 	return true
+# end
+# end
+
+def prime?(integer)
+if !(2..integer-1).to_a.select {|i| integer%i == 0}.empty? || integer <= 1
+	return false
+else 
+	return true
+end
+end

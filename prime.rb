9b2708e0e3d2b95252632ca318b537414373814c
kade042 <dereje88@gmail.com>
2016-03-31 22:48:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+#require 'benchmark'
+def prime?(num)
+  #Benchmark.bm do |bm|
+    #bm.report do
+      if num < 2
+        return false
+      else
+        (2...num).each do |divisor|
+          return false if num % divisor == 0
+        end
+      end
+    #end
+  #end
+  true
+end

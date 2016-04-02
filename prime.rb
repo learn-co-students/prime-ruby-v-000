0b52e82e2fe807955e45fcacef5eb451956dede8
sarond <saron.desta@gmail.com>
2016-04-01 23:52:51 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  end
+
+  i = 2
+  while i < integer
+    if i == integer
+      true
+    elsif integer % i == 0
+      return false
+      break
+    else
+      i += 1
+    end
+  end
+  true
+end
+        
+
+

802722ced77311a33a49794f71ac35f947035ca6
ludaires <ludaires@gmail.com>
2016-04-07 23:17:05 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
+#TODO Look into Benchmarking in ruby and try a different approache.
+
 # Add  code here!
+def prime?(number)
+  counter = 2
+
+  if number % 2 == 0 || number <= 1
+    return false
+  elsif number == 2
+    return true
+  end
+
+  while counter < number
+    return false if number % counter == 0
+    counter += 1
+  end
+  true
+end
+

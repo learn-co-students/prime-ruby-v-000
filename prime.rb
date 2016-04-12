96f27fe00484758d58fcfc3bb955fc2b38720b25
thebrianemory <the@brianemory.com>
2016-04-12 08:31:30 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(number)
+  if number.even? && number > 2
+    return false
+  end
+
+  half_num = number / 2
+  x = 3
+  while x < half_num
+    if number % x == 0 then
+      return false
+    end
+    x += 2
+  end
+
+  return number > 1
+end
+

02eb123799a4e4ed3f0852210e79b305c6eff94e
Saul Ocampo <digitalh2o@gmail.com>
2016-04-15 01:26:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  square = Math.sqrt(num).to_i
+  return false if num <= 1 || num.is_a?(Integer) == false
+  
+  is_prime = true
+  for n in 2..square
+    if num % n == 0
+      is_prime = false
+      break
+    end
+  end
+  is_prime
+end
+      

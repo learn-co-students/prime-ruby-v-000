783647601a5232ddfea4d761435c2ced1394ee56
KentLeong <erickentleongchu@gmail.com>
2016-04-27 14:48:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(num)
+  i = 2
+  array = []
+  while i < num
+    array << i
+    i+=1
+  end
+
+  array.each_with_index.each do |x, index|
+    if num % x == 0
+      return false
+    end
+  end
+
+  if num <= 1
+    return false
+  else return true
+  end
+end

1f8fa7741476865fcf647881407a488f127d5043
EdG570 <cole570@hotmail.com>
2016-04-28 10:15:03 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number == 0 || number == 1
+    return false
+  else
+    range = (2..number - 1).to_a
+  end
+
+  range.all? do |num|
+    number % num != 0
+  end
+end

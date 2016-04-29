3ff99cd9b9bd6a21a67aecbe767324a9a5a23dde
ndaniele <n.daniele@gmail.com>
2016-04-29 18:07:00 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,19 +1,14 @@
-def prime?(num)
-  range = *(2..num/2)
-  return false if num < 1
-  range.each {|m| if m % num == 0 
-      return true
-    else
-      return false
-  end
-}
-  end
+def prime?(num)             #defines method that accepts an argument (num)
+  if num <= 1               # checks to see if num is less than or equal to 1
+    return false            # if so, returns false
+  end                       #end of that if statement
+  counter = 2               # sets a counter to start at 2
+  while counter < num       #while the counter is less than the given argument (num)
+    if num % counter == 0   #check to see if num divded by the current coutner number leaves no remainder
+      return false          #if so, return false
+    end                     #end of if statement
+    counter += 1            # adds 1 to the counter each iteration
+  end                       #end while loop
+  return true               #returns true if line 7 does not return false
+end                         #end of method
   
-#|| squareroot of num
-
-#{|num| if num #divided by m(from 2...to square root of num) with .0 remainder return prime }
-
-  #The simplest primality test is trial division: 
- # Given an input number n, check whether any prime integer m from 2 to √n evenly divides n 
- # (the division leaves no remainder). 
- # If n is divisible by any m then n is composite, otherwise it is prime.[1]

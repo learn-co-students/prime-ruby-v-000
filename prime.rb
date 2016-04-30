153bd4184f7b0abcce5903b995382550928a072d
kaileighrose <kaileighrose@gmail.com>
2016-04-29 21:05:33 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+  testers = (2..110000).to_a
+  result = testers.find do |test|
+    number % test === 0
+    end
+  if result != number
+    false
+  else
+    true
+  end
+end

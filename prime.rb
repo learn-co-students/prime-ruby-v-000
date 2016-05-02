dcdeedc14119e718dfd4a778b35c0d4b7e7ed443
Justin Hall <hall.justin.d@gmail.com>
2016-05-02 17:01:14 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  x = 2
+
+  if num == 2
+    return true
+  elsif num < 2
+    return false
+  end
+
+  while x <= num/2
+    if num % x == 0
+      return false
+    end
+
+    x += 1
+  end
+
+  return true
+end

a9273ad922c1f1bde5eaf9eb708f730eb870b487
Being Young <beingy@gmail.com>
2016-05-04 12:46:20 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(number)
+  if number.odd? && number > 1 || number == 2
+    numbers = (1..number).to_a
+    factors = []
+    numbers.select do |i|
+      factors << i if number % i == 0
+    end
+    if factors.length == 2
+      #puts "#{number} is Prime!"
+      return true
+    else
+      #puts "#{number} is Composite!"
+      return false
+    end
+  else
+    #puts "#{number} is not Prime!"
+    return false
+  end
+end
+
+#prime?(4)

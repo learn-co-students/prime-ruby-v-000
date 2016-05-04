c205a5be42addcc4a26fc4e30e023ef02ebdfc55
Being Young <beingy@gmail.com>
2016-05-04 15:55:50 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,18 +1,15 @@
-def prime?(n)
-  counter = 0
-  if n == 2 || n == 3
-    counter += 1
-    puts "Solved for #{n} in #{counter} steps."
-    true
-  elsif n == 1 || n==0
-    false
-  elsif n % 2 == 0 || n % 3 == 0
-    false
-  elsif (n - 1) % 6 != 0 || (n+1) % 6 != 0
-    counter += 1
-    puts "Solved for #{n} in #{counter} steps."
-    true
-  else
-    false
+require 'pry'
+
+def prime?(number)
+  list_primes = (0..number).to_a
+  list_primes[0] = list_primes[1] = nil
+  list_primes.each do |n|
+    next unless n
+    break if n*n > number
+    n*n.step(number,n) {|i| list_primes[i] = nil}
   end
+  #binding.pry
+  list_primes.compact.include?(number)
 end
+
+#prime?(17)

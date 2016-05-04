b8656cc876ca7dbd2379bde7c141f49df136f8f7
brad72287 <brad72287@gmail.com>
2016-05-04 04:02:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,6 @@
 # Add  code here!
+def prime?(number)
+  return false if number == 0 || number == 1
+  (2..(number - 1)).each {|test_number| return false if number%test_number == 0}
+  return true
+end

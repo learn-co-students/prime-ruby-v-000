d06398aa784aaa194ca9e7074f8c3c5a2c0a70d0
bjacooper <bjacooper@gmail.com>
2016-05-05 21:31:03 +0100

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(integer)
+square_root_of_integer = Math.sqrt(integer).ceil
+range = (0.. square_root_of_integer)
+  range.each do |x|
+    if x == 2 
+      true
+    elsif x == 0
+      false
+    elsif x == 1
+      false
+    elsif x % 2 == 0
+      false
+    else x % 3 == 0
+      false
+    end
+  end
+end
+
+
+
+
+

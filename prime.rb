9c53e9d1e7de72c6cd577ca1307e38d5ae9fbd78
bjacooper <bjacooper@gmail.com>
2016-05-06 17:04:38 +0100

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
+
+
 def prime?(x)
   return false if x < 2
-  2.upto(x - 1) do |n|
-    return false if x % n == 0 
+  max = x - 1
+  (2.. max).each do |n|
+    return false if x % n == 0
   end
   true
 end

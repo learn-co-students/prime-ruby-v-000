758cfcbc6c7fe23995ea9d44c4923089444c23a9
mjdele <mike.delehant@gmail.com>
2016-05-10 13:19:36 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+
+
+def prime?(number)
+  factors = (2..number**0.5).to_a
+  if number <= 1
+    false
+  elsif factors.any? do |i|
+    number%i == 0
+  end
+    false
+  else
+    true
+  end
+end

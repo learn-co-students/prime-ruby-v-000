fa04c36ba91c0daf401326b6f74927512954bdbe
SnowboardTechie <bryan.thompson518@gmail.com>
2016-05-11 14:33:44 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num <= 3
+    return true
+  elsif num % 2 == 0 || num % 3 == 0
+    return false
+  end
+  return true
+end

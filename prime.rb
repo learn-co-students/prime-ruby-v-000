72b0634f01476afc9a91628ced5d7cc094c933ce
Anita Ford <anitamford@gmail.com>
2016-05-12 20:34:55 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    prime = false
+  else prime = true
+    (2..number-1).each do |divide|
+      if number % divide == 0
+        prime = false
+      end
+    end
+  end
+  return prime
+end

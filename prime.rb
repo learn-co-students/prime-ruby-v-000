7ea178d7e0345b3e6629d90078f8bcc3fba1e07e
dannyenfp <daniel.valmas@gmail.com>
2016-05-17 14:18:22 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+
+def prime?(num)
+  if num < 2 
+    return false
+  end
+  a=2
+  until a > (num-1)
+    if num%a == 0 
+    return false
+    else 
+      a=a+1
+  end
+end 
+return true
+end
+
+puts prime?(2)
+puts prime?(3)
+
+puts prime?(5)
+
+puts prime?(4)

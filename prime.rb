13cc97411ab46d078df6cdbce27f5dc3f60743d0
Savannah Scott <sascott624@gmail.com>
2016-05-17 16:01:12 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+# Add  code here!  ONLY LOOKING FOR TRUE FALSE
+
+def prime?(integer)
+  array = (2..integer-1).to_a
+  if integer == 0 || integer == 1
+    false
+  elsif array.all? {|num| integer%num !=0}
+    true
+  else
+    false
+  end
+end

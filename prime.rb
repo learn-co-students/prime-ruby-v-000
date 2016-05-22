010b4d224c104d4e20619aa560a6d2cb899afef0
Jennifer Pazos <jmp521@gmail.com>
2016-05-22 03:27:31 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+	array = []
+	if num == 0 || num == 1
+		return false
+	else
+	  (2..num-1).each do |x|
+	  	return false if num % x == 0
+	   end
+   end
+		   return true
+end

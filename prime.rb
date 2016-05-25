6b0268a7ea8aaa2373cf5e53ccc0246c621808e7
Sandy Hogan <svern99@aol.com>
2016-05-24 20:33:22 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,21 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  else
+  number >= 2 && (2..number/2).none? { |n| number % n == 0 }
+end
+end
+
+
+
+=begin
 def prime?(int)
-  if array = (2..int).to_a
+  if int <= 0
+    false
+  else array = (2..int).to_a
     array.all? {|i| int % i != 0}
     return true
-  else array[] == 0
-    false
   end
 end
+=end

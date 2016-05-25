7c558461c3a3c4dc1ec0ca4828c43a5989a84cbc
Lenn Hypolite <lenntriniman@hotmail.com>
2016-05-25 09:28:51 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  max = integer -1
+  arr = (2..max).to_a
+  arr.any? do |numbers|
+    if integer % numbers == 0
+      return false
+    elsif integer % numbers !=0
+      return true
+    end
+  end
+end

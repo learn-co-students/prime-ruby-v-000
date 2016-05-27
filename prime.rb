12e6301d72b0e8e6a8fc57f75f8fae804757a7cd
ryecox <ryecox@gmail.com>
2016-05-27 11:32:08 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+  #Exempts 0 and 1 from the set of numbers analyzed
+  return false if number < 2
+  #To learn about the Math module, see http://ruby-doc.org/core-2.2.0/Math.html
+  #Finds the square root of each number. If divisible by some other 
+  #number without a remainder, returns false. 
+  (2..Math.sqrt(number)).each do |num|
+    return false if number % num == 0
+  end
+  true
+end
+
+
+
+#Adapted from the previous Learn Student below
+# who had the most elegant prime structure I encountered after much 
+#trial and error
+# https://github.com/mikeappell/prime-ruby-001-prework-web/blob/master/prime.rb

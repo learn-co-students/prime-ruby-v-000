17e2ae253d138bbded8ea03924ab8171b75e15de
Michael Stewart <upp_mike@yahoo.com>
2016-06-01 09:46:22 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(number)
+
+  i = 2
+  if number == 0
+    return false
+  elsif number == 1
+    return false
+  end
+  
+  while i < number
+    if number % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end

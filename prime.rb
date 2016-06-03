0b19c4aeb08f3ede47c4bbfbb04cdea5f7e98880
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-06-03 17:04:37 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(number)
+  check_range = Array(2..(number-1))
+  if number < 2 || check_range.detect {|check_val| number % check_val == 0}
+    return false
+  else
+    return true
+  end
+end

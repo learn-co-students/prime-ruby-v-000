a5158a1831cf3904a667de2b00f00c93d1f3f17a
Perla Yates <pianistps@yahoo.com>
2016-06-10 01:36:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+def prime?(num)#returns bool of whther int is prime num
+  return false if num <=1
+  2.upto(Math.sqrt(num).ceil) do |i|
+        break if num%i==0
+        return true if i==Math.sqrt(num).ceil
+    end
+    return false
+end

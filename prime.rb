5f6d453c07916b91d0ffb98bae542c506c43fb9a
dejesussteven <stevendejesus90@gmail.com>
2016-06-10 20:31:53 +0200

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(num)
+  is_prime = true
+  num.downto(2) do |x|
+    next if x == num
+    if num % x == 0
+      is_prime = false
+    end
+  end
+  if num == 0 || num == 1
+    is_prime = false
+  end
+  is_prime
+end
+
+
+
+

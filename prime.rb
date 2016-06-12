9b35b19669fc41a1763c87751a50bbf1d7e53a62
Keith Wellman <keith.wellman@icloud.com>
2016-06-12 22:23:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(number)
   #first attempt: brute force
   #create an array of numbers up to the given number and check for divisibility
-  last_num = number - 1
-  array = Array(2..last_num)
-  array.any? do |x|
-    number % x != 0
+  return false if number < 2
+  array = Array(2..number - 1)
+  array.none? do |x|
+    number % x == 0
   end
 end

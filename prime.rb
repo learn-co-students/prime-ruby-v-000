88a13b990f5a3c77e681a3cb9486331a148a32d7
andrewmmarquis <andrewmmarquis@gmail.com>
2016-06-14 18:24:32 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,15 @@
 def prime?(i)
-  if i 
+  numbers = (2..i-1).to_a
+  numbers.each do |number|
+    if i % number == 0
+      return false
+    elsif i == 0
+      return false  
+    else 
+      return true
+    end
+  end
 end
+
 
 

abef4fb08324a6ed9f78fe446f3905b01bba4b4d
bging4 <wgingher@gmail.com>
2016-06-14 16:30:43 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+# Prime number test
+def prime?(num)
+  # Create end of range and range of numbers to iterate over
+  test_num = num -1
+  sample_nums = (2..test_num).to_a
+  if num <= 2
+    if num == 2
+      true
+    else
+      false
+    end
+  else
+    !sample_nums.any? do |n|
+      num % n == 0
+    end
+  end
+end

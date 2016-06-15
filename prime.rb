f91fbfc7ea7b6e435eb7dc51edb634def3c20397
natasha carlyon <natasha.carlyon@gmail.com>
2016-06-15 19:31:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def not_prime(number)
+    (2..number).any? {|divisor| number % divisor == 0 && number != divisor}
+end
+
+def prime?(number)
+  
+  return false if number < 2
+
+  if not_prime(number) != true
+    return true
+  else
+    return false
+  end
+end

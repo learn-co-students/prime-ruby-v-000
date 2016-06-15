928ca2ec2105b2f52e369b83c60a62fb079000ca
scottibe <bewick.scott@gmail.com>
2016-06-14 23:04:27 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num == 0 || num == 1
+    return false
+  end
+  
+  (2..(num-1)).each do |primes|
+    if num % primes == 0
+    return false
+    end
+  end
+  return true
+end  
+    
+

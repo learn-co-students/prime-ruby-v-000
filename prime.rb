7bb3c40a0b2e36d04c9b6776a965a7c9abe95205
Jesse Novotny <jessenovotny@gmail.com>
2016-06-15 17:48:47 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime? num
+  test = (1..num).to_a
+  i = []
+  return false if num == 0 || num == 1
+  test.each do |e|
+    i << true if num % e == 0
+  end
+  return false if i.length > 2 else return true
+end

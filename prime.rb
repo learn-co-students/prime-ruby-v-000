a11047cc0043983c9b916b6e3fb74f41c69fd33f
Certain125 <amanita.certain@gmail.com>
2016-06-29 08:04:39 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,17 +1,50 @@
 
-int = [1..200000]
+def sieve(max)
+  # Set up an array with all the numbers from 0 to the max
+  primes = (0..max).to_a
 
-def prime?(int)
-  x = 1
-  until int == 2 || int == 3 || int == (6 * x - 1)
-    x += 1
+  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
+  # aren't prime.
+  primes[0] = primes[1] = nil
+
+  # Iterate through primes array
+  counter = 0
+  primes.each do |p|
+    # Skip if nil
+    next unless p
+
+    # Break if we are past the square root of the max value 
+    break if p*p > max
+    counter += 1
+    # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and replace
+    # that value with nil in the primes array
+    (p*p).step(max,p) { |m| primes[m] = nil }
   end
-  if int == 2 || int == 3 || int == (6 * x - 1)
-    return true
-  else
-    return false
-  end
+
+  # Finally, return the compacted array.
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
 end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
+
+puts prime?(105557)
+#int = [1..200000]
+
+#def prime?(int)
+#  x = 1
+#  until int == 2 || int == 3 || int == (6 * x - 1)
+#    x += 1
+#  end
+#  if int == 2 || int == 3 || int == (6 * x - 1)
+#    return true
+#  else
+#    return false
+#  end
+#end
 
 #######THIS WORKS IN PRY... 
 

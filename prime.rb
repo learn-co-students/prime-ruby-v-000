2588587334c2210892d442c41a4343906b67cb08
Brad Watson <brad.watson.orlando@gmail.com>
2016-07-07 12:29:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(int)
+  is_prime = true
+  range = *(2..int-1)
+  if int > 2
+    range.each do |num|
+      if int % num == 0
+        is_prime = false
+      end
+    end
+  else
+    is_prime = false
+  end
+  is_prime
+end

f24ff621e889aadc9e51c7e7436802da7659857a
Binny Lewis <binnylewis@gmail.com>
2016-07-10 10:55:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  i = 2
+  prime = true
+  while i < int
+    if int % i == 0
+      prime = false
+    end
+    i += 1
+  end
+  if int == 0 || int == 1
+    prime = false
+  end
+  prime
+end

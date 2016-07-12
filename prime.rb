d703265ba6fcbcb3fb91c6c9ef8fb8209e61c89a
vanita eckert <vanita112003@gmail.com>
2016-07-12 15:29:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+     if integer < 2
+    false
+  else
+    counter = 2..integer-1
+    counter.all? do |number|
+      integer % number != 0
+
+end
+end
+end

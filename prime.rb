fd2669e1ef20b77006745e9218def46edc47d13f
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-07-21 11:06:43 -0400

    prime passing

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+	if number <= 1
+		false
+	elsif number <= 3
+		true
+	elsif number % 2 == 0 
+		false
+	elsif number % 3 == 0
+		false
+	else 
+		(5..number).to_a.each do |i|
+			if number % i == 0 && i < number
+				return false
+			else
+				return true			
+			end
+		end
+	end
+end

b773fa6e662274008f114487805fd2f2378f8d18
KerryAlsace <kerrydougherty@gmail.com>
2016-07-26 15:37:33 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(n)
+  divisors = [2, 3, 4, 5, 6, 7, 8, 9]
+  modulos = []
+  divisors.each do |divisor|
+    if divisor != n
+      modulos << n % divisor
+    end
+  end
+  if n == 1
+    false
+  elsif modulos.include?(0)
+    false
+  else
+    true
+  end
+end

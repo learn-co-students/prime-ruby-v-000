dabe98fea969b1ed8d0e8ca2e143dcd205a52152
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-07-26 20:31:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
-# Add  code here!
+def prime?(int)
+  if int == 1
+    return false
+  end
+  if int == 2
+    return true
+  end
+  if int == 3
+    return true
+  end
+  if  int % 2 == 0
+    return false
+  end
+  if  int % 3 == 0
+    return false
+  end
+  i = 5
+  w = 2
+
+  while i * i <= int
+    if int % i == 0
+      return false
+    end
+
+      i += w
+      w = 6 - w
+  end
+  return true
+end

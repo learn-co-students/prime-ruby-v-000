604f55e1663b953d77c122da299ec3798762afce
William Jones <enquiries@forestfed.com>
2016-07-27 15:56:28 +0000

    code method to determine if a number is prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+  prime_numbers = [2, 3, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+  q = 0
+  r = 0
+  if prime_numbers.include?(number)
+    return true
+  elsif number < 2
+    return false
+  elsif number.even?
+    return false
+  else
+    prime_numbers.each do |prime|
+      q, r = number.divmod(prime)
+      if r == 0
+        return false
+      end
+    end
+    return true
+  end
+end

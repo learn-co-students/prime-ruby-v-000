330c3321ade97832c84f40503038f4c465b929f5
Jesse Wenger <wengerj@gmail.com>
2016-08-02 20:11:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+require 'benchmark'
+# require 'pry'
+
+def prime?(integer)
+  range = (1..integer).to_a
+  new_range = []
+  range.each do |num|
+    if integer % num == 0
+      new_range << num
+    end
+  end
+  if new_range.length == 2
+    return true
+  else
+    return false
+  end
+end
+
+# def benchmark
+#   puts Benchmark.measure {prime?(97)}
+# end
+#
+# benchmark

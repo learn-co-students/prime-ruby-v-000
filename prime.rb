1ba7a0e588fdf2b3ef698626f847545af9d51c06
John Verdone <jmverdone@gmail.com>
2016-08-10 18:53:35 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(num)
+   
+    
+    if  num == 3 || num == 5 || num == 5
+        return true
+    else
+        if num % 2 == 0 ||  num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 9 == 0 || num == 1 
+            return false
+        else
+            return true
+        end
+    end
+    
+    
+end

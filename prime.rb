8dee2199e3a904fa15e10091d74beaeb93327391
Samir Singh <singhstart@gmail.com>
2016-08-10 17:09:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  elsif n % 2 == 0
+    return false
+  elsif n % 3 == 0
+    return false
+  else
+  	return true
+  end
+end

9b43fe195a2a997db465efea5cd591ea2cc928f0
Clare <clare.e.horton@gmail.com>
2016-08-28 06:09:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(number)
+   return false if number == 0 || number == 1 
+   (2..(number/2)).each do |i| 
+       return false if number % i == 0 
+    end
+    true
+end

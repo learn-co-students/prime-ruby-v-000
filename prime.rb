dcdf2a0df0c660e64688995f308b47a11b3ea066
Matthew Hallquist <matthallquist@yahoo.com>
2016-08-28 20:44:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+
+def prime?(integer)
+  if (integer == 1 || integer == 0)
+    false
+  elsif (integer != 2) && (integer % 2 == 0)
+    false
+  elsif (integer != 3) && (integer % 3 == 0)
+    false
+  elsif (integer % 1 == 0) && (integer % integer == 0)
+    true
+  end
+end

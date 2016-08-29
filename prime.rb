7b1384eb7d9f1e3035910725e010958b93ea0441
Samuel Lair <lair001@gmail.com>
2016-08-29 20:02:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(num)
+  return false if num <= 1 || num % 2 == 0
+  return true if num == 2 || num == 3
+
+  i = 3
+  while i < num/2 do
+    return false if num % i == 0
+    i += 2
+  end
+  return true
+end

9ba715d06c7592240ad9c6a96e02c6ab03888038
Chirag Dudhat <cdudhat@gmail.com>
2016-09-13 23:34:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@ def prime?(n)
   if n < 2
     false
   else
-    (2..Math.sqrt(n)).none? {|i| n % i == 0}
+    (2..Math.sqrt(n)).none? {|i| n % i == 0} #see the definition of prime numbers in wikipedia
   end
 end

76bd255203975f2cb7a007e341f79028a8036d63
Benny Hudson Hung <wakenightryu@gmail.com>
2016-09-13 21:04:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  criteria = [3, 5, 7, 9]
+  if number.even?
+    false
+  elsif number == 1
+    false
+  elsif number == 3
+    true
+  elsif criteria.all? {|criterium| number % criterium != 0}
+    true
+  else
+    false
+  end
+end

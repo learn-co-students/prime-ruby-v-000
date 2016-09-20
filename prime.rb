043cce77d110fd965e71dc618aeba53b6749d79a
noson cohen <natancohen23@gmail.com>
2016-09-20 15:42:46 +0000

    #prime? tests for integer arguement

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if (num == 0 || num == 1) || num > 2 && num.even?
+    return false
+  end
+  array = (2..num - 1).to_a
+  array.each do |x|
+    if num % x == 0
+      return false
+    end
+  end
+else
+  true
+end

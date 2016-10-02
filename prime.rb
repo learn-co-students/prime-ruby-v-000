5a7fd3006b435893dd792497107b26181f929630
Marine Boudeau <m14868@gmail.com>
2016-10-02 13:31:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(int)
+  if int === 1 
+    false
+  else
+    i = 100
+    divisors = []
+    while i > 1
+      if int % i === 0
+        divisors << i
+      end
+      i -= 1
+    end
+    puts "Number: #{int} has #{divisors.length} divisors."
+    if divisors.length > 1
+      false
+    else
+      true
+    end
+  end
+end

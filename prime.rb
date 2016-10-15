d5e9cabfa6b1a4acbed4458c719319b213de6341
Bruno Boehm <bruno.boehm@gmail.com>
2016-10-15 18:59:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(number)
+  if number == 2
+    true
+  elsif number == 1 || number == 0
+    false
+  else
+    i = 2
+    while i < number
+      return false if number % i == 0
+      i += 1
+    end
+    true
+  end
+end

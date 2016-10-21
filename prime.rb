2ec59cc1c3a7f54276f087c739f4e34651c53dbe
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2016-10-21 21:24:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+# Might try batches?
+#(2..#)
+#eliminate after each
+
+def prime?(num)
+    if num < 2 || num ==0
+        return false
+    elsif num==2
+        return true
+    else
+        divs = (2..num-1).to_a
+
+        while divs.length > 0 do
+            if num%divs.first==0
+                return false
+            end
+            divs.shift
+        end
+        return true
+    end
+end

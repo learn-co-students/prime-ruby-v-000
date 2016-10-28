8bdb60457db9d10a0c7151fc174c8d8f997cd909
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-10-28 02:34:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(n)
+   #(2..n/2).none?{|i| n % i == 0}
+   i = 2
+   count = 0
+while i <= n
+if(n%i == 0)
+  count += 1
+end
+i += 1
+end
+if(count == 1)
+  x =  true
+else
+  x =  false
+end
+x
+end

f44cab71c05f6d89472d90545da02ab61f1a0d96
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-10-29 04:25:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+   if (number < 2)
+     return false
+   elsif (number < 4) && (number > 1)
+     return true
+   elsif (number % 2 == 0) || (number % 3 == 0)
+     return false
+   else
+     i = 5
+     while i < number
+       if (number % i == 0)
+         return false
+       end
+        i += 1
+     end
+   end
+   return true
+
+end

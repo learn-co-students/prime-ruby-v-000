822e89759a75cb6d69ee8bd78951fbcf4bf11ec7
Teresa Grosch <tgrosch91@gmail.com>
2016-11-03 01:02:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+def prime?(num)
+  if num==0 || num==1 || num==-1
+    false
+  else
+    if num>0
+      range=2..(num-1)
+      range=range.to_a
+      if range.any? do |div|
+        num%div == 0
+        end
+        false
+      else
+        true
+      end
+    else
+      num=num*-1
+      range=2..(num-1)
+      range=range.to_a
+      if range.any? do |div|
+        num%div == 0
+        end
+        false
+      else
+        true
+      end
+    end
+  end
+end

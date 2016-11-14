0feeaac2f83472b332a48212d906434157611406
Tayari Appiah <cjhhh.ta@gmail.com>
2016-11-14 22:27:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,25 @@
 # Add  code here!
+
 def prime?(number)
-  prime = (2..number).to_a
-prime.each { |n|
-  if 1 % number == 0 && n % number == 0
-    return true
-  else
-    false
+  # i need to control for weird outliers
+  if number <= 1
+    return false
   end
-}
+
+  # I need a list of all possible divisors
+  prime = (2..number - 1).to_a
+
+  #i need to go through all the divisors  and see if any go into the number evenly
+  # if they do, return false
+  # if none of them divide ebenly, return true
+  prime.each do |n|
+    return false if number % n == 0
+  end
+
+  true
+end
+
+def prime?(number)
+  return false if number <= 1
+  (2..number/2).none? {|n| number % n == 0 }
 end

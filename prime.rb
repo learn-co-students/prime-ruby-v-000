3b622a2aafff16374e793991751332caf4e66773
Senad Hadrovic <senadhadrovic@gmail.com>
2016-11-20 13:13:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(number)
+  i = 2
+ if number <= 1
+   return false
+ elsif number == 2
+   return true
+ elsif
+     while i < number
+       if number % i == 0
+         return false
+       else
+         i += 1
+       end
+     end
+   else
+     return true
+   end
+end

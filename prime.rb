f1f2abe6f6760a5a789c69adf40fefbf0dbef378
Adam Lucia <lucia.adam@gmail.com>
2016-12-06 23:16:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  return true if ((num == 2 || num == -2) || (num == 3 || num == -3))
+  return false if ((num == 1 || num == -1) || (num % 2 == 0 || num % 3 == 0))
+
+  if num > 1
+    factor_arr = (2..(num/2)).to_a
+    factor_arr.each { |factor| return false if num % factor == 0 }
+  else
+    factor_arr = ((num/-2)..-2).to_a
+    factor_arr.each { |factor| return false if num % factor == 0 }
+  end
+  true
+end

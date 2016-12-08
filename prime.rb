80c587b9581dff9fbc8a32bb431d4eed8ff59a1a
Alexander Friedland <friedlandaj@gmail.com>
2016-12-08 23:34:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+	primes = []
+    (1..n).each {|num|
+        if
+            (2..num-1).all? {|denom| num%denom >0}
+        then
+            primes.push(num)
+        end
+    }
+    if primes.include?(n) == true && n != 1
+    	true
+    else
+    	false
+    end
+end

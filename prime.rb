b886edac311a35badbce09cc187e46d8244612fe
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2016-12-10 03:25:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  num = -num if num < 0
+  return false if num == 1 || num == 0
+
+  i = 2
+  while i * i <= num
+    return false if num % i == 0
+    i += 1
+  end
+  true
+end

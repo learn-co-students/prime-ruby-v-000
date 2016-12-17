266967c39489cd5149348acb56f90fe00d316133
Daniel Goldberg <danpaulgo@aol.com>
2016-12-17 04:17:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    prime = false
+  else
+    prime = true
+    all_numbers = (2..(num-1)).to_a
+    all_numbers.each do | number |
+      if num % number == 0
+        prime = false
+        break
+      end
+    end
+  end
+  prime
+end

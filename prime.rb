e96a01e0fe9b42e5a15f3dcc4b2ffedb2c83e0e0
Alyssa Sidebottom <alyssa.m.sidebottom@gmail.com>
2016-12-19 21:49:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  absnumber = number.abs
+  divisor = absnumber - 1
+  prime = false
+  while divisor > 1
+    if absnumber % divisor == 0
+      prime = false
+      break
+    else
+      prime = true
+    end
+    divisor -= 1
+  end
+  return prime
+end

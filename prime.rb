33af35afccc729e90cd4d4de237de727d1e9ef69
Tom Su <djtomblue@gmail.com>
2016-12-23 03:33:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry'
+#Returns True for numbers that cannot be divided by anything except for 1 and itself. False if it can.
+
+def prime?(number)
+  a = []
+  n = (2...number).to_a
+  if number == 1 || number == 0 || number.negative?
+    false
+  else
+    n.collect {|x| a << number % x}
+      if a.include?(0)
+      false
+    else
+      true
+    end
+  end
+  # (2...number).to_a.each {|x| (number/x).integer? ? false : true}
+end

6f69c811460d60e6a87b524c9110b5cb9acb5d1f
charlie lee <ctlee9096@gmail.com>
2016-12-24 22:13:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,33 @@
-require 'pry'# Add  code here!
-def prime?(number)
-  binding.pry
-if number/1 == number || number/number == 1 
-    binding.pry
-    return true
-elsif number<=0
-    false
-else
-  false
+#require 'pry'
+def sieve(max)
+  # Set up an array with all the numbers from 0 to the max
+  primes = (0..max).to_a
+
+  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
+  # aren't prime.
+  primes[0] = primes[1] = nil
+
+  # Iterate through primes array
+  counter = 0
+  primes.each do |p|
+    # Skip if nil
+    next unless p
+    # Break if we are past the square root of the max value
+    break if p*p > max
+    counter += 1
+    # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and replace
+    # that value with nil in the primes array
+    (p*p).step(max,p) { |m| primes[m] = nil }
   end
+
+  # Finally, return the compacted array.
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
 end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
+
+##https://gist.github.com/loganhasson/8937903

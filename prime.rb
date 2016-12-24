cc44622ee3db0ab26f8cfac469f988be94fe5c34
Bianca Laurendine <bnlauren@gmail.com>
2016-12-24 05:40:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@ def prime?(n)
   if n <= 1  #takes care of negative values
      false
   else
-     (2..n/2).none? {|i| n % i == 0}  # modification of searching up to sqrt(number), returns true if number is not a multiple of any of the divisors between 2 and number/2
+     (2..n/2).none? {|i| n % i == 0}  #take number from 2 to n/2
    end
  end

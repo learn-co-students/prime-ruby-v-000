ba5db611c868b4cc0a9134bd7191a2911301a97f
charlie lee <ctlee9096@gmail.com>
2016-12-24 05:48:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'pry'# Add  code here!
+def prime?(number)
+  binding.pry
+if number/1 == number || number/number == 1 
+    binding.pry
+    return true
+elsif number<=0
+    false
+else
+  false
+  end
+end

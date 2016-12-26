4849a27238485d35fa7a6096f0b1f6b4b091a28a
charlie lee <ctlee9096@gmail.com>
2016-12-26 17:15:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,31 +1,22 @@
-def sieve_of_eratosthenes(max)
-  arr=(0..max).to_a
-  arr[0] = arr[1] = arr[2] = nil
-  counter = 0
-  arr.each do |i|
-    next unless i
-    break if i*i > max
-    counter += 1
-    (i*i).step(max,i) { |m| arr[m] = nil }
-     #http://ruby-doc.org/core-1.9.3/Array.html delete_if deletes every
-     #elements of self for which block evaluates to true.  
-   end
-   puts "solved for #{max} in #{counter} steps"
-   arr
-end
-
-# Example Call
-puts sieve_of_eratosthenes(20)
-
+require 'pry'# Add  code here!
 def prime?(number)
-	if sieve_of_eratosthenes(number).include?(number)
-		true
-	else
-		false
-	end
+  negative_number = number *-1
+	not_prime_array= [0,1,-1]
+  not_prime_array.last << negative_number
+  binding.pry
+	#not_prime_array.each do |item|
+		if number == "#{not_prime_array[item]}"
+      binding.pry
+      false
+    elsif number/number == 1 || number/1 == number
+      true
+    else
+      false
+		end
+	#end
 end
 
-#failed to work for 4, wonder why?
+#https://gist.github.com/loganhasson/8937903
 
 
 

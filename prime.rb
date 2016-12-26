46ffbb5100d2a9d9b42239623fd5bad551308467
charlie lee <ctlee9096@gmail.com>
2016-12-26 18:07:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,23 +1,10 @@
-require 'pry'# Add  code here!
 def prime?(number)
   negative_number = number *-1
-	not_prime_array= [0,1,-1]
-  not_prime_array.last << negative_number
+  not_prime_array= [0,1,-1]
+  #.last returns the last element of self
+  # append << pushes given object on array end
+  not_prime_array<< negative_number
 	#not_prime_array.each do |item|
-  not_prime_array.each do|item| 
-    if number== "#{not_prime_array[item]}" 
-      binding.pry
-      false 
-    elsif number/number == 1 || number/1 == number
-      true
-    else
-      false
-		end
-  end
+  #.each returs element as output
+  not_prime_array.include? (number)
 end
-
-#https://gist.github.com/loganhasson/8937903
-#https://rosettacode.org/wiki/Sieve_of_Eratosthenes#Ruby
-
-
-

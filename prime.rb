e22f4c658c31d7851f1581aab0136c4f53c1739e
Jonathan Foster <redcabinny@gmail.com>
2017-01-05 03:46:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
 # Add  code here!
+def prime?(n)
+  if n < 0 || n == 1
+    return false
+  end
+  if n == 2
+    return true
+  end
+  if n == 3
+    return true
+  end
+  if n % 2 == 0
+    return false
+  end
+  if n % 3 == 0
+    return false
+  end
+  i = 5
+  w = 2
+  while i * i <= n
+    if n % i == 0
+      return false
+    end
+    i += w
+    w = 6 - w
+  end
+  true
+end

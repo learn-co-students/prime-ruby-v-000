01e82b574f578fb85d24a6e16173f8519df94cac
Alex Glover <alexanderbglover@gmail.com>
2017-01-05 00:42:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number<=2
+    return false
+  else
+    range=*(2..number-1)
+    range.each do |i|
+      return false if number % i == 0
+    end
+  end
+  true
+end

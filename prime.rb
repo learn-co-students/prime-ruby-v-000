325793eab9485b46e2ccaea7c8c983900966aa5c
Jenna Leopold <jleopold424@gmail.com>
2017-01-10 18:30:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+  return false if integer <= 1
+  numbers = (2..integer - 1).to_a
+  numbers.each do |number|
+    if (integer % number == 0)
+      return false
+    end
+  end
+  true
+end

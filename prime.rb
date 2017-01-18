c66494ff7a2100c3d8e4e3e110d92b3124cb60f8
Elias Ventura <eliasventura23@gmail.com>
2017-01-18 01:34:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,3 @@
 def prime?(number)
-  is_prime = true
-
-  (2..number/2).each do |i|
-    if (number % i == 0)
-      is_prime = false
-      break
-    end
-  end
-
-  return is_prime == true && number > 1 ? true : false
+  number >= 2 && (2..number/2).none? { |n| number % n == 0 }
 end

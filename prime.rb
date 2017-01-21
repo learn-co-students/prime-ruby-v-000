6fc6b48df6771f136fc9493079c03ceba752098a
Falgoon Patel <falgoonit@gmail.com>
2017-01-21 23:56:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(int)
+  if int < 2
+    false
+  else
+    max_num = int / 2
+    if (2..max_num).any? {|i| int % i == 0}
+      false
+    else
+      true
+    end
+  end
+end

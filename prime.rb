623b8f4cae63395a17c1bf539cd2e86352f340e1
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-01-29 22:35:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+    if number < 2 || number.even? || number % 6 == 0
+      false
+    else
+    divs = (2..Math.sqrt(number)).to_a
+    divs.delete_if {|i| i > 2 && i.even?}
+    divs.none? do |i|
+      number % i == 0
+    end
+  end
+end

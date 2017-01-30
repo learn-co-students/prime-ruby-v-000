e6929a87792fa671afbf3d63bfbfab16db7a36e9
Carlos Quinones <cquinones100@gmail.com>
2017-01-30 21:38:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,5 @@
-# Add  code here!
 def prime?(num)
-  return false if num <= 1 || num.even?
   test_array = (2..(num/2)).to_a
-  return false if test_array.find { |x| num % x == 0}
+  return false if num <= 1 || num.even? || test_array.find { |x| num % x == 0}
   true
 end

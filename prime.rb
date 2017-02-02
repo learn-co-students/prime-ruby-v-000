e710c68a290bdb9b8640b10a127358084772be77
Isis Tejeda <isis@isistejeda.com>
2017-02-02 13:03:13 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+# def prime?(number)
+#   return false unless number > 1
+#
+#   !(2...number).any? do |divisible|
+#     number % divisible == 0
+#   end
+# end
+
+
+
+def prime?(number)
+  return false unless number > 1
+
+  divisible = 2
+  while divisible < number
+    if number % divisible == 0
+      return false
+    end
+    divisible += 1
+  end
+  true
+end

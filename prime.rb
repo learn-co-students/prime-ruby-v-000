1a4d1c015d607a06efd4dca2c6928cfddea03013
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-04 21:54:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+
+  elsif num == 2 || num == 3
+    return true
+    
+  else
+    for i in (2..(num/2))
+      if num%i == 0
+        return false
+      end
+    end
+      return true
+  end
+end

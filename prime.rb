00a5a9677ae0914332e04349dab8df60bfe65ffe
Pepper <axpepper@gmail.com>
2017-02-07 19:20:11 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+	if int<=1
+		return false
+	elsif int == 2 || int == 3
+		return true
+	else
+	for i in (2..(int-1))
+		if 0 == (int % i)
+			return false
+		end
+	end
+	return true
+	end
+end

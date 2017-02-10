27d9bbb7cec0b1e85ddb12b6d19e0de33bd4c50f
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-02-10 04:09:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  end
+
+  is_prime = true
+  i = 2
+  while i < n
+    puts i
+    if n % i == 0
+      is_prime = false
+    end
+    i += 1
+  end
+  return is_prime
+end

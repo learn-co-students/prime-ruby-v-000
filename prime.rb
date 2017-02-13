a22357d75b05c3da04b4ee34a536f3d8fca3a7f8
Dalma Boros <dalma.boros@gmail.com>
2017-02-13 16:33:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,8 @@
 def prime?(number)
-  if [2,3,5,7].include?(number)
-    true
-  elsif number < 2 || number.even?
+  if number < 2 
     false
   else
-    denominators = (7..number).to_a
-    denominators.delete_if { |denominator| denominator.even? }
+    denominators = (2..number).to_a
     return denominators.select { |denominator| (number % denominator == 0) }.count == 1
   end
 end

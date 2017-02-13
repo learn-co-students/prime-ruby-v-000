0e4dabbfe012bf74a2026699f622f6a2ff60be26
Dalma Boros <dalma.boros@gmail.com>
2017-02-13 16:42:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(number)
-  if number < 2 
+  if number < 2
     false
   else
     denominators = (2..number).to_a
-    return denominators.select { |denominator| (number % denominator == 0) }.count == 1
+    denominators.select { |denominator| (number % denominator == 0) }.count == 1
   end
 end

b0d206b8fe5b8354f2c760fcd5be7e99299359a6
mxdavis <md01@me.com>
2017-02-19 22:29:02 +0200

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
+require "pry"
+
 # Add  code here!
+def prime?(i)
+  if i > 1
+    [*2..9].each do |factor|
+      return false if factor == i
+      return false if i % factor == 0
+    
+      end
+    true
+  end
+
+end

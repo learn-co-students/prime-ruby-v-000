0c797f5d191f1be1b104b1f72af02708e866e0a9
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-02-23 06:22:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,32 @@
 # Add  code here!
+require 'benchmark'
+
+Benchmark.bm(15) do |bm|
+
+bm.report('long array') do
+def prime?(integer)
+  if integer > 1
+    num_array = (2..integer.pred).to_a
+    num_array.none?{|number| integer % number == 0}
+  else
+    false
+  end
+end
+
+prime?(105557)
+end
+
+bm.report('trial division') do
+def prime_again?(integer)
+  if integer > 1
+    num_array = (2..integer ** 0.5.to_i).to_a
+    num_array.none?{|number| integer % number == 0}
+  else
+    false
+  end
+end
+
+prime_again?(105557)
+end
+
+end

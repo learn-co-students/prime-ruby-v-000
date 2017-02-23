099f0ea065aa746fc8910293863d8e99774812dc
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-02-23 06:55:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,32 +1,26 @@
 # Add  code here!
-require 'benchmark'
-
-Benchmark.bm(15) do |bm|
-
-bm.report('long array') do
+# first solution using longer array
+=begin
 def prime?(integer)
-  if integer > 1
+  if integer == 2
+    true
+  elsif integer >= 3
     num_array = (2..integer.pred).to_a
     num_array.none?{|number| integer % number == 0}
   else
     false
   end
 end
+=end
 
-prime?(105557)
-end
-
-bm.report('trial division') do
-def prime_again?(integer)
-  if integer > 1
-    num_array = (2..integer ** 0.5.to_i).to_a
+# second solution using trial division
+def prime?(integer)
+  if integer == 2
+    true
+  elsif integer >= 3
+    num_array = (2..(integer ** 0.5).to_i).to_a
     num_array.none?{|number| integer % number == 0}
   else
     false
   end
-end
-
-prime_again?(105557)
-end
-
 end

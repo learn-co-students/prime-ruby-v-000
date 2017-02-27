4a8b37f56f20378837d795a802962e1112116d6e
Nadine Fischoff <nadine.khattak@gmail.com>
2017-02-27 21:02:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,49 +1,24 @@
-def primestuff(num)
- #Create a list of consecutive integers from 2 to n
- rg = (2..num).to_a
 
- #initially, let p=2, the smallest prime number
- p = 2
-
-
- counter = 0
- #Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list
-
-
- array = []
- primes = [2]
-
- while p != nil
-  rg.each do |int|
-
-    array << int * p
-
-  end
-
-
- #Find the first number greater than p in the list that is not marked.
-
-  array1 = rg - array
-
-
-  #puts "#{array1}"
-
-
-
-    p = array1.find {|x| x > p}
-
-
-  primes << p
-  #puts "newp #{p}"
-  #puts "array #{array}"
-  #puts "rg #{rg}"
-  end
-
-  primes
-
+def primeNum(num)
+  
+  
+ array = (0..num).to_a
+ array[0] = array[1] = nil
+ array.each do |p|
+   
+   next unless p 
+   
+   break if p*p > num
+   
+  (p*p).step(num,p) { |m| array[m] = nil }
 end
+  
+  return array
+end    
+
+primeNum(100)
 
 
 def prime?(num)
-  primestuff(num).include?(num)
+  primeNum(num).include? num
 end

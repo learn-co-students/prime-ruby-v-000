0beb720d0b8a319f5e50a2ac2b05fb0a3174d2da
Feiran Wang <wangfeiranpku@163.com>
2017-03-08 03:29:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 1
+    false
+  else
+    i=2
+    numbers=[]
+    while i<integer
+      n=i
+      numbers << n
+      i=i+1
+    end
+
+  numbers.all?{|number|
+    integer % number != 0}
+  end
+end

c9d436cda1c306f826551f850ec16865edbe18c0
Jason McElrath <jmcelrath84@gmail.com>
2017-03-09 11:08:18 -0500

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -1,58 +1,17 @@
-def prime?(n)
-  if n <= 2
-    false
-  elsif n == 3
-    true
-  elsif n % 2 == 0 || n % 3 == 0
-    false
-  else
-    true
-  end
-end
-
-
-# def prime? (n)
-#   if n <=1
-#     false
-#   elsif n <= 3
-#     true
-#   elsif n % 2 == 0 || n % 3 == 0
-#     false
-#   end
-#   i = 5
-#   while i*i <= n
-#     if n % i == 0 || n % (i+2) == 0
-#       false
-#       i = i + 6
-#     end
-#   end
-# end
-
-
 def sieve(max)
-  # Set up an array with all the numbers from 0 to the max
-  primes = (0..max).to_a
+  primes = (1..max).to_a
 
-  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
-  # aren't prime.
   primes[0] = primes[1] = nil
-
-  # Iterate through primes array
+  
   counter = 0
-  primes.each do |p|
-    # Skip if nil
-    next unless p
 
-    # Break if we are past the square root of the max value 
-    break if p*p > max
+  primes.each do |p|
+    next unless p
+    break if p*p > max 
     counter += 1
-    # Start at the square of the current number, and step through.
-    # Go up to the max value, by multiples of the current number, and replace
-    # that value with nil in the primes array
-    (p*p).step(max,p) { |m| primes[m] = nil }
+    (p*p).step(max,p) {|m| primes[m] = nil}
   end
 
-  # Finally, return the compacted array.
   puts "Solved for #{max} in #{counter} steps."
   primes.compact
 end
@@ -61,4 +20,21 @@ def prime?(num)
   sieve(num).include?(num)
 end
 
-puts prime?(105557)
+
+=begin
+Psuedocode algorithm for smaller numbers
+
+function is_prime(n)
+    if n ≤ 1
+        return false
+    else if n ≤ 3
+        return true
+    else if n mod 2 = 0 or n mod 3 = 0
+        return false
+    let i ← 5
+    while i * i ≤ n
+        if n mod i = 0 or n mod (i + 2) = 0
+            return false
+        i ← i + 6
+    return true
+=end

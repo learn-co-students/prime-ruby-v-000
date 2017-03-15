8b0f1f7eba0109ae8049fb2c9b7d7bdb7897c87a
Anthony McCormick-Gonzales <a.j.mccormickgonzales@gmail.com>
2017-03-15 18:55:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+ # Add  code here!
+
+ def prime?(number)
+   if number < 2
+     false
+   else
+     (2..number-1).none? { |test_number| number % test_number == 0}
+   end
+ end

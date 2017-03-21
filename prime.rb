4859d5366236d11dad2e055000df860303158e66
Lia HUANG <lisa.yc.huang@gmail.com>
2017-03-21 00:54:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+
+def prime?(int)
+  if int <2
+    false
+  else
+    max = Math.sqrt(int).floor
+    divisor = (2..max).to_a
+    divisor.each do |num|
+      return false if int % num == 0
+    end
+    true
+  end
+end

f5ac2b0ea1df51fab960280e65d6154e635e7390
Amanda George <ageorge0160@gmail.com>
2017-03-25 14:19:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+# # Add  code here!
+# def prime?(n)
+#   for d in 2..(n-1)
+#     if  n < 2
+#       return false
+#     elsif (n % d) == 0
+#       return false
+#     else
+#       return true
+#     end
+#   end
+# end
+def prime?(n)
+  if n <= 1
+    false
+  elsif n == 2
+    true
+  else
+    (2..n/2).none?{|i| n % i == 0}
+end
+end

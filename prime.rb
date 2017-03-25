b434f895c488ea16f49bf7ecd48c7add019fb368
Jennifer Haddock <jentx123@hotmail.com>
2017-03-25 03:35:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+ prime = true
+ if int <= 1
+  prime = false
+else
+  array = (2..int - 1).to_a
+  array.each do |num|
+    if int % num == 0
+      prime = false
+      break
+    end
+  end
+end
+ prime
+end

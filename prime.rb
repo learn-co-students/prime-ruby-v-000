0001361bea030204058ff5d3b308a9aa7c4dc7fa
Aditi Green <asaki444@gmail.com>
2017-03-31 18:16:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+num_array = (1..num).to_a
+i = 0
+divisors = []
+  while i < num_array.length
+     if num%num_array[i] == 0
+       divisors << num_array[i] 
+     end
+     i +=1
+  end
+  if divisors.size == 2
+    return true
+  else
+    return false
+  end
+end

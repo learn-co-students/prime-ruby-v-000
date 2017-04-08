e99cc0c36bbbb0c9228f043bebfed151cd0aee2b
Saad Ahmed <dropheaven@live.com>
2017-04-08 22:55:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+
+  i = 2
+  while i <= num / 2
+    if num % i == 0
+      return false
+    end
+    i += 1
+  end
+    true
+end

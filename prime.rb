91f64fd4bdd86c913d1b1d5820fec6cb8f0eeac5
Rosie Martinez <rosiemin11@gmail.com>
2017-04-11 22:14:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime? (n)
+  if n<=1
+    false
+  else
+    i=2
+    number = []
+    while i<n
+      m=i
+      number << m
+      i += 1
+    end
+
+    number.all? do |num|
+      n % num != 0
+    end
+  end
+end

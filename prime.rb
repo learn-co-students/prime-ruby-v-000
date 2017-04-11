d2902ac1d479c79cca55c93076c58b6e8969b1c0
sarjumulmi <mulmi.sarju@gmail.com>
2017-04-11 14:58:26 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require 'benchmark'
+
+def prime?(num)
+  return false if num.even?()
+  return false if num<2
+  puts "#{(num-1)**0.5}"
+  divisors= (2..(num-1)**0.5).to_a
+  divisors.none? {|divisor|
+    num%divisor==0
+  }
+end
+puts "11215 is prime?: #{prime?(11215)}"
+puts Benchmark.measure{prime?(11215)}

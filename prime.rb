7e13aafad65969703a97cf193314e830bfba1891
Martyn Conkling <haoleboy3000@yahoo.com>
2017-04-13 10:18:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(integer)
+
+  test_array = (2..integer-1).to_a
+  if integer <= 0 || integer == 1
+    return false
+  else
+    test_array.all? {|divisor| integer % divisor != 0}
+  end
+end

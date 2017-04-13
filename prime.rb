8d83f8a0ab82e6cf7c2d8d1c8b463aa10379422c
Cynthia Rittenbach <crittenbach@gmail.com>
2017-04-13 03:25:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  if int.positive? == false || int == 1
+    return false
+  end
+  range = (2..int - 1).to_a
+  range.find do |n|
+   if int == 1 || int < 1
+     return false
+   elsif int % n == 0 || int % -n == 0
+     return false
+   end
+  end
+ return true
+end

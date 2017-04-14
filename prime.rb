3dffead6a4ab06e3f118c5b5eb0041222562a94d
Julia Thompson <thompson.julia@gmail.com>
2017-04-14 14:20:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+	if num <= 1
+		return false
+	else
+		(2...num).each do |divisor|
+			if num % divisor == 0
+			  puts "#{num} % #{divisor} #{num % divisor}"
+				return false
+			end
+		end
+	true
+	end
+end

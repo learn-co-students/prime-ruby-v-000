f02dc67c9daa59f6bf121ffcb221720116df401d
Aly Alidina <aly_alidina@hotmail.com>
2017-04-15 14:35:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+require 'benchmark'
+require 'bigdecimal/math'
+
+def prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  puts Benchmark.measure { BigMath.PI(10_000) }
+  true
+
+end

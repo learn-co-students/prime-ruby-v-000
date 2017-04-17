298f5384b545e076f8c278574672fe685e0c46ea
lucketta <lucketta@umich.edu>
2017-04-17 02:45:18 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require 'pry'
+
+def prime?(n)
+  array = (2..(n-1)).to_a
+
+  if (n > 1)
+    array.each do |item|
+      if (n % item == 0)
+        return false
+      end
+    end
+    return true
+
+  else
+    false
+  end
+end

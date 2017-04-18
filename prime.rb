e752ec588c83c79c73e308e64ac01bd0a7e8b83b
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-04-18 19:22:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,14 @@
 
-def prime?(numbers)
-
+def prime?(number)
+x = (-100..100).to_a
+  x.any? do |num|
+    if number > 1 && number % num == 0 && number % 1 == 0
+      true
+    elsif number % num != 0 ||
+      false
+    end
+  end
+end
 
 
 

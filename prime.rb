da794d6ed5ea187bc1dadc6c2f662b5c51cd8d06
Heather You <heatherhyou@gmail.com>
2017-04-18 17:45:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(n)
+  i = 0
+  prime_number = ('1'..'n').to_a
+  while i < prime_number.length
+    prime_number.each do |num|
+      if num % 1 == 0 && num % n == 0 && num > 0
+        return true
+      else
+        return false
+      end
+    end
+  end
+end

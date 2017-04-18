56f3a302c0c1ed9953a754bd52ea6089968c35e4
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-04-18 19:05:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+
+def prime?(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+#def prime?(number)
+#  if number != 0 && number % 1 != 0 && number % number != 0
+#     false
+#  elsif number
+#    true
+#  end
+#end

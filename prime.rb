3a1006ca8f3ebe5201b1c304f2e34763b27b3419
Heather You <heatherhyou@gmail.com>
2017-04-18 19:59:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,14 +1,9 @@
-# Add  code here!
 def prime?(n)
-  i = 0
-  prime_number = ('1'..'n').to_a
-  while i < prime_number.length
-    prime_number.each do |num|
-      if num % 1 == 0 && num % num == 0 && num > 0
-        return true
-      else
-        return false
-      end
-    end
+  if n <= 1
+    false
+  elsif n == 2
+    true
+  else
+    (2..n/2).none? {|i| n % i == 0}
   end
 end

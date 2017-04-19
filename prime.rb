19219898ff42e5b938e13521bb9fbe51e3170fcd
Monika Williams <monika.m.williams@gmail.com>
2017-04-19 08:39:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(i) #prime numbers are divisible by themselves and 1
+    if i <= 1 #one is not prime
+  return false
+elsif i == 2 #two is the only even prime number
+       true
+elsif (2..(i/2)).any? {|prime| i % prime == 0 } 
+  return false
+else
+  return true
+   end
+end

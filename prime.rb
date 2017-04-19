57addf8c0bf4e3c609ffaaff0b9c93e2f217c605
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-04-19 00:38:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
-require 'pry'
+
 
 def prime?(number)
-  if integer <= 1
-    return false
-  end
-  for num in 2..(number - 1)
-    if number % num == 0
-      return false
-    end
-  end
-      true
-end
+   if number <= 1
+     return false
+   end
+   for num in 2..(number - 1)
+     if number % num == 0
+       return false
+     end
+   end
+   true
+ end

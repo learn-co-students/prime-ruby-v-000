84127761c34a7b6b0dda3230c491857d45ba7155
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-19 01:28:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(integer)
+    if integer <= 1
+        return false
+    end
+
+    i = 2
+    limit = sqrt(integer)
+    while i < limit
+        if integer % i == 0
+            return false
+        end
+        i += 1
+        limit = integer / i
+    end
+    return true
+end

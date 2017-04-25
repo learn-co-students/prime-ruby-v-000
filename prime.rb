549d33d5c1e189f09f5470654ede8213ff8746b3
Stephen Harrington <stephenharrington04@yahoo.com>
2017-04-25 15:29:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'pry'
+
+def prime?(int)
+  if int <= 1
+      return false
+  else
+    divided_num = 0.0
+    number_array = (2...(int)).to_a
+    number_array.none? do |number|
+      divided_num = Float(int) / Float(number)
+      rounded_num = divided_num.round
+      rounded_num - divided_num == 0
+    end
+  end
+end

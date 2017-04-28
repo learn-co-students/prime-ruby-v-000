182f75de986bff1b1d97a17c06658b5a9572e32e
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-04-28 15:20:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,2 +1,18 @@
 def prime?(n)
-  
+  numbers = (0..n).to_a
+  numbers.slice!(0..1)
+  divisors = []
+  prime = true
+  if  n < 2
+    prime = false
+  else numbers.each do |number|
+    if n % number == 0
+      divisors << number
+    end
+    if divisors.length > 1
+      prime = false
+    end
+  end
+end
+return prime
+end

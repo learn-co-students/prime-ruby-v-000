aa53d8b02148150a3e9112e9a386d0db85d66d98
Timothy Ege <retibra@gmail.com>
2017-04-30 13:09:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  end
+
+  array = (2..(n-1)).to_a
+      if n == 2
+        return true
+      end
+    array.each do |divisor|
+      if n%divisor == 0
+        return false
+      end
+    end
+    return true
+end

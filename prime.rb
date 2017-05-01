6e4572ee8c905bb4164b2d7aec045fe2c0694887
Sam Zeidman <sam.zeidman@gmail.com>
2017-05-01 16:52:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+require 'benchmark'
+
+def prime?(n)
+  if n < 2
+    false
+  else
+    range = (2..n).to_a
+    range.none? {|i|
+      if (i * i) <= n
+        n % i == 0
+      end
+    }
+  end
+end
+
+puts Benchmark.measure{prime?(123413)}

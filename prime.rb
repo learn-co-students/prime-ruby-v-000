f0fced5bd81cb9f15ad087293234a3e63ba2931d
Alex <github email address>
2017-05-04 20:18:50 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+
+        factors = 0
+        n=1
+        while n <= integer
+            if integer %n==0
+              factors+=1
+            end
+            n+=1
+        end
+        factors <= 2 && integer >1 ? true : false
+
+
+end

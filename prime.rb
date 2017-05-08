9e68a824644f68cac5d3139b838b8cefdf3a94ac
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-05-08 23:46:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if(number <= 1)
+      return false
+  end
+
+  for num_count in 2...(number/2)+1
+      if(number % num_count == 0)
+          return false
+      end
+  end
+  return true
+end

ce9fd722d631e91f54427b7d83c149e2b9a16692
Sam Marshall <samuel.r.marshall@gmail.com>
2017-05-13 16:27:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require "pry"
+
+def prime?(num)
+  divid = [*1..num]
+  collection = []
+  flag = false
+  divid.each_with_index do |digit,index|
+    if num%digit==0
+      collection << divid[index]
+    end
+    #binding.pry
+  end
+  if collection == [1,num]
+    flag = true
+  end
+  return flag
+end

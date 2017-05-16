4b06dbaa0bab9838a6a4af0e9aded6990318d853
Noah Summers <summers.noah@gmail.com>
2017-05-16 19:31:16 -0400

    reduce verbosity

--- prime.rb
+++ prime.rb
@@ -1,26 +1,11 @@
-def divisible_by_remaining_odd_numbers?(n)
-  potential_divisors = (3..Math.sqrt(n)).step(2).to_a
-  divisible = false
-  potential_divisors.each do |x|
-    if n % x == 0
-      divisible = true
-      potential_divisors = []
-    else
-      potential_divisors.delete_if { |y| y % x == 0 }
-    end
-  end
-
-  divisible
-
-end
-
-
 def prime?(n)
   if n == 2
     true
   elsif n <= 1 || n.even?
     false
+  elsif (3..Math.sqrt(n)).step(2).to_a.any? {|x| n % x == 0}
+    false
   else
-    divisible_by_remaining_odd_numbers?(n) ? false : true
-  end  
+    true
+  end
 end

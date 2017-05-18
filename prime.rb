703739db3ca170749b4b36acdaaff639bf199b13
Menachem Hornbacher <me@mendel.tech>
2017-05-18 18:33:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  return false if number <= 1
+  return true if number <= 3
+  return false if number % 2 == 0 || number % 3 == 0
+  i = 5
+  while i * i <= number
+    return false if number % i == 0 or number % (i + 2) == 0
+    i += 6
+  end
+  return true
+end

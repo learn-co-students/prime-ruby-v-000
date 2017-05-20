7a570cb8a3c72257208c55126b3b36f0d16777ae
ctab33 <ctab33@me.com>
2017-05-20 11:52:12 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+# def prime?(n)
+#   #i = 2
+#   if n <= 1 || n % 2 == 0
+#     false
+#   elsif n/n == 1 && n/1 == n
+#     true
+#   end
+# end
+
+def prime?(n)
+  if n <= 1
+    return false
+  end 
+  (2..n-1).each do |num|
+    if n % num == 0
+      return false
+    end
+  end
+  return true
+end

08f48b8a7e12597266eea43b97413e8c091ffaf9
Elysa Silveira <elysa.silveira@gmail.com>
2017-05-22 01:57:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(integer)
+
+if integer<=1
+  return false
+else
+  m_array = [2]
+  i = 2
+  while i< (integer)**0.5
+    m_array<< i
+    i+=1
+  end
+
+  m_array_div = []
+  m_array.each do |num|
+    m_array_div<< (integer)%num
+  end
+
+  m_array_div.include?(0) ? false : true
+end
+end

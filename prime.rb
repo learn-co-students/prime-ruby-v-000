d0b28eeda601c2a0f898c5c413d011bd344cb8fb
Richard Burd <richardburdx@yahoo.com>
2017-05-22 14:45:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def sorting(top)
+  prime_numbers = (0..top).to_a
+  prime_numbers[0] = prime_numbers[1] = nil
+  counter = 0
+  prime_numbers.each do |i|
+    next unless i
+    break if i*i > top
+    counter += 1
+    (i*i).step(top,i) { |y| prime_numbers[y] = nil }
+  end
+
+  puts "Solved for #{top} in #{counter} steps."
+  prime_numbers.compact
+end
+
+def prime?(number)
+  sorting(number).include?(number)
+end
+
+puts prime?(1)

a303c2f64033e8f357ef6e980faba8ebd72d3ea1
mikka <mikkanthrope@gmail.com>
2017-05-24 23:04:52 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,17 @@
 def prime?(int)# Add  code here!
+
   i = 2
-  if int > 1
-      while i < int
-        if int % i == 0
-          return false
-        end
-        i += 1
-      end
-    return true
+
+  if int <= 1
+    return false
   end
-  return false
+
+  while i < int
+    if int % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+
 end

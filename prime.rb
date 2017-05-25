ca9f2006554442f07d968506cf3aa9d6e72ac0b9
mikka <mikkanthrope@gmail.com>
2017-05-24 22:56:10 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)# Add  code here!
+  i = 2
+  if int > 1
+      while i < int
+        if int % i == 0
+          return false
+        end
+        i += 1
+      end
+    return true
+  end
+  return false
+end

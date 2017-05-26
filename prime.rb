76968131562b601c4f0123e21b4e8d1305618ef8
Joseph Kim <josephkim0224@gmail.com>
2017-05-26 13:10:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
 # Add  code here!
+def prime?(num)
+  return false if num < 2
+  return true if num == 2 || num == 3
+  find_prime_numbers_below_sqrt(Math.sqrt(num).to_i, num) # this uses ruby library but NOT prime()
+  #find_prime_numbers_below(num)
+end
+
+# This method iterates worst case of O(sqrt(n))
+def find_prime_numbers_below_sqrt(revised_num, original)
+  i = 2
+  while i <= revised_num
+    return false if original % i == 0
+    i += 1
+  end
+  true
+end
+
+# Bruteforce worst case of O(n) interation
+def find_prime_numbers_below(num)
+  i = 2
+  while i <= num - 1
+    return false if num % i == 0
+    i += 1
+  end
+  true
+end

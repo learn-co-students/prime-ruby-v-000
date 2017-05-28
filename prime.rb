e6cbc79a2cf04411f34e998dabe4d0c7765ebf7f
Luisa Scavo <luisascavo@gmail.com>
2017-05-28 12:26:58 +0000

    refactor prime

--- prime.rb
+++ prime.rb
@@ -1,15 +1,11 @@
 # Add  code here!
 def prime?(n)
-  if n <= 1
-    false
-  elsif n <= 3
-    true
-  else
+  if n > 1
     return false if (2..Math.sqrt(n)).detect {|denom|
     n % denom == 0
     }
     true
+  else
+    false
   end
-
-
 end

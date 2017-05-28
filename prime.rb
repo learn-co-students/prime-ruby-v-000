d700850eb120497f42a800955d9ec1ffb95709be
Luisa Scavo <luisascavo@gmail.com>
2017-05-28 02:30:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    false
+  elsif n <= 3
+    true
+  else
+    return false if (2..Math.sqrt(n)).detect {|denom|
+    n % denom == 0
+    }
+    true
+  end
+
+
+end

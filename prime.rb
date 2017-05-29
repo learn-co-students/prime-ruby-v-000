f0cfe7e5a21051dd086dcd4c7e9fb2d835935143
Nicholas D'Amico <nickalan82@icloud.com>
2017-05-29 20:29:22 +0000

    refactor

--- prime.rb
+++ prime.rb
@@ -1,24 +1,5 @@
 require "pry"
 
-def prime?(n)
-  if n <= 1
-       return false
-   elsif n <= 3
-       return true
-   elsif n % 2 == 0 || n % 3 == 0
-       return false
-  else
-    i = 5
-   while i * i <= n
-     if n % i == 0 || n % (i + 2) == 0
-       return false
-     end
-     i = i + 6
-   end
-   return true
-  end
-end
-
 # def prime?(n)
 #   if n <= 1
 #        return false
@@ -27,8 +8,28 @@ end
 #    elsif n % 2 == 0 || n % 3 == 0
 #        return false
 #   else
-#     range = (3..Math.sqrt(n)-1).to_a
-#     range.detect { |i| n % i == 0 }
+#     i = 5
+#    while i * i <= n
+#      if n % i == 0 || n % (i + 2) == 0
+#        return false
+#      end
+#      i = i + 6
+#    end
+#    return true
 #   end
-#   true
 # end
+
+def prime?(n)
+  if n <= 1
+     return false
+ elsif n <= 3
+     return true
+ elsif n % 2 == 0 || n % 3 == 0
+     return false
+else
+  range = (2..Math.sqrt(n)).to_a
+  return false if range.any? { |i| n % i == 0 }
+end
+return true
+ end
+ prime?(25)

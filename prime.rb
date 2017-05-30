378a9447443277c5aec1ae1b9d723cfcb2d84d35
Ninian Bond <gentile.bond@gmail.com>
2017-05-30 22:11:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  array = (2..(num - 1)).to_a
+  missed = []
+  missed = array.select do |number|
+    num % number == 0
+  end
+  if missed == []
+    return true
+  else
+    return false
+  end
+end

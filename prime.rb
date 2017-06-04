11301880f24d49a1c00570adb74a83467ae6d16c
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-04 23:51:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,33 @@
+require 'benchmark'
+
 def prime?(num)
-  prime_array = (2..num).to_a
-  if prime_array.any? { |int| num % int == 0 }
+  if (2..num-1).any? { |int| num % int == 0 }
+    return false
+  elsif num <= 1
     return false
   else
      return true
    end
+end
+
+def prime_one?(num)
+  if num <= 1 || (2..num-1).any? { |int| num % int == 0 }
+  return false
+  else
+     return true
+   end
+end
+
+Benchmark.bm(10) do |bm|
+  bm.report('method') do
+    1000.times do
+      prime?(60)
+    end
+  end
+
+  bm.report('method_one') do
+    1000.times do
+      prime_one?(60)
+    end
+  end
 end

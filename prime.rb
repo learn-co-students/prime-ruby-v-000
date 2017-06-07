b17c18e8288a8e455604fa84e3b26918f952837e
Meredith Reece <nerdybug@gmail.com>
2017-06-07 05:28:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 def prime?(n)
+  # 0, 1 and negative numbers are not prime
   if n < 2
     return false
   end
+  # no need to use divisors that are more than half of n
+  # the divisor won't divide evenly if it's more than half of n
   half_n = n / 2
   range = (2..half_n).to_a
   range.none? {|divisor| n % divisor == 0}

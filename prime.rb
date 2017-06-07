efe03f53a603c249023d413bc5d2997898e2e587
Meredith Reece <nerdybug@gmail.com>
2017-06-07 05:19:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(n)
-  half_n = n / 2
-  range = (2..half_n).to_a
   if n < 2
     return false
-  else
-    range.none? {|divisor| n % divisor == 0}
   end
+  half_n = n / 2
+  range = (2..half_n).to_a
+  range.none? {|divisor| n % divisor == 0}
+
 end

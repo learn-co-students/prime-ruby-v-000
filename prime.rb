e4c6bbaececfba0ceb0dc66e7055b845f1d3b0b3
Brian Bytnar <bytnarb2@gmail.com>
2017-06-07 02:09:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    return false
+  else
+    i = 2
+    array = (2...n)
+    array.each do |number|
+      if n % number == 0
+        return false
+      end
+    end
+  end
+  true
+end

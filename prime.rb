ee24db80ee64a5a1267dd0057abdfe2a8e494e89
Denise Dearman <denisedearman@Patricks-MacBook-Pro.local>
2017-06-08 13:27:25 -0400

    naive solution

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(n)
+  d = 3
+  if n % 2 == 0 || n == 1 || n == -1
+    return false
+  end
+  while d < n
+    if n % d == 0
+       return false
+    end
+    d += 2
+  end
+  true
+end

8c5178da22961dc24ab1ec084ecb3e3f42f26c59
github username <github email address>
2017-06-14 17:13:01 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  x = 2
+  if (int <= 0 || int == 1 || int % 2 == 0)
+    return false
+  else
+    while x * x <= int
+    return false if int % x == 0
+    x += 1
+    end
+  true
+end
+
+
+end

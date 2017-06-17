4e743638067666a38499f2897a6732d67a27d950
Justina Tran <justinavtran@yahoo.com>
2017-06-17 18:30:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+  number = -number if number < 0
+    return false if number == 1 || number == 0
+  x = 2
+  while x * x <= number
+    return false if number % x == 0
+    x += 1
+  end
+ true
+end

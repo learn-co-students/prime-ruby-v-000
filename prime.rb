5753c644e5983bfd8a1192f8f938168b55804dff
Ary Baldioceda <abaldioceda@gmail.com>
2017-06-22 21:54:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,20 +1,10 @@
 # Add  code here!
 
-#def prime?(i)
-  #i % 1 == 0 && i % i == 0
-#end
-
 def prime?(i)
   range = (1..i).to_a
   n = []
   range.each do |e|
     n << i/e if i%e == 0
   end
-  if n.size == 2
-  true
-  else
-  false
-  end
+  n.size == 2
 end
-
-#acquire range of numbers to int: (1..int/2).to_a // 6 -> [1,2,3]

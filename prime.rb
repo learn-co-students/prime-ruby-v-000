7e211e70a829271c53992b813f77b8fa80b3cf98
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2017-06-24 15:28:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+
+def prime?(num)
+  return false if num < 2 
+  return true if num == 3 || num == 2 
+  if (2..num-1).any? {|int| num % int == 0} 
+    false                                
+  else
+    true
+  end
+end

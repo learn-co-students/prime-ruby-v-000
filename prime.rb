78a8be765f678bd6a6e168fb3f8758e99c271cea
Jonathan Yi <jonathanyi81@gmail.com>
2017-06-26 00:10:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
+require 'pry'
 # Add  code here!
+def prime?(number)
+  count = 2
+  prime = TRUE
+  if number <= 1
+    return FALSE
+  elsif number == 2
+    return TRUE
+  else
+    while count < number
+      if number % count == 0
+        return FALSE
+      end
+      count += 1
+    end
+  end
+  return prime
+end

8d35fdee226303d94551c3cf2a8451c46547d322
Sohee Cho <soheecho94@gmail.com>
2017-06-29 05:41:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  if n == 2 || n == 3
+   return true
+  elsif n <= 1
+   return false
+  else
+   (2..n-1).each do |i|
+     if n % i == 0
+      return false
+     end
+   end
+  end
+  return true
+ end

b8ff9b3f134a13c51332b9a38b6b13893bae6bc9
Ron Morgan <ronm7581@gmail.com>
2017-06-30 20:47:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  end
+
+  i = 2
+  while i < int
+    if i == int
+      true
+    elsif int % i == 0
+      return false
+      break
+    else
+      i += 1
+    end
+  end 
+  true
+end

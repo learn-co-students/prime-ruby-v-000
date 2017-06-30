4f0f64ae31ec949ec90cbd757dbc0bf8876810de
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-06-30 13:41:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require "pry"
+
+def prime?(integer)
+  if integer <= 1
+    return false
+  end
+  n = 2
+  while n < integer
+    if integer % n == 0
+      return false
+    end
+    n = n + 1
+  end
+  true
+end

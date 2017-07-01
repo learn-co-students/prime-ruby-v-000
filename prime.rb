de6f1166b9f45826cc1e45e29b45422e4476c451
Sam Crowder <samecrowder@gmail.com>
2017-07-01 00:18:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+
+  i = 2
+  while i <= (number / 2)
+    if number % i == 0
+      return false
+    end
+    i += 1
+  end
+  true
+end

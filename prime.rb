1a37b26d8fafc4a4490d785a030f992bbaa0b639
Richard Steinbach <steinbach.dick@gmail.com>
2017-06-30 21:26:29 -0700

    prime numbers

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+
+	if num <= 1
+		return false
+	end
+	
+	i = 0
+	r = (2..num - 1)
+	arr = r.to_a
+	while i < arr.length
+	return false if num % arr[i] == 0
+		
+			i += 1
+		
+	end
+	return true
+end

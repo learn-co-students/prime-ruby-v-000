fc1b87c8afe5e2212f68c488e97dc5b106078c55
britt <britt.cumens@gmail.com>
2017-07-02 20:13:49 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(n)
+
+
+  prime_range = (2..Math.sqrt(n.abs)).to_a
+  is_prime = true
+
+  if n <= 1
+    is_prime = false
+  else
+    prime_range.each do |prime|
+      if n.abs % prime == 0
+        is_prime = false
+        break
+      end
+    end
+  end
+
+  is_prime
+end

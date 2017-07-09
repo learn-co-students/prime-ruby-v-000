1e008c6f9386ec8efd52b2bb837cb3c7e347faca
Yan Kozlovskiy <yankozlovskiy@icloud.com>
2017-07-09 21:10:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(prime_number)
+  if prime_number < 2
+    return false
+  else
+    range = Array(2...prime_number)
+    range.each do |n|
+      if prime_number % n == 0
+        return false
+      end
+    end
+  end
+  true
+end

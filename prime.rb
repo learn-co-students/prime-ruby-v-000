4413568d8b06441e1372cecbf2c1cf45e1555287
Femi F <ofpupp@princeton.edu>
2017-07-11 23:53:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  else
+    puts num
+    halfnum = num/2
+    puts halfnum
+    range = (2..halfnum).to_a
+    range.each do |pnumber|
+      if num % pnumber == 0
+        return false
+      end
+    end
+    return true
+  end
+end

c651ec08e956426dfbac01bf18d36347c4916d3b
Yumekia Ward <yumekiaw@yahoo.com>
2017-07-11 00:50:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+#Sieve of Eratosthenes
+def sieve(max)
+  primes = (0..max).to_a
+
+  primes[0] = primes[1] = nil
+
+  counter = 0
+  primes.each do |p|
+    next unless p
+
+    break if p*p > max
+    counter += 1
+
+    (p*p).step(max,p) {|m| primes[m] = nil}
+  end
+  primes.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
+end

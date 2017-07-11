cfc3faec3edc155fc8371656d12c1db0151f82f8
Daniel Karagiannis <dakaragiannis@gmail.com>
2017-07-11 00:10:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+require 'benchmark'
+def prime?(num)
+  return false if num <= 1 
+  (2..num-1).none? do |i|
+    num % i == 0
+  end
+end
+
+
+# return false if num <= 1
+# i = 2
+# if (num == 2 || num == 3 || num == 5)
+#   return true
+# end
+# while (i < num)
+#   if (num % i == 0)
+#     return false
+#   end
+#     i += 1
+# end

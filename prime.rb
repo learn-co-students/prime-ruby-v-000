393959282e062e1258ab6aa7c83eb15417b5d4d3
Elizabeth Kuok <elizabethkuok@gmail.com>
2017-07-12 21:56:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  end
+  n = 2
+  while n < integer
+    if integer % n == 0
+      return false
+    end
+    n = n + 1
+  end
+  true
+end

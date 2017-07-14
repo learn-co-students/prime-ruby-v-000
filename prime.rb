d7e780cf6bdcfbaeef69cfa082c267922619f09c
roitblatari <roitblatari@gmail.com>
2017-07-14 00:19:05 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+require "pry"
+
+
+def prime?(number)
+    i = 2
+    while i <= number 
+        
+        if  1..number %  i == 0  
+            return true
+         else 
+            true
+        end
+    i += 1 
+    end
+    false
+end
+
+
+

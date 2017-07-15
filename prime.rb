94036f2927affcb484824e811e5175fa217fae1e
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-07-15 23:17:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+# Add  code here!]
+def prime?(num)
+  c=2
+  if(num<=1)
+    return false
+  end
+  while (c<num)
+     if (num%c==0)
+       return false
+     end
+     c+=1
+  end
+   true
+end

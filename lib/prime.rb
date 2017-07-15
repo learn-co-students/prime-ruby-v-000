bbb4c967be8cb96f9a6ff5ea14a1e9c4f63c3b03
Steven Lennis <steven.lennis@icloud.com>
2017-07-15 15:03:24 -0500

    passed tests for non-prime numbers

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,36 +1,34 @@
-require "pry"
 def eratosthenes(prime)
   number = prime
   sqrt_num = Math.sqrt(number)
   sqrt_num = sqrt_num.round
   int_num = sqrt_num.to_i
   my_truth_array = Array.new(prime, true)
-  my_truth_array.insert(0, false)
-  my_truth_array.insert(1, false)
+  my_truth_array.shift
+  my_truth_array.unshift(false)
+  my_truth_array.unshift(false)
   counter = 2
-  binding.pry
   while counter <= int_num
     index = 2
     z = 0
-    binding.pry
     while index <= number
       index = counter * 2 + (z * counter)
       my_truth_array.insert(index, false)
       z += 1
-      #binding.pry
     end
     counter += 1
   end
-  my_truth_array.at(number += 2)
-  #binding.pry
+  my_truth_array.at(prime)
+  if my_truth_array.at(prime)
+    return true
+  end
 end
 def prime?(prime)
   num = prime
   if num.even? && num != 2 || num <= 1
-    #binding.pry
     return false
   elsif eratosthenes(num)
-    binding.pry
+    return true
   else
     false
   end
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -3,21 +3,18 @@ require_relative '../lib/prime.rb'
 
 describe "prime?" do
   it 'returns true for prime numbers' do
-    expect(prime?(13)).to be(true)
-    #expect(prime?(17)).to be(true)
-    #expect(prime?(19)).to be(true)
+    expect(prime?(3)).to be(true)
+    #expect(prime?(11)).to be(true)
+    #expect(prime?(105557)).to be(true)
   end
 
   it 'returns false for non-prime numbers' do
     expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
-    #expect(prime?(0)).to be(false)
-    #expect(prime?(1)).to be(false)
-    #expect(prime?(4)).to be(false)
-    #expect(prime?(40)).to be(false)
+    expect(prime?(0)).to be(false)
+    expect(prime?(1)).to be(false)
+    expect(prime?(4)).to be(false)
+    expect(prime?(40)).to be(false)
     #expect(prime?(1763)).to be(false)
     #expect(prime?(101013)).to be(false)
   end
 end
-
-#105557
-#40

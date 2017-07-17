b5ae6591e563561022d79e3fe39e950d5c4ebc58
Vinodh Swami <virtualviper786@gmail.com>
2017-07-17 00:54:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+require 'pry'
+def prime?(x)
+
+ if x < 2 || x % 2 == 0
+   return false
+ elsif x == 2 || x == 3
+   return true
+ else
+   max = (x**0.5).round
+   counter = 2
+   div = []
+   while counter < max
+     div << x % counter
+     counter += 1
+   end
+   if div.include?(0)
+     return false
+   else
+     return true
+   end
+  end
+ end

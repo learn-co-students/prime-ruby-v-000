2c715f7c6b7da389c1674f3d01669a145cd7b959
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-07-18 19:26:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+
+  if number <= 1
+    return false
+  end
+
+  counter = 2
+
+    while counter < number do
+      if number % counter == 0
+        return false
+      end
+      counter += 1
+    end
+  true
+end

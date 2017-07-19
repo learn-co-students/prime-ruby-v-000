3208de953191f398ff4720bae5337090567558a6
Joanna Sese <jmgsese@gmail.com>
2017-07-19 20:54:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,14 +1,6 @@
-# def prime?(integer)
-#   return false if integer <= 1
-#   divisors = (2..integer).to_a
-#   divisors.each {|divisor| return false if integer % divisor == 0 && integer != divisor}
-#   true
-# end
-
-def prime(int)
-  if int % 2 == 0
-    true
-  else
-    false
-  end
+def prime?(integer)
+  return false if integer <= 1
+  divisors = (2..integer).to_a
+  divisors.each {|divisor| return false if integer % divisor == 0 && integer != divisor}
+  true
 end

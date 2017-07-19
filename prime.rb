d1d2ad7d2d635b408ae2027edb2b02f63109ff04
Tyler Buchheim <buchheimt@yahoo.com>
2017-07-19 13:59:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,10 @@
 def prime?(n)
-  n > 1 ? (2..n ** 0.5).to_a.none? { |x| n % x == 0} : false
+  return true if n == 2 || n == 3
+  return false if n <= 1 || n % 2 == 0 || n % 3 == 0
+  i = 5
+  while i < n ** 0.5 do
+    return false if n % i == 0
+    i % 3 == 2 ? i += 2 : i += 4
+  end
+  true
 end

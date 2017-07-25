b18fd0ed7028d515d615851e465e6e7012ff86ab
polishpuff <karolina.kumiega@gmail.com>
2017-07-25 18:22:27 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+    c = 2
+    if integer <= 1 #negative integer check
+      return false
+    end
+    while (c < integer)
+      if (integer % c == 0)
+        return false
+      end
+    c += 1
+  end
+  true
+end

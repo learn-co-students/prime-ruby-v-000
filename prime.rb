d56c5a953eb94ebbd97e654b30c2c911d693bf9d
Donna <github email address>
2017-07-27 10:58:23 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+	n = n.abs
+	array = (2..n**0.5).to_a
+	
+	if n==0 || n==1
+		return false
+	elsif n==2 || n==3
+		return true
+	elsif array.any?{|i| n % i == 0}
+		return false
+	else
+		return true
+	end
+end

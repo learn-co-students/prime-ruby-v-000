fd44459a7c0ebae02f7bdda551e7eabe80b083ab
Yehuda Makarov <yehudamakarov@gmail.com>
2017-07-28 16:52:38 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry'
+def prime?(num)
+	if num < 2
+		return false
+	else
+		list = (2..num - 1).to_a
+		list.each do |number|
+			if num % number == 0
+				return false
+			end
+		end
+		true
+	end
+end
+
+
+
+ 

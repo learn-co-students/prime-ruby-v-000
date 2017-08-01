435dbf8afcefe54ea3b55ec7b7117635e67c4d58
Cody Kasselman <ckasselman@fordham.edu>
2017-08-01 17:08:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(n)
   if n <= 1
     false
   else n % 1 == 0
-    (2..(n/2).floor).to_a.none? do |factor|
-        n % factor == 0
+    (2..(n/2).floor).to_a.none? do |num|
+        n % num == 0
     end
   end
 end

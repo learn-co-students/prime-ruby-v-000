72b2566abe8d9f5989f8f2e890617b72c154b634
subash karki <skarki35205@hotmail.com>
2017-08-01 15:02:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
+
 # Add  code here!
+
+def prime? (num)
+  if num <=1
+    false
+  elsif num == 2
+    true
+  else
+    (2..num/2).none? { |i| num % i == 0}
+end
+end
+
+def prime?(num)
+  if num <= 1
+    false
+  elsif num == 2
+    true
+  else
+    (2..num/2).none? { |i| num % i == 0}
+  end
+endlearn 

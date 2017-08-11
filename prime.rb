93c630083e9334b9d2e5960b7b7b74696733148b
Sam Ojling <s.ojling@gmail.com>
2017-08-10 20:17:53 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,4 @@
-require 'pry'
-# Add  code here!
-
 def prime?(num)
-  if num > 1
-    (2...num).each { |n| return false if (num % n).zero? }
-    true
-  else
-    false
-  end
+  return false if num < 2
+  (2...num).none? { |n| (num % n).zero? }
 end

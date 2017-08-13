ced699234fadda850642fc052d5bdfd351b73193
Mary Benitez <meebenitez@gmail.com>
2017-08-13 04:45:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+  if integer == 2
+    return true
+  elsif integer > 2
+    a=*(2..integer-1)
+    a.all? {|num_check| integer % num_check != 0}
+  else
+    return false
+  end
+end

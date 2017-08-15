5b478bfa8da131b6eca37d13fda18407334d2120
Allen Anderson <dande313@byu.net>
2017-08-15 16:22:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+  if int <= 2
+    puts "#{int} is not a prime!"
+    return false
+  end
+  (2..Math.sqrt(int)).each do |i|
+    if int % i == 0
+      puts "#{int} is not a prime!"
+      return false
+    end
+  end
+  puts "#{int} is a prime!"
+  return true
+end
+
+prime?(-1)

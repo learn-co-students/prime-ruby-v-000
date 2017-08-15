176d346a170b5389283f53fdf5c70da7f91e80ca
kylek <kbelway29@gmail.com>
2017-08-15 10:54:23 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(n)
+  range = Array(1..n)
+  check = []
+  range.each do |x|
+    if n % x == 0
+      check << x
+    end
+  end
+  if check.size == 2
+    true
+  else
+    false
+  end
+end

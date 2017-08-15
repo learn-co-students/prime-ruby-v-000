7dc92e14da549753fda2b8943d901cd8c988043c
Dana ONeil <doneil1580@gmail.com>
2017-08-15 15:33:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <=3
+    return true
+  else (2..number/2).none? do |n|
+    number % n == 0
+  end
+end
+end

ab6cc8981d46cb22f86b324862477fbe80829bb9
ronald vilorio <soccer23415@gmail.com>
2017-08-17 16:35:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(int)
+  if(int <= 0 || int == 1)
+     return false
+  end
+  for num in 2..(int - 1)
+   if (int % num) == 0
+     return false
+   end
+  end
+  true
+ end

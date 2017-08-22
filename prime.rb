71e4530b327d948608896ef7238e3ad40d3df80e
Cecily Downs <cecily.downs@gmail.com>
2017-08-22 22:26:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require('pry')
+
+def prime?(number)
+  return false if number <= 1
+  i = 2
+  while i < number
+    if number % i == 0
+      return false
+    end
+    i += 1
+  end
+
+true
+end

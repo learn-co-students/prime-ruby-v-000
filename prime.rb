03f83c6741d3a243fe31f901e9b006063c84abdd
Sergio Correa <correa.sergio@gmail.com>
2017-08-22 04:36:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+# def prime?(num)
+#   if num > 1
+#     range  = (1..num)
+#     divisors = []
+#     range.each { |x| divisors << x if num % x == 0 }
+#     if divisors.length < 3
+#       true
+#     else
+#       false
+#     end
+#   else
+#     false
+#   end
+# end
+
+def prime?(num)
+  if num > 1
+    divisors = []
+    (1..num).each { |x| divisors << x if num % x == 0 }
+    if divisors.length < 3
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end

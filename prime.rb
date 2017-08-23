fe08e299b60c84109eff93174bd2f9a583bb12ee
Jameson Bass <jamesonbass@gmail.com>
2017-08-23 00:42:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(int)
+
+    if int < 2
+      false
+    elsif int == 2
+      true
+    else
+    i = 2
+    while i < int
+      if int % i == 0
+        return false
+      else
+        i += 1
+      end
+    end
+    true
+  end
+end

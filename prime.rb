23fbb34b36d9b9dec95332d0d30d8a7366504f86
Sofia Candiani <sofia.candiani123@gmail.com>
2017-08-24 21:28:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(n)
+  #takes care of negative numbers
+  if n < 2
+    return false
+  end
+  #a range of numbers to the squareroot of the int
+  (2..Math.sqrt(n)).each do |i|
+      if n % i == 0
+       return false
+      end
+  end
+  return true
+end

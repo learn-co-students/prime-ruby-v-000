eaa1f1593ed7c23da3798e44c8f3b71ad9b52824
lita stephenson <lita.stephenson@gmail.com>
2017-08-26 01:16:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  if num <= 0
+    return false
+  end
+
+  array = (1..num).to_a
+  multiples = []
+
+  array.each do |int|
+    if num % int == 0
+      multiples << int
+    end
+  end
+  multiples.length == 2
+end

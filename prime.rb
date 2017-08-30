6c18e2c598bbbc534bd2f5b770fb6ac9e213f552
Bret Burau <burhooo@gmail.com>
2017-08-30 10:36:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+require 'pry'
+def prime?(test_num)
+  if test_num > 0
+    array = (2...test_num).to_a
+  else array = (test_num...-2).to_a
+  end
+  # binding.pry
+  prime = false
+  array.each do |num|
+    if test_num % num == 0
+      prime = false
+      break
+    else prime = true
+    end
+  end
+  prime
+end
+
+ prime?(-1)

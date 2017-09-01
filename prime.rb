b9eb00a79f0cb36735c3cc42023c47e1117e3f7a
matos89 <matos89@gmail.com>
2017-08-31 21:08:31 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number == 3 || number == 5 || number == 7
+    true
+  elsif number <= 1
+    false
+  else
+    halved = number/2
+    array = (2..number-1).to_a
+      array.all? do |divided_by|
+      number % divided_by != 0
+    end
+  end
+end

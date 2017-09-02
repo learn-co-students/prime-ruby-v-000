6d31fc587b345676923056b34c20500b4be5ff34
Hunter Kleindl <hkleindl@gmail.com>
2017-09-02 18:14:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+  array = (2..(n-1)).to_a
+  if n == 2 || n == 3
+    true
+  elsif n < 2
+    false
+  elsif array.any? {|i| n % i == 0}
+    return false
+  else return true
+  end
+end

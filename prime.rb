56778b3c34a49fd8ed45198bb58d95140f586a3c
Robin Pahlman <robin.pahlman@gmail.com>
2017-09-02 15:09:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    puts "#{num} is not prime"
+    return false
+  end
+  b = num - 1
+  is_prime = true
+  while b > 1
+    if num % b == 0
+      is_prime = false
+    end
+    b -= 1
+  end
+    if is_prime == false
+      puts "#{num} is not prime"
+      return false
+    else
+      puts "#{num} is prime"
+      return true
+    end
+end

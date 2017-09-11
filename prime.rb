088fd48377ee347f70da8a8456e37711bc460bf3
Ally Kadel <akadel01@gmail.com>
2017-09-11 20:22:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
+
+
 # Add  code here!
+def prime?(integer)
+  numbers = (2..integer-1).to_a
+  if integer <= 1
+    return false
+  elsif integer.between?(1,3)
+    return true
+  elsif numbers.any?{|number| integer % number == 0}
+    return false
+  else
+    return true
+  end
+end
+
+
+#if number can not be divided by 2 or 3, or any prime number, then it is a prime number

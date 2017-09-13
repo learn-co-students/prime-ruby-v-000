9f95a70fa1ee9a47c859177ff76b11618774697d
Cristina Collazos <clcgalex@gmail.com>
2017-09-13 17:40:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+
+  array = (2..(number - 1)).to_a
+  if (array.any? {|int| number % int == 0} || number <= 0 || number == 1)
+    return false
+  else
+    return true
+  end
+end

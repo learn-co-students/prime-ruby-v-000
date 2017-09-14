24b24bbf77115e397596bd69d004f54b08d22589
turners <github email address>
2017-09-13 20:42:43 -0600

    devonjboen

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)# Add  code here!
+return false if num < 2
+return true if num == 2 || num == 3
+  true
+  top = Math.sqrt(num)
+  if (2..top).any? {|i| num % i == 0}
+    false
+  else
+    true
+end
+end

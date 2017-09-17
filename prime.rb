cd494c0027cd137d34c80731ab51ed704772ef2d
Dasha Nikitina <dashadariallc@gmail.com>
2017-09-17 07:33:26 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,10 @@
 # Add  code here!
-def prime(num)
+def prime?(integer)
+  return false if integer <= 1
+  return true if integer <= 3
+    if (2...integer-1).any?{ |i| integer % i == 0}
+      false
+    else
+      true
+    end
 end

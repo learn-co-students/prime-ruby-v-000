da5c26135b5ed54def982a5c7c8f430de8b3808b
mevanstron <michael@michaelgregoryevans.com>
2017-09-18 14:10:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+
+  if number == 2
+    true
+  elsif number > 2
+    one_to_number = (2..number - 1).to_a
+
+    one_to_number.all? do | factor |
+      number % factor != 0
+    end
+  else
+    false
+  end
+end

74072354a1d0fa57287fcf2257b2326fee00e1ac
Tony Distinti <adistinti@gmail.com>
2017-09-19 13:42:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,9 +2,15 @@
 
 require "pry"
 
+#This is what I came up with on my own.  
+
 def prime?(num)
-  list_array = (2..num-1).to_a
-  list_array.any? { |i| num / i % 0 }
+  if num <= 1
+    false
+  elsif num == 2 || num == 3
+    true
+  elsif num > 3
+    list_array = (2..num-1).to_a
+    list_array.none? { |i| num % i == 0 }
+  end
 end
-
-

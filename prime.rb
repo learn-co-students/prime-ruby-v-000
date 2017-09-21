2db614c847f907f68f9b7d86f300bfb074899fbd
Melissa Rudick <mel.madrugada@gmail.com>
2017-09-21 03:08:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+    if number < 2
+      false
+    elsif number == 2
+      true
+    else
+      #number = 50
+      #[2][25]
+      (2..number/2).none? {|num| number % num == 0}
+  end
+end

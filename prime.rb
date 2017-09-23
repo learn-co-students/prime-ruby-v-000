a09d05d6d2a29d435a47f0c3c1181538aa4fbf7d
Emily Zhao <tong.zhao.92@gmail.com>
2017-09-23 13:38:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+  if number <= 0 || number == 1
+    return false
+  else
+    divide_array = [*2..number-1]
+    divisible_array=[]
+    divide_array.each do |division|
+      if number % division == 0
+        divisible_array<<division
+      end
+    end
+    if divisible_array.length == 0
+      return true
+    else
+      return false
+    end
+  end
+end

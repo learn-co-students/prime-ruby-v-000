97273e1814c26cab1779bfd3431ff3f5d15e66ab
camneu37 <neuner.camille@gmail.com>
2017-09-23 22:42:00 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -3,10 +3,6 @@ require 'pry'
 def prime?(integer)
   range = (2..(integer-1)).to_a
   possible_prime = integer
-  if possible_prime <= 1
-    return false
-  end
-  range.all? do |e|
-    possible_prime % e != 0
-  end
+  return false if possible_prime <= 1
+  range.all?{|e| possible_prime % e != 0}
 end

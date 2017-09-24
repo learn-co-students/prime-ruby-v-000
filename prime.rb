7dd647c5c1cfccf99fe633d4a7b1dc4f4697929a
camneu37 <neuner.camille@gmail.com>
2017-09-23 22:35:11 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+  range = (2..(integer-1)).to_a
+  possible_prime = integer
+  if possible_prime <= 1
+    return false
+  end
+  range.all? do |e|
+    possible_prime % e != 0
+  end
+end

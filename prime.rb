0f9b31faa99cde09359e6f3cb84c8d96c0f187b1
John Tuttle <thejwt@gmail.com>
2017-09-26 04:16:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(test_num)
+  counter = test_num - 1
+  if test_num == -2 || test_num == -1 || test_num == 0 || test_num == 1 || test_num == 2
+    return false
+  end
+  while counter > 1
+    if test_num % counter == 0
+      return false
+    end
+    counter -= 1
+  end
+  true
+end

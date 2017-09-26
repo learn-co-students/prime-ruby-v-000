7a31450fba45b6eb6688d7dc9036e6fc922eae8d
Adam Fudge <coolfudge@gmail.com>
2017-09-26 00:04:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  elsif int == 2 || int == 3
+    return true
+  else
+    return prime_detection(int)
+  end
+end
+
+def prime_detection(int)
+  a = 2
+  while a < int
+    b = int / a
+    if a * b == int
+      return false
+    end
+    a += 1
+  end
+  return true
+end

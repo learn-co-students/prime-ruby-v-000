d91515db000ec8384ce0e3d769e35ba40b05fa6f
hailey endres <hailey.endres@gmail.com>
2017-09-27 17:46:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  if number == 2 || number == 3 || number == 5 || number == 7
+    return true
+  elsif number <= 1 || number.even?
+    return false
+  end
+
+  max = Math.sqrt(number).floor
+  possible_factors = (3..max).to_a
+
+  possible_factors.each do |i|
+    if number % i == 0
+      return false
+    end
+  end
+  true
+end

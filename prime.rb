77d8503298b9b92538ba7b37bdb5d3eea88e5ecc
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 12:26:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(integer)
+  array = (2..integer-1).to_a
+     if array.any? {|number| integer % number == 0 }
+       return false
+     elsif integer <= 1
+       return false
+     else
+       return true
+     end
+end

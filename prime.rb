0023f75ab2fc05d090400afc3c1846a026c18f75
Rony Boeck <ramg.9@live.com>
2017-09-29 05:09:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
+
 # Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number <= 1
+    false
+  else
+    begin_range = 2
+    end_range = number/2 + 1
+    factors = ((begin_range)..(end_range)).to_a
+    if number % 2 == 0
+      return false
+    else
+      prime = factors.any? do |factor|
+        number % factor == 0
+      end
+      !prime
+    end
+  end
+end
+
+puts prime?(3)

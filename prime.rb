d022d596a1871030c5211216c5b4ef647b203e8e
zolkocarbon <eathena@hotmail.com>
2017-09-30 18:10:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+  i = 2
+  n = number.abs #change negative values to positive
+  sqrtN = Math.sqrt(n)
+
+  return true if n == 2
+  return false if n == 0 || n == 1
+
+  while i <= sqrtN
+    return false if n % i == 0
+    i+=1
+  end
+
+  return true
+end
+
+# Source
+# http://www.stoimen.com/blog/2012/05/08/computer-algorithms-determine-if-a-number-is-prime/

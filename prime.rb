8af2dc9aa75ae55087fe5c0890b1c1dff37f344f
Michael Hirschberger <mlhirschberger@gmail.com>
2017-10-03 03:21:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(i)
+  if i < 2
+    return false
+  end
+  if i <= 3
+    return true
+  end
+  if i % 2 == 0 or i % 3 == 0
+    return false
+  end
+  n = 5
+  while n * n <= i
+    if i % n == 0 or i % (n + 2) == 0
+      return false
+    end
+    n += 6
+  end
+  true
+end

175c74e36733c093b8d7f8339ddc7bfd22e6c049
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-04 02:43:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  factors = []
+
+  (2..number).to_a.each do |n|
+    factors << n if number % n == 0 && number != n
+  end
+
+  if factors.length > 0
+    false
+  elsif number < 2
+    false
+  else
+    true
+  end
+end

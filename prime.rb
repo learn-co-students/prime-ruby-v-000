191f97c2bfa27a88e9f86d4c3bea2460eb2a96b3
Nathan  Visak <nathan.visak@gmail.com>
2017-10-07 20:08:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
 # Add  code here!
+#def prime?(number)
+#   if number > 2
+#     if number % 2 != 0
+#       true
+#     elsif ((number * 6) + 1) <= number ** 1/2
+#       true
+#     elsif ((number * 6) - 1) <= number ** 1/2
+#       true
+#     else
+#       false
+#     end
+#   else
+#     false
+#   end
+# end
+
+def prime?(number)
+  return false if number < 2 # guard clause (for special cases)
+  i = 2
+  while i < number
+    if number % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end

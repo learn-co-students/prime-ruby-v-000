70943ff5561c07b3a7e349d49bc2dc65d7c5ee30
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-11 02:14:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+  i = 2
+  if integer.to_i == 1 || integer.to_i == -1 || integer.to_i == 0
+    return false
+  else
+  while i < integer.to_i.abs
+    if integer.to_i % i == 0
+      return false
+    end
+    i += 1
+  end
+end
+  true
+end

36777e2cb108f53581384c3b588f2521bc438d06
Chrissa Constantine <chrissa1@mac.com>
2017-10-11 03:26:57 +0000

    finished

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+# Natural number greater than 1, no positive divisors other than 1 and itself. Modulo (% operator) is the remainder after division.
+def prime?(integer)
+
+  if integer <= 2 || integer.negative?
+    return false
+  end
+
+  counter = 2
+  while counter < integer
+    if integer % counter == 0
+      return false
+
+    end
+      counter += 1
+  end
+  return true
+end

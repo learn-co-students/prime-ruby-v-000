d120449644c0953327146753789d63a8cdb6d682
github username <wigmore3.14@gmail.com>
2017-10-13 09:32:03 -0700

    Primality test - Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'benchmark'
+
+def prime?(int)
+  n = int.to_i
+  num_list = (2...n).to_a
+  if n > 1 && num_list.any? { |x| n % x == 0 }
+    false
+  else
+    true
+  end
+end
+
+# Tests computation and outputs report
+puts Benchmark.measure { prime?(int) }

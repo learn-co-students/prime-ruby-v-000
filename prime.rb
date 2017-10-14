ce357fae294fa5cfec38644d3c9111ca32f758b8
Kayla Hansen <shiverr@gmail.com>
2017-10-14 01:34:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  array = (2..num-1).to_a
+if array.any? {|number| num % number == 0}
+  return false
+elsif num <= 1
+  return false
+else
+  return true
+end
+end

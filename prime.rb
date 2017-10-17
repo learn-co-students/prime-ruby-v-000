706f5e670687f00fa2674c60767d9ea4f77c7736
Kevin Huang <yellow.khaos@gmail.com>
2017-10-17 02:26:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  i = number -1
+  return false if i <=0
+  while i > 1
+    if number % i == 0
+      return false
+    end
+    i-=1
+  end
+  return true
+end

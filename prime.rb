9771d59a891e090938c0eea5ac4ef2b04a6a2a46
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-10-17 21:04:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(int)
+  i = 2
+  if int <= 2
+    return false
+  elsif int == 3
+    return true
+  else
+    denominators = []
+    while i * i <= int
+      denominators << i
+      i += 1
+    end
+    if denominators.any?{|n| int % n == 0}
+      return false
+    else
+      return true
+    end
+  end
+end

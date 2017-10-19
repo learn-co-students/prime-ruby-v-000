42f9f28a9eb0c60a16162005f81b744d7c38932f
Julianne Kim <juliannne@gmail.com>
2017-10-19 02:03:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  end
+  x = 2
+  while x < n
+    if n % x == 0
+      return false
+    end
+    x += 1
+  end
+  return true
+end

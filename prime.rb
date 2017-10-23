560419f94a8f961c6cf2e9ebd582d772e1c083e2
Thomas Sharkey <tbsharkey@gmail.com>
2017-10-23 23:04:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  upper_limit = integer / 2
+  (2..upper_limit).each do |i|
+    if integer % i == 0
+      return false
+    end
+  end
+  true
+end

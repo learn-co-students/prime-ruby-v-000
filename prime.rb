1e04669b4d9083f9282122afc0104787c0a3a689
Shibani Mookerjee <shibani.mookerjee@gmail.com>
2017-10-26 01:55:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+  #prime numbers are natural numbers greater than 1
+  if number > 1
+    num_array = (2...number).to_a
+    result = num_array.none? { |num| number % num  == 0 }
+    return result
+  else
+    false
+  end
+end

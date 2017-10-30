86fdbc56a82003709f255fbd2a2b0b03bf926d20
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-30 17:15:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number.abs == 1 || number.abs == 2 || number == 0
+    return false
+  end
+  (2..(number.abs - 1)).each do |num|
+    if(number.abs % num == 0)
+      return false
+    end
+  end
+  true
+end

6f4512f6b9d664fb6bb8f1810ca10e6f1623f376
JOSELY CASTRO <joselycas@gmail.com>
2017-11-01 14:48:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+require 'pry'
+def prime?(value)
+  numbers = (2..value-1).to_a
+  if value <= 1
+    false
+  else
+   numbers.none? do |i|
+   value % i ==  0
+  end
+  end
+end

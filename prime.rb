d0cbc670fbd6044b2d0ec4b27d1aea0930aeadbb
Jess Mitchell <jessicamitch42@gmail.com>
2017-11-01 19:38:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+  		return false
+  elsif number % 2 == 0
+    return false
+	end
+
+	i = 2
+	limit = number / i
+	while i < limit
+		if number % i == 0
+			return false
+		end
+		i += 1
+		limit = number / i
+	end
+	return true
+end

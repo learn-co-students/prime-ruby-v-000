3bf8018a768d0cee08f0b136d972ead7f59a056e
Blakeley Bond <burrick2003@yahoo.com>
2017-11-02 12:52:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,15 +1,16 @@
-def prime?(input)
+# V1.0 I know I missed the point of the assignment, just getting it to work.  Generating an input range seems pointless for this and would add overhead.
+def prime?(input) #integer prime test excluding even numbers, up to the sqrt, with no math calls.  Takes 2 secs @ quadrillion on an i7.
 test_array = []
   if input == 2
     true
   elsif input <= 1 || input % 2 == 0
     false
-  elsif input <= 3
-    true
+  #elsif input <= 3
+  #  true
   else
     counter = 3
 
-      while (counter**2) <= input
+      while (counter**2) <= input #works de facto for 5 and 7 as not run (no 1, 2, test, with everything below 9 already excluded)
 
         if (input % counter) == 0
           test_array << counter

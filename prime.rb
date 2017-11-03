6db24b344d4cb1b76904bbdd9b55a02b34cf3711
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-03 16:39:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+require 'pry'
+
+def prime?(num)
+  hold = []
+  if num > 1
+    check = Array (2..num-1)
+    check.each() {|array_num| hold << false if num % array_num == 0}
+  else
+    return false
+  end
+  hold.include?(false) ? false : true
+end

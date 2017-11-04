d85fedcf8c701e477dc05c0a4d31de97e27c37c6
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2017-11-04 20:54:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(n)
+  return false if n <= 1
+  return true if n <= 3
+  i = 2
+  while i < n
+    if n % i == 0
+      return false
+    else
+      i += 1
+    end
+  end
+  return true
+end

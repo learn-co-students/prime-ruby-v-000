4fc2e42ada9c6bfc9ac58505a9a4123d6b68d2bc
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-08 20:03:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+#definition of prime number:
+#an integer greater than one, if its only positive divsors are one and itself
+def prime?(integer)
+
+  primeFlag = true
+  max = integer - 1
+
+  if [2,3].include?(integer) then
+      return primeFlag
+  elsif integer <= 0 || integer == 1
+      return false
+  else
+      (2..max).each do |denom|
+        primeFlag = false if integer % denom == 0
+      end
+      return primeFlag
+  end
+
+end

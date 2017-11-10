13b3380bbec1b6cdadcf6473550a682bf12ff646
Molly <mollykathrynnelson@gmail.com>
2017-11-10 02:07:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+# Add  code here
+def prime?(number)
+  return false if number < 2 || number.even?
+
+  range = (2..number-1).to_a
+
+  range.each do |digit|
+    if number%digit == 0
+      return false
+    end
+  end
+  return true
+end

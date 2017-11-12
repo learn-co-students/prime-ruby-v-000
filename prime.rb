6aa1895db9b20baf0a8ff6e063646b7fe5f7b5ef
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 17:03:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+def prime?(int)
+  if int == 1 || int == 0 || int == -1
+    false
+  else
+    Math.sqrt(int).to_i.downto(2).each {|i| return false if int % i == 0}
+    true
+  end
+end

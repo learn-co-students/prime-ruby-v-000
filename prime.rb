bb8c2141ebcfb0ac667a4783c136e27b08c47fa2
Richard Schaubeck <richard.schaubeck@gmail.com>
2017-11-13 22:41:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  range_array = (2...num).to_a
+  if range_array.any?{|i| num % i == 0}
+    return false
+  elsif num <= 1
+    return false
+  else
+    true
+  end
+end

c852e2564296c7cb59378c1798093048ba466e95
Connor Gallopo <connor.gallopo@me.com>
2017-11-14 21:10:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+def prime?(max)
+  if max <= 1
+    return false
+  elsif
+    prime = (2..max-1).to_a
+
+    prime.each do |i|
+      x = max % i
+      if x == 0
+        return false
+      end
+    end
+  end
+  true
+end

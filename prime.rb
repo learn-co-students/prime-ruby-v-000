6384d64e13ea035322eeb5143e22fe8b397aadac
Bryan Hooper <bryan@lindsayfarm.net>
2017-11-14 09:15:28 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+#
+#
+#
+require 'pry'
+
+def prime?(integer)
+  if integer <= 1 
+    return false
+  end
+  top = integer - 1
+  needed_range = (2..top)
+  needed_nums = needed_range.to_a
+  needed_nums.each do |num|
+    if integer % num == 0 
+      return false 
+    end
+  end
+  true
+end
+
+prime?(-1)
+

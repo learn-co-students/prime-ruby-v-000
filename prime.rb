d9343d76da1d00b9f1c5d17123974c3ada38c1c1
John Wojciechowski <jwojo13@gmail.com>
2017-11-15 19:54:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(x)
+potential_factors=*(2..1000000)
+actual_factors = potential_factors.select {|i| x % i == 0}
+if actual_factors.count == 1
+  return true
+else
+  return false
+end
+end

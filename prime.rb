8c49d1847b7aef5df4e67aa186e1cf15cd0e3ea4
cboujoukos <cboujoukos@gmail.com>
2017-11-20 13:51:35 -0500

    add comment

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
 def prime?(num)
-  array = (2..num/2).to_a
+  array = (2..num/2).to_a #dividing num by 2 because after the halfway point we arebasically redoing numbers we already checked
   (array.none? {|n| num % n == 0 }) && num > 1
 end

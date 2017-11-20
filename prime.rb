2e50dd5a4e02c8126f597c63272db459ba81af0f
Amanda White <emmalinerosewhite@gmail.com>
2017-11-20 14:44:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  if  number <= 1
+    return false
+  elsif (2..number/2).none? { | i | number % i == 0 }
+    return true
+  else
+    return false
+  end
+end

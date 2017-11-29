e4576d2ef6b027bce72b15c67a88ec77939f2977
Annette <micha232@umn.edu>
2017-11-29 19:20:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,29 +2,36 @@
 require 'benchmark'
 require 'bigdecimal/math'
 
+
 def prime?(int)
-   if (int % 2 == 0 || int < 2)
-       return false
+  if (int < 2)
+      return false
     else
-        squareroot = unsquare(int, (int/2))
-        numbarray = []
-        i = 3
-        while i < squareroot
-            numbarray << i
-            i += 1
-        end
+        squareroot = unsquare(int) #sqrt gives lowest number to reduce potential factors
+        numbarray = (2..squareroot)
         !numbarray.any? {|number| int % number == 0} 
-   end
+  end
+end
+
+# until the previous sqrt estimate matches the new estimate redo.
+# sqrt via babylonian method
+
+def unsquare(prime, new=prime, prev=prime/2)
+    new/2 == 0? n=1: n=new/2
+    new = (n + (prime/n))/2
+    if new == prev
+        return new
+    else unsquare(prime, prev, new)
+    end
 end
 
 
+##old inefficient sqrt function
 
-def unsquare(int, seed)
-    
-    seed -= 1 until (seed*seed <= int)
-	return seed + 1
-	
-end
+# def unsquare(int, seed)
+#     seed -= 1 until (seed*seed <= int)
+# 	return seed + 1
+# end
 
 
 

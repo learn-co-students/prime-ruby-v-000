534633a3ce53b0d89ed21356251b10d629178f10
Brittany Morris <brittmmendez@gmail.com>
2017-12-04 15:42:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+     if num<=1
+       return false
+
+     elsif num.even?
+       return false
+
+     elsif num>1 and num.odd?
+       counter = (num-1)
+       until counter ==2
+         if num % counter==0
+           return false
+          end
+           counter=counter- 1
+        end
+
+        return true
+    end
+  end

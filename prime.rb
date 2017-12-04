948b91e0c70536a741562a72a8a7570a7f1c0b12
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-04 21:49:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,17 +1,9 @@
 require "pry"
 
 def prime?(number)
-  array = (2..number-1).to_a #array ranging from 2 to number -1 (since 1 and the number itself dont matter in checking for falsehoods)
-  prime = true
-  if number <= 1 || number == -number
-    prime = false
-  else array.each do |n| #iterate through each number in the array
-    if number % n == 0
-      prime = false
-    else
-      prime = true
-    end
+  if number < 2
+    false
+  else
+  (2...number).none? {|i| number % i == 0}
   end
-end
-  prime
 end

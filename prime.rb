84f7074389b4864800fbf7f96e5da9732e0e03a5
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-04 21:30:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def prime?(number)
-  array = (2..number-1).to_a #array ranging from 2 to number
+  array = (2..number-1).to_a #array ranging from 2 to number -1 (since 1 and the number itself dont matter in checking for falsehoods)
   prime = true
   if number <= 1 || number == -number
     prime = false
@@ -9,7 +9,7 @@ def prime?(number)
     if number % n == 0
       prime = false
     else
-      prime == true
+      prime = true
     end
   end
 end

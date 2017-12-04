84d6d6de276e30c5b4c033efa35ffccc9725c239
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-04 21:27:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require "pry"
+
+def prime?(number)
+  array = (2..number-1).to_a #array ranging from 2 to number
+  prime = true
+  if number <= 1 || number == -number
+    prime = false
+  else array.each do |n| #iterate through each number in the array
+    if number % n == 0
+      prime = false
+    else
+      prime == true
+    end
+  end
+end
+  prime
+end

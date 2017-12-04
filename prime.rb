d17a4d68290055fb6da92a5fc885ae9ecdec3e14
Evan  Markert <evanmarkert@gmail.com>
2017-12-04 22:14:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    false
+  elsif (2..num/2).any? {|i| num % i == 0}
+    false
+  elsif (2..num/2).none? {|i| num % i == 0}
+    true
+  end
+end

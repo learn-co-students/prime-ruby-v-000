00ac395561fde3247937362f55fc80aaaf7a23da
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-04 21:51:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 require "pry"
 
 def prime?(number)
+  array = (2...number).to_a
   if number < 2
     false
   else
-  (2...number).none? {|i| number % i == 0}
+  array.none? {|i| number % i == 0}
   end
 end

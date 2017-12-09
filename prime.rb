5b8f66668db81908796e5ced7ca879110b631558
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-09 17:54:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+# def prime?(int)
+# end
+
+def first_n_primes(n)
+  prime_array = []
+    if prime = Prime.new
+      n.times do
+        prime_array.push(prime.next)
+      end
+  return true
+  else
+  return false
+end

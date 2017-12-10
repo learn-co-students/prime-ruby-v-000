109f979e35ed1c8d277966791e29b65c347babee
Andy Maskiell <maskiella@gmail.com>
2017-12-10 18:26:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,18 @@
 # Add  code here!
 def prime?(integer)
-  x = 1
-  until x > integer
-    if ((integer % x) >= 1) && ((integer / x) == 1)
-      true
-    elsif ((integer % x) == 0) || ((integer / x) > 1)
-      false
-    else
-      x += 1
+  x = 2
+  if integer >= 2
+    until x > integer
+      if ((integer % x) == 0) && ((integer / x) > 1)
+       return false
+      # elsif ((integer % x) == 0) || ((integer / x) > 1)
+      #   false
+      else
+        x += 1
+      end
     end
+    return true
+  else
+    return false
   end
 end

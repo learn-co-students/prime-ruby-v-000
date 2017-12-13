b6475ba2b29f68f0f60cfe6b60d8a2a4c8731e92
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-13 20:25:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  is_prime = true
+  if number < 2
+    is_prime = false
+  else
+    for i in 2..(number-1)
+      if number % i == 0
+        is_prime = false
+      end
+    end
+  end
+  is_prime
+end

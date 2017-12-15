8ea1e611cab08b6c019335d49b75eec6e15d167f
Lena Sawyer <lenasawyer93@gmail.com>
2017-12-15 16:52:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+#def prime?(i)
+#  small_numbers = [2..i-1]
+#  large_numbers = [i+1..20,000]
+#  if i <= 0 || i == 1
+#    false
+#  elsif i / i == 0 && i / 1 == i && i % small_numbers != 0 && i % large_numbers != 0
+#    true
+#  else
+#    false
+#  end
+#end
+
+def prime?(i)
+  numbers = (2..i-1)
+  list_numbers = numbers.to_a
+  if i == 2
+    true
+  elsif i <= 1 || i.even?
+    false
+  elsif i > 0 && i != 1 && list_numbers.all? {|n| i % n != 0}
+    true
+  else
+    false
+  end
+end

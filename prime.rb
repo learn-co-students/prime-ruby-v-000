fd8b4db394de5987d402834bd0bcc4bf2edb5dd9
Judy McGoldrick <jjeanius@verizon.net>
2017-12-17 04:34:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+require 'prime'
+require 'pry'
+def prime?(integer)
+  if integer < 2 || integer < 0
+    return false
+  end
+  range = (1..integer).to_a
+  even_div = []
+     range.each do |number|
+    if integer % number == 0
+      even_div << number
+      binding.pry
+    end
+  end
+  if even_div.count == 2
+    return true
+  else
+    false
+  end
+end

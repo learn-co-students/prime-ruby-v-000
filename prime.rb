2a7cca7b703293afed4d6f017fac15c157b42b2e
github username <github email address>
2017-12-16 19:11:19 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def function is_prime(n)
+    if n ≤ 1
+       return false
+    elsif n ≤ 3
+       return true
+    elsif n % 2 = 0 or n % 3 = 0
+       return false
+     i = 5
+    while i * i ≤ n
+       if n % i = 0 or n % (i + 2) = 0
+           return false
+       i = i + 6
+    return true
+  end
+end

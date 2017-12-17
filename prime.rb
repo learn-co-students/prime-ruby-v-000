8583bb8b5beb98be5cf00c08512de1660478986c
github username <github email address>
2017-12-16 19:26:48 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,8 @@
 # Add  code here!
-def function is_prime(n)
-    if n ≤ 1
-       return false
-    elsif n ≤ 3
-       return true
-    elsif n % 2 = 0 or n % 3 = 0
-       return false
-     i = 5
-    while i * i ≤ n
-       if n % i = 0 or n % (i + 2) = 0
-           return false
-       i = i + 6
-    return true
+def prime?(n)
+  if n <= 1
+      false
+  else
+      (2..n/2).none? { |i| n % i == 0}
   end
 end

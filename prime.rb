05553fb8a1c713dd2ff26ac4dda94cfe4c15a02e
DanL12186 <danlrcn@hotmail.com>
2017-12-17 23:34:41 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,12 @@
 def prime?(num)
   if num < 2
     return false
-
   else
-    for i in (2..(num/2))
+    for i in (2..(num**0.5))
       if num%i == 0
         return false
       end
     end
-      return true
+    return true
   end
 end

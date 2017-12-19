d6fc906b9c741635b7672ddc1f71546b344f6263
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 22:57:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+
+
+def prime?(given)
+  prime = []
+  1.upto(given){|number| prime << number if (given % number == 0)}
+    if given <= 1
+      return false
+    elsif prime.count > 2
+      return false
+   else
+     return true
+   end
+ end

26120613db7dda11f19a71604ccde97475e598f1
mnimmz <morgannimmons@gmail.com>
2017-12-19 18:17:09 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+def number(max)
+
+  primes = (0..max).to_a
+
+  primes[0] = primes[1] = nil
+
+  counter = 0
+  primes.each do |pnum|
+    next unless pnum
+
+    break if pnum*pnum > max
+    counter += 1
+
+    (pnum*pnum).step(max,pnum) { |m| primes[m] = nil }
+  end
+
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
+end
+
+def prime?(num)
+  number(num).include?(num)
+end
+
+puts prime?(105557)

7bc88af8062a442581bbc5481e4ae900379d622a
David Towster <djt4xe@virginia.edu>
2017-12-20 03:26:44 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'pry'
+
+def prime?(int)
+    if int < 2
+        return false
+    else 
+        range = (2..int-1).to_a
+        range.each do |div|
+            if (int % div) == 0
+                return false
+            end
+        end
+    end
+    true
+end

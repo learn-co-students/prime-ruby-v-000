71568e0f1ef8d711482a9ebe66b5e3fbd956255e
kostanzhoglo <jeffreywithers@gmail.com>
2017-12-19 21:52:09 -0500

    Create prime number method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,40 @@
 # Add  code here!
+def prime?(number)
+  array = (2..number-1).to_a
+  i = 1 # 2, 3
+  if number <= 1
+    return false
+  end
+  # while i <= array.length
+    array.each do |num| # 2
+      # num % i
+      if number % num == 0
+        return false
+      end
+      i += 1
+    end
+    true
+  # end
+end
+
+
+# def filter(max_numb)
+#   special = (0..max_numb).to_a
+#   special[0] = nil
+#   special[1] = nil
+#
+#   counter = 2
+#   special.each do |num|
+#     (num).step(num) do |i|
+#       special[i] = nil
+#     end
+#     break if (num * num) > max_numb
+#     counter += 1
+#   end
+#   special
+# end
+#
+#
+# def prime?(number)
+#   filter(number).include?(number)
+# end

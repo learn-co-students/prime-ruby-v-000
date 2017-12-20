83c3836bff003160c934ba0e6991b80c47e875d0
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-20 14:08:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,19 @@
-# Add  code here!
-=begin
-accept an integer as an argument
-check if prime
-  not prime if integer/any other number % == 0
-  prime if integer/any other number (exept 1 and integer) & != 0
+require 'pry'
 
-
-=end
-def prime?(integer)
-  last = integer - 1
-  divisors = [2..last].to_a
+def prime?(num)
+    divisors = [2..num].to_a
+    puts divisors
+    binding.pry
     divisors.none? do |divisor|
-      integer % divisor == 0
+      boo % divisor == 0
     end
   end
+
+
+  # Add  code here!
+=begin
+  accept an integer as an argument
+  check if prime
+    not prime if integer/any other number % == 0
+    prime if integer/any other number (exept 1 and integer) & != 0
+=end

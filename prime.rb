6956959f26c641aadc5e2af92f323bb99453adf8
Laura <laurao148@gmail.com>
2017-12-22 05:08:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer)
+  array = []
+  array.push(integer)
+  array.each do |integer|
+    if integer <= 1
+      return false
+    elsif (integer == 2) || (integer == 3) || (integer == 5) || (integer == 7)
+      return true
+    elsif (integer % 2 != 0) && (integer % 3 != 0) && (integer % 5 != 0) && (integer % 7 != 0)
+      return true
+    else
+      return false
+    end
+  end
+end

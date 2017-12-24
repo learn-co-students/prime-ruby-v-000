3e1b3819e170433dc8621a189f2b59fca4df6e56
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-24 17:28:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,5 @@
-# Add  code here!
-def prime?(n)
-  if n <=1
-    return false
-  end
-  Math.sqrt(n).floor.downto(2).each {|i| return false if n % i == 0}
+def prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
   true
 end

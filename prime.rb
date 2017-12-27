4c69b36db1bc71ab3f6592aab27b997acd174483
Ron Sala <ron.sala@gmail.com>
2017-12-27 21:01:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'benchmark'
+require 'bigdecimal/math'
+
+def prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+end
+#
+# def prime?(num) #failing
+#    Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0 || num < 0 }
+#    return false if num % i == 0 || num < 0
+# end

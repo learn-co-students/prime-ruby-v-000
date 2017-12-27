3a6015c9c259ecc1322cca3046c626a4f0a35663
EPIC448 <samrey2018@gmail.com>
2017-12-27 00:21:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'pry'
+def prime?(n)
+
+if (n < 0)
+  (3..n - 1).to_a.none? do |numb|
+  #  How do you turn a range into an array
+  #   so that it can be iterated over?
+    if numb == n
+        n % numb == 0
+    # we divide n by numb
+  end
+ end
+  false
+# binding.pry
+end

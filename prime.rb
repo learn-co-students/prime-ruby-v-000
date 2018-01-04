8ec0b381b86c8393aaa6d05e157ab2dbde4d4db3
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-04 14:28:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number > 1
+    for i in 2..(number)
+      if (number % i) == 0 && i < number
+         return false
+      elsif (number % i) == 0 && i == number
+        return true
+      else
+        i += 1
+      end
+    end
+  else
+    return false
+  end
+end
+
+prime?(1763)

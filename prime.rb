f0d27ea9d387bc0b04ae36a66ac15db4851b37f8
Brad Hambrice <hambrice@uab.edu>
2018-01-05 06:23:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+    end 
+    array = (2..integer).to_a
+    array.pop
+    if array.any? do |number|
+    integer % number == 0
+  end
+    return false
+  else 
+    return true 
+  end
+end
+   
+   

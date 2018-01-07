b5b006cc1db424cf179b8dc0d36f3da86d5853b1
Helen Du <helendu.614@gmail.com>
2018-01-07 23:04:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  else
+    number = *(2..num-1).to_a
+    number.each do |i|
+      return false if num % i == 0
+    end
+  end
+  return true
+end

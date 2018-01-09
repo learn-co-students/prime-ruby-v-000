6df6b532b646d19cd60f2850d5e5da8627d0ef11
bethurban <beth.urban@gmail.com>
2018-01-09 14:57:38 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+  if integer <= 1
+    return false
+  elsif integer == 2
+    return true
+  elsif integer == 3
+    return true
+  elsif integer % 2 == 0
+    return false
+  elsif integer % 3 == 0
+    return false
+  end
+
+  i = 5
+  w = 2
+
+  while i * i <= integer
+    if integer % i == 0
+      return false
+    else
+      i += w
+      w = 6 - w
+    end
+  end
+
+return true
+
+end

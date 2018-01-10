4313336bb4c95c83d2972c5d19e5dba69b02fbb0
Berkeley Fahrenthold <berkeley_fahrenthold@urmc.rochester.edu>
2018-01-10 22:01:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+#not divisible by anything but itself and 1
+# is not negative numbers, 0, 1
+
+def prime?(num)
+  if num <= 1
+    return false
+  end
+
+  for n in 2..(num - 1)
+    if num % n == 0
+      return false
+  end
+end
+
+  return true
+end

65f71111ab8666fa2af7f9804d3644ca4060b3dc
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-11 01:17:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require 'pry'
+
+def prime?(number)
+  #check for 0, 1 or negative numbers can't be prime
+  #binding.pry
+  if number <= 1
+    return false
+  end
+  (2..number-1).each do |n|
+    return false if number % n == 0
+  end
+  return true
+end

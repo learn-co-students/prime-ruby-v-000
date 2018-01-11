f53a8820dd41d81bb5886f393a31deeeb4208863
Kriti Rai <kriti.rai@gmail.com>
2018-01-11 01:44:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+	array = (2..num-1).to_a
+  new_array =[]
+	if num==2
+		return true
+	elsif num<2
+		return false
+	elsif array.each do |i|
+		new_array<<(num % i == 0)
+    end
+		if new_array.include?(true)
+			return false
+		else
+		  return true
+		end
+	end
+end

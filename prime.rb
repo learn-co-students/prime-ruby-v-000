813eed58f720b6d4075a7db74d5369b7d490d08d
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-01-11 19:12:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+require'pry'
+
+def prime?(number)
+  range= (2...number).to_a
+  answer = range.map {|d| (number % d == 0)}
+  if answer.include?(true)||answer==[]
+    return false
+  else
+    return true
+  end
+end

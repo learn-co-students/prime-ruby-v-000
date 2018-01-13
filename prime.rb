bc0edcb9761b602fa2221839332afade0b730422
Ingrid Wong <yw340@georgetown.edu>
2018-01-13 22:38:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+
+def prime?(num)
+ if num > 0
+  possible_factors = (2..num-1).to_a
+else
+  small=num+1
+  #puts small
+  #possible_factors = ((-(num+1))..-1).to_a
+  possible_factors= (small..-2).to_a
+end
+  possible_factors.each do |factor|
+    if num % factor == 0
+      return false
+    end
+  end
+  
+  return true
+end
+
+prime?(-9)

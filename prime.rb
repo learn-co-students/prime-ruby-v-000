e8d2230f798953102e49278cd821beaa3066644e
jljardon <github email address>
2018-01-14 01:38:15 -0500

    add algorithm to check if a number is prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require 'pry'
+def prime?(num)
+   if num <= 1
+      return false
+   elsif num <= 3
+      return true
+   elsif num.even? || num % 3 == 0
+      return false
+   end
+
+   i = 5
+   while i * i <= num
+      return false if num % i == 0 || num % (i + 2) == 0
+      i += 6
+   end
+   return true
+end

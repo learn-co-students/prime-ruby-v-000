b4585785a7d31d2fe455fbdbc79e24fe32862ede
Autumn J <autumnkangas@gmail.com>
2018-01-16 17:52:52 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(i)
+  return false if i <= 1 || i.even?
+  search_array = [*3..(i/2).ceil]
+    search_array.each do |divisor|
+      if i % divisor == 0
+        return false 
+      end
+    end
+    return true
+end
+

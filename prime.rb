8b62a899ac9292f9d6d08772015c0fb80f09af20
Sean Clifford <sclifford6262@gmail.com>
2018-01-16 02:48:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  if num > 1 || num < -1
+    i = 2
+    potential_divisors = []
+    while i < num.abs
+      potential_divisors << i
+      i += 1
+    end
+    potential_divisors.none? {|divisor| num % divisor == 0}
+  else false 
+  end
+end

ebde91fa7989057ccaaf241ae621c6594dbc0dbe
Jonathan Dust <d24dust@hotmail.com>
2018-01-17 15:43:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(num)
+  range = (1...num + 1)
+  divisors = []
+  return false if num < 2
+  range.each do |i|
+    divisors << i if num % i == 0
+  end
+  return divisors.all? { |i| i == 1 || i == num }
+end

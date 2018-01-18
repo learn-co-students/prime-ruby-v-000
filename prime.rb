bc04b5b12296cd662de801b2030970b85fb544a5
jpkim921 <jpkim921@gmail.com>
2018-01-18 06:13:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
+require 'benchmark'
+
+# benchmark time = 0.000000   0.000000   0.000000 (  0.000004)
 def prime?(number)
   return false if number <= 1
 
   i = 2
-  while i < number
+  while i < number/2
     if (number % i) == 0
       return false
     end
@@ -11,3 +14,14 @@ def prime?(number)
 
   return true
 end
+
+
+# benchmark time = 0.000000   0.000000   0.000000 (  0.000017)
+# def prime?(number)
+#   return false if number <= 1
+#   # return true if number / 2 == 1
+#
+#   !(2..number/2).any? {|x| number % x == 0}
+# end
+
+puts Benchmark.measure{prime?(7)}

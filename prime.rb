c70fe2c685ee975d8984cd54733e61e30742fffd
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-01-19 11:45:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1 
+    return false
+  end 
+  
+  for i in 2..(number -1) do 
+    if number % i == 0 
+      return false     
+    end 
+  end 
+  true 
+end

cf8ad4c5f7f19db4c5e79095622966a524c01dea
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 23:45:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(a)
+  if a <= 1 || a % 2 == 0
+    false
+  else
+    (3..a-1).to_a.each do |i|
+      if a % i == 0
+        return false
+      end
+    end
+    true
+  end
+end

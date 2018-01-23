be45be1628f6b83aae6d4e2202300b8edaf1b812
github username <github email address>
2018-01-22 19:33:41 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  end
+  testarray = (2..Math.sqrt(n)).to_a
+  testarray.each do |m|
+    if n % m == 0
+      return false
+    end
+  end
+   return true
+end
+
+
+# The simplest primality test is trial division: Given an input number n, check whether any prime integer m from 2 to √n evenly divides n (the division leaves no remainder). If n is divisible by any m then n is composite, otherwise it is prime.[1]

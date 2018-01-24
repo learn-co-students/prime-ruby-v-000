255a9bcce36ce9e2780595c2da1e70392261d25d
Kyle Horsch <krhorsch@yahoo.com>
2018-01-24 16:33:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+def prime?(num)
+  if num < 2
+    return false
+  end
+  range = (2..(num-1))
+  range.each do |integer|
+    result = num.to_f/integer.to_f
+    if result == result.floor
+      return false
+    end
+  end
+  return true
+end

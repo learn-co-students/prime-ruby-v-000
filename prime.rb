5355fa7ba574d915f4c9ef46f7f7e62bc0937dd7
Brennan Heisler <learn@heislercreative.com>
2018-01-25 03:53:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+
+  i = 0
+  while i < integer.length
+    if integer[i] % 2 == 0 && array[i] != 2
+      false
+    elsif integer[i] % 3 == 0 && array[i] != 3
+      false
+    elsif integer[i] % 5 == 0 && array[i] != 5
+      false
+    else
+      true
+    end
+    i += 1
+  end
+end

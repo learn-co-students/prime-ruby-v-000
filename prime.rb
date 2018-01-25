e32f97a80ae45ab087dcf71444c9ff8f47b3c84b
Stephen Thurman <thurman024@gmail.com>
2018-01-25 17:12:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number <= 1
+    false
+  else
+
+    list_of_num = *(2..number/2)
+    divisor = list_of_num.detect {|x| number % x == 0}
+    #binding.pry
+    if divisor
+      false
+    else
+      true
+    end
+  end
+end
+
+# puts prime?(5)
+# puts prime?(8)

7c0b12497c5de41959c211fb2e20c02b58d06135
Kim Gonzales <kimgo589@gmail.com>
2018-01-26 01:38:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1
+   return false
+  end
+  (2...integer).each do |num|
+     if integer % num == 0
+      return false
+     end
+   end
+   true
+ end

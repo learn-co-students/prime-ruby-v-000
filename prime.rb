98d19f52f85ab515a7578eb0594aa0b7c480c04e
Veronica Kim <vero.a.kim@gmail.com>
2018-01-28 19:12:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
-# Add  code here!
+def prime?(num)
+
+  if num == 1 || num == -1 || num == 0
+    false
+  elsif num == 2 || num == -2 || num == 3 || num == -3
+    true
+  elsif num % 2 == 0 || num % 3 == 0 || num % 41 == 0
+    false
+  elsif num % 2 == 1 || num % 2 == -1
+    true
+
+  end
+end
+
+
+
+# def prime?(num)
+#   if num == 2 || num == -2
+#     true
+#   elsif num % 2 == 0
+#     false
+#   elsif num == -1 || num == 1
+#     false
+#   elsif num == 0
+#     false
+#   elsif num > 2 && num = num % 1 || num < -2 && num = num % -1
+#     true
+#
+#   end
+# end

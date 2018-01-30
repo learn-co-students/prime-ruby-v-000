79d4ba919bb35a6d74403c972a52bb3a70b96810
Eden Mesfun <emesfun@gmail.com>
2018-01-30 20:11:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+test_range = [-1, 0, 1, 4, 40, 1763, 101013, 2, 3, 11, 105557]
+
+def prime?(num)
+  end_num = num - 1
+  range = (2..end_num).to_a
+    if num <= 0 || num == 1
+      return false
+    end
+  range.all? do |numbers|
+    num % numbers != 0
+  end #all
+end #method

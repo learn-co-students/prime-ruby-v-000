a84412155b44b1f16db6689a5dd43f77738ed399
Erin Walker <erinb0317@hotmail.com>
2018-01-30 01:02:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
+require 'pry'
 # Add  code here!
+
+def prime?(int)
+  if int <= 1
+    false
+  else
+
+    list_of_num = *(2..int/2)
+    divisor = list_of_num.detect {|x| int % x == 0}
+
+    if divisor
+      false
+    else
+      true
+    end
+  end
+end

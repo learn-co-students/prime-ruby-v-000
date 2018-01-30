4c83a1dbfbb16c10cb62d95f50ffd6fb5731b36d
Dee Race <hey@deeraceart.com>
2018-01-30 22:47:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(i)
+  return true if i == 2
+  return false if i <= 1 || i.even?
+  search_array = [*3..(i/2).ceil]
+    search_array.each do |divisor|
+      if i % divisor == 0
+        return false
+      end
+    end
+    return true
+end

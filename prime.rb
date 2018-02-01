87f1eee39707906eb0c801f769c8aaf61a6043da
Harley Rayner <harleymteden@gmail.com>
2018-02-01 05:54:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,18 @@
 # Add  code here!
-def prime_numbers(number_to_test)
-    # number_to_test.each do |num|
-      number_to_test > 1 && number/number == 1
-    #   true
-    # else
-    #   false
-    end
-end 
+# def prime?(number_to_test)
+#   # 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+#     if number_to_test > 1 && number_to_test/number_to_test == 1 && number_to_test % 2
+#       true
+#     else
+#       false
+#     end
+# end
+
+def prime?(number)
+  if number > 1
+    (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
+    return true
+  else
+    false
+  end
+end

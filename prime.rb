125226d9fa770c8ff3463cf544c007372e6dc20c
Harley Rayner <harleymteden@gmail.com>
2018-02-01 05:33:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+def prime_numbers(number_to_test)
+    # number_to_test.each do |num|
+      number_to_test > 1 && number/number == 1
+    #   true
+    # else
+    #   false
+    end
+end 

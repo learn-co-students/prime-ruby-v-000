c82157130f02348a2278eb58f2e113224f6a1bff
Olivia Hanson <olivia.joy.hanson@gmail.com>
2018-02-05 04:20:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+  if int < -1
+    numbers = (int...-2).to_a
+    numbers.none? do |num|
+      int%num == 0
+    end
+  elsif int > 1
+    numbers = (2...int).to_a
+    numbers.none? do |num|
+      int%num == 0
+    end
+  else
+    false
+  end
+end

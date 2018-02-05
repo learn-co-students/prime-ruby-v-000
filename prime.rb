f2160cccfc2feb4b452de70856b0b5fff4d27a61
Katrina Brinson <kbrinso@gmail.com>
2018-02-05 22:51:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+require 'benchmark'
+
+def prime?(num)
+  if num < 2 
+    return false 
+  elsif num == 2 
+    return true
+  else
+  	list = (2..num).to_a
+  	p = 2
+  	i = 0
+  	while i < list.length
+  		list.each{|item| 
+  			if item % p == 0
+  				list.delete(item)
+  			end
+  		}
+  		i += 1
+  		p = list[i]
+  	end
+  	list.include?(num)
+  end
+end

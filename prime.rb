b1fd68bd19e2abe203d1dcf7b354e9377afc89a0
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-05 03:51:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+   return false
+  elsif number <= 3
+    return true
+  elsif number % 2 == 0 || number % 3 ==0
+    return false
+  else
+
+    i=2
+    while i*i <= number
+      if number % i == 0
+        return false
+      else
+        i = i + 1
+      end
+    end
+    return true
+  end
+end

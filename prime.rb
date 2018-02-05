f13d5258bb923ce4cc952abf5baff11014a61ba9
Michael Kang <michaelkang85@gmail.com>
2018-02-05 16:36:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(n)
+   if n == 2 || n == 3
+     return true
+   elsif n.abs == 1
+     return false
+   elsif n == 0
+     return false
+   else
+     i = 2
+     while i < n.abs
+       if n.abs % i == 0
+         return false
+       end
+       i += 1
+   end
+ end
+ return true
+end

8f75d532dac8da2578b246f91d6bc1aea0627e14
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-06 18:46:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime? (int)
+  sq_rt = int ** 0.5
+  i = 3
+  
+  if int < 2
+    false
+  elsif int == 2 || int == 3
+    true
+  elsif int.even?
+    return false
+  else
+    while i < sq_rt
+      if int % i == 0
+        return false
+      end
+      puts int % i
+      i += 1
+    end
+    return true
+  end
+end

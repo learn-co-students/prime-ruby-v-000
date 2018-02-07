11d5d2514a1fd056aa9a754ded9905a2c8713a5e
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-07 17:28:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(i)
+    if i == 2
+  return true
+elsif i <= 1
+       false
+elsif (2..(i/2)).any? {|prime| i % prime == 0 }
+  return false
+else
+  return true
+   end
+end

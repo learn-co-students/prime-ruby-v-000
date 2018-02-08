0e0a7d32db78551b219ea06d1195258ac79f953f
sgibson47 <sgibson47@gmail.com>
2018-02-08 14:51:36 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if num<=1
+    false
+  elsif num>1
+    (2..Math.sqrt(num)).each { |i|
+    if num % i == 0 && i < num
+      return false
+    end
+    }
+    true
+  end
+end
+    

d4bf3965890141a153cfd547e40315304ff0a9b6
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-08 16:32:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  integer_range = (2..integer-1).to_a 
+  if integer > 1 && integer_range.none? do |integer_tested|
+    integer % integer_tested == 0
+    end
+    return true
+  else
+    return false
+  end
+end
+

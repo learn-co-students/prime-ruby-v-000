c4910fd57f2ef4599adc6b2db5ff0ca50eab0c73
Justin K <justin-kim1@hotmail.com>
2018-02-08 06:25:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,9 @@
-def prime?(number)
-  start = 2
-  if number > 1
-    range = (start..number-1).to_a
-    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
-                                # return true, then they are not divisible by the number and the number is prime
-      number % num_to_test == 0
-    end
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
   else
-    false
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
 end

60025e24ac4895b6e649d665c67cc4f92960d2ec
Trac Nguyen <nguyendtrac@gmail.com>
2018-02-11 20:05:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  elsif number <= 3
+    true
+  else
+    (2..number/2).none? do |x|
+      number % x == 0
+    end
+  end
+end

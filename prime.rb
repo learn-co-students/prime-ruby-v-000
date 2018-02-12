a9cb67752e4e9a9e0835bc330073cc777f581009
Juan Rivera <juanriv45@gmail.com>
2018-02-12 04:47:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(test_num)
+  i = test_num
+  collection = []
+  until i == 0
+    if test_num % i === 0 then
+      collection << i
+    end
+    i-=1
+  end
+
+collection.length == 2
+
+end

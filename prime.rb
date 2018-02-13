bd28797aa2b5d2f564106bfb4b6121529af1df0b
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-13 20:53:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number < 2
+    return false
+      # binding.pry
+  else
+    f = 2
+    array = []
+    while f < number do
+      if number % f == 0
+        array << f
+        f += 1
+      else f += 1
+      end
+    end
+    array
+  end
+  array.length != 0 ? false : true
+end

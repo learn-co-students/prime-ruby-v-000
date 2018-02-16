aa9e96b6bf336dad85f08873ca8582b1744c4bb2
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-16 15:00:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  if number < 0 or number == 0 or number == 1
+    return false
+  else
+    (2..number-1).to_a.all? do |possible_factor|
+      number % possible_factor != 0
+    end
+  end
+end

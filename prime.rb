b2fb0ec1c727c274d2963ec2071b79e1ec68ce3d
Veronica Bustabad <vbustabad@gmail.com>
2018-02-17 03:04:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(integer)
+  if integer == 1 || integer == 0 || integer < 0
+      return false
+  else (2..integer - 1).to_a.all? do |possible_factor|
+    integer / 1 == integer && integer / integer == 1 && integer % possible_factor != 0
+    end
+  end
+end

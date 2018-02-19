668fd898f684acf7172f39341c05f7ed031a458f
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-02-19 15:43:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,12 @@
 # Add  code here!
-require 'prime'
+
 
 def prime?(integer)
-  integer.prime? 
-
+  if integer <= 1
+    false
+  else
+  (2..integer-1).to_a.all? do |possible_factor|
+    integer % possible_factor != 0
+end
+end
 end

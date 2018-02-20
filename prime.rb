551028c9354250ce7719bc0ce3fb25edb066b3e6
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-20 21:34:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
+require'benchmark'
+
+puts Benchmark.measure {
 def prime?(integer)
-  if integer > 0 && integer != 1
+  if integer > 1
     range = (2...integer).to_a
     range.any? {|num| integer % num == 0} ? false : true
   else
     false
   end
 end
+}

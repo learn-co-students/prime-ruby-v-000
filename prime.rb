fdb1dc368c3f615956ef75ba14d83055c6578223
Katie Cassell <kcassell944@gmail.com>
2018-02-21 23:53:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require "pry"
+require "prime"
+def prime?(number)
+  if number <= 0 || number == 1
+    return false
+  end
+  number.prime?
+#   number.each{ ||
+# if number.prime? == 2
+#   return true
+# end
+#   }
+
+end

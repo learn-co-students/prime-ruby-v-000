e1911c4b5eb480beadb0162df48eff9242cc5cee
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-23 00:29:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,8 @@
 # Add  code here!
 def prime?(n)
-  i_pos = (2..n-1).to_a
-  i_neg = (n+1..-2).to_a
-
-  if n > 0
-    i_pos.none?{ |i_pos| n % i_pos == 0 }
-  elsif n <= 0
-    i_neg.none?{ |i_neg| n % i_neg == 0 }
+  i = (2..n-1).to_a
+  if n <= 1
+    false
+  else i.none?{ |i| n % i == 0 }
   end
 end

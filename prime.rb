8e70480a51bdde5509d0c1ee1784d295cc30c217
Tatyana Scotce <tscotce@gmail.com>
2018-02-23 01:18:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(i)
+  if i <= 1
+    return false
+  elsif i.between?(2, 3)
+    return true
+  elsif i > 3 
+  array = Array(2...i)
+    array.each do |d|
+      if i % d == 0
+        return false
+      else return true
+      end
+    end
+  end
+end
+

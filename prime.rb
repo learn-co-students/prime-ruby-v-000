6ec46cb0e4d29164d9e36bd116934715c0dd8675
Holden Altaffer <haltaffer@gmail.com>
2018-02-23 21:45:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+# num > 0 && num % num == 0 && num % 1 == 0 && num % [2..num - 1] != 0
+# [2..num]
+def prime?(num)
+  if num == 2
+    true
+  elsif num > 1 && num % num == 0 && num % 1 == 0 && !(2 .. (num - 1)).to_a.any?{|number| num % number == 0}
+     true
+  else
+    false
+  end
+end

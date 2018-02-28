e443271a5e5a0417257d7c80db25a623711ac062
Annie Bourke <aabourke@gmail.com>
2018-02-28 23:40:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(x)
+
+  range = (2..(x-1)).to_a
+  answer = true
+
+  if x <= 1
+    answer = false
+  else
+    range.each do |y|
+      if x % y == 0
+        answer = false
+      end
+    end
+  end
+
+  answer
+end

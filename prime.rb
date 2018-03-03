f7631c5435c19511675858e074b7ebf15398bf5f
Aspen James <aspenjames@tqca.org>
2018-03-03 01:15:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,6 @@
-## This method returns true if the argument is prime.
- # Only certain for prime numbers up to 9409.
-require'benchmark'
-# def prime?(number)
-#   return false if number <= 1
-#   primes_to_100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-#   return true if primes_to_100.include?(number)
-#   primes_to_100.each{|prime| return false if number % prime == 0}
-#   true
-# end
+## Uncomment line 2 and lines 12-19 to use Benchmark testing
+# require'benchmark'
+
 
 def prime?(number)
   return true if number == 2
@@ -16,11 +9,11 @@ def prime?(number)
   !poss_divisors.any? { |div| number % div == 0 }
 end
 
-puts Benchmark.measure{prime?(68351)}
-puts prime?(68351)
-puts Benchmark.measure{prime?(100069)}
-puts prime?(100069)
-puts Benchmark.measure{prime?(104729)}
-puts prime?(104729)
-puts Benchmark.measure{prime?(104725)}
-puts prime?(104725)
+# puts Benchmark.measure{prime?(68351)}
+# puts prime?(68351)
+# puts Benchmark.measure{prime?(100069)}
+# puts prime?(100069)
+# puts Benchmark.measure{prime?(104729)}
+# puts prime?(104729)
+# puts Benchmark.measure{prime?(104725)}
+# puts prime?(104725)

538b06074dbb596bf472b3e83955e473f3a1714d
Maverick Tesini <mavericktesini@gmail.com>
2018-03-05 18:39:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,13 @@
 # Add  code here!
+#this method uses a range between 2 and half a given number.
+#The method checks if none?(true) iteration returns true.
+#if so, then only 1 and the given number divide perfectly into the given number.
+#hence, the definition of a prime number.
+#Any other number less than 2, by default isn't prime.
 def prime?(n)
   if n < 2
     return false
-  elsif (2..n/2).none?{|i| n % i == 0}
+  elsif (2..n/2).none?{|i| n % i == 0}  #between 2 and (argument/2), if no numbers are whole dividers (no remainders), then only 1 and self will divide... definition of a prime.
     return true
   else
     false

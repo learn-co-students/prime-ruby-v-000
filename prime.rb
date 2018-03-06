397362a6ebbe219fab0ff4e41ac5eaf90614f082
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-06 07:11:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1
+      return false
+    elsif number == 2
+      return true
+    elsif number == 3
+      return  true
+    elsif number % 2 == 0
+      return false
+    elsif number % 3 == 0
+      return false
+    end
+
+
+end

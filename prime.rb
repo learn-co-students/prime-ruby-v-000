9a2d35c081a00a14f1c7ab0b5dbc4ca905dd1b29
Kelsey Fecho <kfecho30@gmail.com>
2018-03-07 21:47:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(n)
+  test = true
+  if n < 2
+    test = false
+  else
+    for i in 2..(n-1)
+      if n%i == 0
+        test = false
+      end
+    end
+  end
+  test
+end

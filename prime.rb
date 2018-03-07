d4409ede227e0d2626451d2d751a93894e6cad5c
Travis Williamson <travywill@gmail.com>
2018-03-07 22:53:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+def prime?(number)
+  numbers_to_check = (1..number).to_a
+  factors = []
+
+  numbers_to_check.each do |number_in_range|
+    if number % number_in_range == 0
+      factors << number_in_range
+    end
+  end
+
+  if factors == [1, number]
+    true
+  else
+    false
+  end
+end

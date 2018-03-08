1daa22bc7f713d101f826b28fc5a23d951a15af4
Tatyana Scotce <tscotce@gmail.com>
2018-03-08 02:33:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(i)
-  if i <= 1 || i == 4
+  if i <= 1 
     return false
   elsif i.between?(2, 3)
     return true
-  elsif i > 4 
-  array = Array(2...i/2)
+  elsif i > 3 
+  array = Array(2...i)
     array.all? do |d|
        i % d != 0 
     end

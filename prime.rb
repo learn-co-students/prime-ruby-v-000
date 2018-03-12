b0c3196284cf9a8ebde76815fbb5cdbeb99bc0d7
Erica Murdock-Waters <ericahpa@gmail.com>
2018-03-12 04:41:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+# Prime.rb
+# returns boolean of whether integer is a prime number or not
+
+def prime?(n)
+  if n == 2
+    true
+  elsif n == 3
+    true
+  elsif n < 2
+    false
+  elsif n.even?
+    false
+  elsif (2..n/2).none?{|i| n % i == 0}
+    true
+  else
+    false
+ end
+end

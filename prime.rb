c493735fae1a5149807641e7a4dad3bb32bb2573
Parul Gupta <parulguptasmail@gmail.com>
2018-03-12 15:32:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  if number == 2 || number == 3
+    return TRUE
+  elsif number > 3 
+    arr = (2..number-1).to_a 
+    
+  elsif number <= 1
+    return FALSE
+  end
+  arr.each do |x|
+    if number % x == 0 
+      return FALSE
+    end
+  end
+  TRUE
+end

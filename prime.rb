4fed343b07625f32d72b56552e235d8cb44113da
David Markel <davidgmarkel@gmail.com>
2018-03-13 19:57:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 def prime?(integer)
+
   if integer < 2
     false
   else
     integers = (2..integer - 1).to_a
-    integers.all? {|number| integer % number != 0}
+    integers.odd? {|number| integer % number != 0}
   end
+
 end

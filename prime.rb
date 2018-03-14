8825d8bb8f7fdde3f18f59f653ec4c0133d6bbe9
Emily Welsch <emwelsch@gmail.com>
2018-03-14 20:59:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,10 @@
 def prime?(i)
-  i / 1 == i && i / i == 1 # is i divisible by 1 and the i itself
+  # i / 1 == i && i / i == 1 # is i divisible by 1 and the i itself
+  if i < 0 or i == 0 or i == 1
+      return false
+    else
+      (2..i-1).to_a.all? do |possible_factor|
+        i % possible_factor != 0
+      end
+    end
 end

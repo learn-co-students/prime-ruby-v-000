ffb7275fdde3538d4a5170af0296e0f42c10c3f8
Megan Collier <meganraec@live.com>
2018-03-15 19:28:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,32 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    return false
+  elsif n == 2 || n == 3
+    return true
+  end
+    x = 2
+    while (x * x <= n)
+      if n % x == 0
+        return false
+      end
+    x += 1
+    end
+  return true
+end
+
+
+#     x = (2..n-1).to_a
+#     x.each do |i|
+#       while (i * i <= n) do
+#       n % i == 0
+#       break if false
+#       end
+#     end
+#   else
+#     return true
+#   end
+# end
+
+
+# for i in (2..n-1)
+# n % i == 0 ? (return false) : (return true)

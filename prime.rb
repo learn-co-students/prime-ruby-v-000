1bd8c686e7a6ec9fb1154159d5ee799b6af9c08f
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-15 23:58:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,19 @@
 
 def prime?(n)
-  inf = 1.0/0.0
-  numbers = (1..inf).to_a
-
-  numbers.each do |integer|
-
-   if n > 1 && n%integer > 0 && integer != n
-     reuturn true
-   end
+  numbers = (2..100).to_a
+  if 1 > n || n == 1
+    false
+  else
+  numbers.all? do |integer|
+   n/1 == n  && n/integer == 1 || n%integer != 0
+  end
+  end
 end
-end
+
+
+  #if 0 < n &&
+  #  numbers.any? do |integer|
+  #  if 0 < integer
+
+  #    if n/1 == n  && n/integer == 1 || n%integer != 0
+  #      return false

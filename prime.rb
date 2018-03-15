9e6dd19d57d5c05929fe26852465abd682c72628
Emma Freeman <freemoe04@gmail.com>
2018-03-15 16:56:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(integer)
+  return false if integer <= 1
+  for n in 2..(integer - 1)
+    if (integer % n) == 0
+      return false
+    end
+  end
+  return true
+end

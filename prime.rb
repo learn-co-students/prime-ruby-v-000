ae01ab4b9a99a59699a07c6871ab4718dfdf2d2b
Karen G <kgraves0701@gmail.com>
2018-03-16 19:51:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  else
+    i = 2
+    while i < number
+    return false if number % i ==0
+    i+=1
+    end
+    true
+  end
+end

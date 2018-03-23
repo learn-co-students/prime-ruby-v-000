33d839b709eed76d5b0c0c557c52d45da9aed28e
Heidi Hlavinka <hchlav@gmail.com>
2018-03-23 15:28:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+
+def prime?(num)
+  primes = []
+    return true if primes.include?(num)
+    return false if num < 2
+  (2...num).each do |i|
+    return false if num % i == 0
+  end
+  primes << num
+    true
+end
+
+#   return if num <= 1
+#   (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+# end

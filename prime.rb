afe354f6551fd4d9ab33af3b90bb0a20fde368e5
Madeline Stark <madeline.stark1@gmail.com>
2018-03-26 18:42:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+    if
+      integer < 2
+      false
+    elsif integer == 2
+      true
+    else
+      (2..integer/2).none? {|i| integer % i == 0}
+    end
+end

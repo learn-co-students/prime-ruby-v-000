9acd2258497096447c0089f167a71be819e5bae8
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-27 22:35:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime? (num)
+  # return false for the negatives, etc
+  if num <= 1
+    return false
+  end
+  is_prime = true
+  factor = 2
+  limit = num/2
+  while factor<=limit && is_prime
+    if num%factor == 0
+      is_prime=false
+    end
+    factor += 1
+  end
+  is_prime
+end

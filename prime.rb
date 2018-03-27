422d188b8fc073ea41d4b0a17a9c07dceec51900
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-27 22:48:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,21 @@
 def prime? (num)
   # return false for the negatives, etc
   if num <= 1
-    return false
+    prime= false
+  elsif num == 2
+    prime = true
+  else
+    prime = (2..num/2).all? { |n| num%n!=0}
   end
-  is_prime = true
-  factor = 2
-  limit = num/2
-  while factor<=limit && is_prime
-    if num%factor == 0
-      is_prime=false
-    end
-    factor += 1
-  end
-  is_prime
+  prime
+#  is_prime = true
+#  factor = 2
+#  limit = num/2
+#  while factor<=limit && is_prime
+#    if num%factor == 0
+#      is_prime=false
+#    end
+#    factor += 1
+#  end
+#  is_prime
 end

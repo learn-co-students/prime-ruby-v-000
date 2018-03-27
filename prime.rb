d42a313302b407bb1d8c228dab4b2080f7dc679c
Seth Wax <sethlwax@gmail.com>
2018-03-27 18:50:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  is_prime = true
+  is_prime = false if number < 2 || (number != 2 && number % 2 == 0) || (number != 3 && number % 3 == 0)
+  i = 5
+  while i**2 < number
+    is_prime = false if number % i == 0 || number % (i+2) == 0
+    i += 6
+  end
+  is_prime
+end

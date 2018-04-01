1e129f31c58f68b4ff2ebc779afd40b10a8f4a11
Ally Netzel <netzelally@gmail.com>
2018-04-01 04:12:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(n)
+  return false if n < 2 
+  return true if n == 3 || n == 2 
+    if (2...n-1).any?{|i| n % i == 0}
+      false
+    else
+      true
+    end
+end

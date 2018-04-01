b4ce72a02e238d6f0fc146b219a21be87162c608
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 19:44:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    else
+        range = (2..number-1).to_a
+        if range.any? { |n| number % n == 0 }
+            return false
+        else
+            return true
+        end
+    end
+end

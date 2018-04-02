66fe88fc9b18fb69156d0ee30004bee1e9b4ad43
Chris Dundon <chris.dundon@gmail.com>
2018-04-02 19:35:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  elsif number == 2 
+    return true
+  elsif number.even?
+    return false
+  end
+  
+  i = 3
+  while i < number / 2 do
+    if number % i == 0
+      return false
+    end
+    
+    i += 2
+  end
+  true
+end

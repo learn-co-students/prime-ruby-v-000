7384fd3e4f3db2fc10c1bbcbe34902deb82fd104
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-04 02:36:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
-def prime?(int)
-  if int != 2 && int != 3 && (int <= 1 || int % 2 == 0 || int % 3 == 0)
-    false
+def prime? (int)
+  if int <= 1
+      false
+  elsif int == 2
+      true
   else
-    true
+      (2..int/2).none? { |i| int % i == 0}
   end
 end

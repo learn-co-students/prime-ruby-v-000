8b93cebf708025053c54625e2013736c7311fba8
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-05 01:22:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,28 @@
+# require 'benchmark'
+# def prime?(integer)
+#   divisor = 2
+#   while integer % divisor != 0
+#     if integer <= 1
+#       break
+#     else
+#       divisor += 1
+#     end
+#   end
+#   if integer == divisor
+#     true
+#   else
+#     false
+#   end
+# end
+#
+# puts Benchmark.measure{prime?(105557)}
+# #> 0.000000   0.000000   0.000000 (  0.006580)
+#
+# puts Benchmark.measure{prime?(101013)}
+# #> 0.000000   0.000000   0.000000 (  0.000004)
+
+
+require 'benchmark'
 def prime?(integer)
   divisor = 2
   while integer % divisor != 0
@@ -7,9 +32,16 @@ def prime?(integer)
       divisor += 1
     end
   end
+  # return integer == divisor
   if integer == divisor
     true
   else
     false
   end
 end
+
+puts Benchmark.measure{prime?(105557212312123123123123123123123123123454455)}
+#> 0.000000   0.000000   0.000000 (  0.006580)
+
+puts Benchmark.measure{prime?(101013)}
+#> 0.000000   0.000000   0.000000 (  0.000004)

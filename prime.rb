53e480f3fa65df353a74964b826d0f29f2d87509
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-05 00:56:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+  divisor = 2
+  while integer % divisor != 0
+    if integer <= 1
+      break
+    else
+      divisor += 1
+    end
+  end
+  if integer == divisor
+    true
+  else
+    false
+  end
+end

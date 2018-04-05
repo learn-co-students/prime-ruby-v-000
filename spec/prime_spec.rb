a4c2133becedbb8733fa77a95b11555e6f7397e9
Joseph Parker <jparker010190@gmail.com>
2018-04-05 02:26:24 +0000

    Done.

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -11,9 +11,9 @@ describe "prime?" do
 
   it 'returns false for non-prime numbers' do
     expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
-    expect(prime?(0)).to be(false)
     expect(prime?(1)).to be(false)
     expect(prime?(4)).to be(false)
+    expect(prime?(0)).to be(false)
     expect(prime?(40)).to be(false)
     expect(prime?(1763)).to be(false)
     expect(prime?(101013)).to be(false)

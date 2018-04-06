e523c298b1d9eda734c836d6102645cea6e05148
Sean Febo <seanfebo@techie.com>
2018-04-06 22:45:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime? (i)
+   if i<= 1 
+     return false 
+    elsif i == 2 
+      return true 
+    else 
+      (2..i/2).none? do |n| 
+        i % n == 0
+    end
+  end
+end

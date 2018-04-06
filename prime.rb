e6704910225351b36638efedcd44b8e26951b962
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-06 19:01:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require 'pry'
+def prime?(number)
+  return false if (number.even? && number > 2) || number <= 1
+
+  half_num = number / 2
+  x = 3
+  while x < half_num
+    return false if number % x == 0
+    x += 2
+  end
+
+  true
+end

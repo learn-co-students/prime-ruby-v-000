16d353e26587cead1288da077621fbe851e04760
Kayla Reopelle <kreopelle@gmail.com>
2018-04-09 12:13:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+
+def prime?(my_number)
+  factor_array = []
+  possible_factor = 2
+  while my_number > 1
+    if (my_number % possible_factor == 0)
+      factor_array << possible_factor
+      my_number /= possible_factor
+    else
+      possible_factor += 1
+    end
+  end
+  if factor_array.length == 1
+    true
+  else
+    false
+  end
+end

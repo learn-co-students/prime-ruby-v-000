2e2aaa8abc43c51bc1e7a195e6b2175afaeff53e
Stacey Neathery <neathery.stacey@gmail.com>
2018-04-09 03:48:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(integer)
+  if integer < 0 or integer == 0 or integer == 1
+    return false
+  elsif
+    divide = (2..(integer - 1)).to_a
+    divide.any? {|divide_number| integer % divide_number == 0} ? false : true
+  else
+    false 
+  end
+end

07d706e73f6287a747b88a97150acdb307b8f70f
Everett <everett.bowline@gmail.com>
2018-04-10 01:29:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(integer) 
+  i = 4
+  if integer <= 1 
+    return false
+  elsif integer == 2 || integer == 3
+    return true
+  elsif integer % 2 == 0
+    return false
+  end
+  while i < integer/2
+    if integer % i == 0
+      return false
+    end
+    i += 1
+  end
+  true
+end

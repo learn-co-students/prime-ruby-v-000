e19e021f6bc2c45ff163fa62934706e9de6ea8c2
Laura Messa Travin <laura.travin@gmail.com>
2018-04-10 04:54:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(num)
+
+    i = 2
+
+   if num <= 0 or num == 1
+       return false
+   elsif  num == 2
+      return true
+   else
+      while i <= num/2
+
+         if num % i == 0
+           return false
+         end
+         i += 1
+      end
+      return true
+    end
+
+
+
+end

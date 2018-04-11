b00c00479092bff89b13a685cb9641c12bb5b01c
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-11 01:59:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if
+    number < 2 || (2...number).to_a.any?{|i| number % i == 0} # => not prime
+    return false
+  else
+    return true
+  end
+end
+
+# number has to be 2 or greater
+# number cannot have a remainder when divided by a number between 2...n-1

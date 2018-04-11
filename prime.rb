01c10693b21c4d8a2c8c974de9c14d93a1df0d6c
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-11 17:11:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+  range =*(1..100)
+  new_range = Array.new
+  range.select do |numbers|
+      new_range << numbers if integer % numbers == 0
+  end
+  new_range
+    if integer.negative?
+      false
+    elsif integer == 0 || integer == 1
+      false
+    elsif new_range.count > 2
+      false
+    else
+     true
+    end
+  end

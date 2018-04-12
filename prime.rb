63259c1cd12fe25bb958f9ba27c0753a33b9b7b7
Anuradha Khambete <anukhambete@gmail.com>
2018-04-12 22:05:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    false
+  else
+    factors = []
+    arr_num = (1..num).to_a
+    arr_num.each do |n|
+      if num % n == 0
+        factors << n
+      end
+    end
+    
+    if factors.length > 2
+      false
+    else
+      true
+    end
+    
+  end
+end

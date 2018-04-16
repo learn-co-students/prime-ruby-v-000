e656d3455c646190ed4956942db71eeb94daf436
Valencia Caldwell <vkcaldwell@gmail.com>
2018-04-16 23:37:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  if number >= 1
+    (2.. number -1).each do |num|
+      if number%num == 0
+        return false
+      end
+    end
+  end
+  return true
+end

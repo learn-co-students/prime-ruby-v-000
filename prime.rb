d56d78d7cdf12177d60ce3fd7dd7cb8b47ed9688
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-16 17:53:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  counter = 2
+  until counter == integer
+    if integer % counter == 0
+      return false
+    end
+    counter +=1
+  end
+  return true
+end

b6523df4db98f2732ab220c88cb3f9b865c2de84
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-18 23:40:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+  i = 1
+ number_divisible_by = []
+  while i <= number
+    if number % i == 0
+      number_divisible_by << i
+    end
+      i += 1
+  end
+  number_divisible_by
+
+  if number_divisible_by.size === 2
+    return true   #number is prime
+  else
+    return false
+  end
+end

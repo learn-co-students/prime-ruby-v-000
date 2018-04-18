ac7170942e97aeae9cf6a354d77cc0c10f58f7a8
Ji Yim <gsooyim@gmail.com>
2018-04-18 21:55:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(number)
+  test_array = (2..(number - 1)).to_a
+  if number <= 1
+    false
+  else 
+    test_array.any? { |n| number % n == 0 } ? false : true
+  end
+end

f5181e293f196a946ad29fc1f7ad9189044ff30d
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-19 02:05:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,18 +1,13 @@
-# Add  code here!
-def prime?(number)
-  i = 1
- number_divisible_by = []
-  while i <= number
-    if number % i == 0
-      number_divisible_by << i
-    end
-      i += 1
-  end
-  number_divisible_by
 
-  if number_divisible_by.size === 2
-    return true   #number is prime
-  else
+def prime?(number)
+  number = number.abs
+  collection = (1..number).to_a
+
+  if collection.any?{|num| number % num == 0 and num != number and num != 1} || number == 1 || number == 0
     return false
+  else
+    return true
   end
 end
+
+# (1...number).to_a = collection;  if collection.any?{|num| number%num == 0 } == true then return false

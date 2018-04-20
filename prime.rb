865fd9590fc5ca2859156ae14e34e280ace3c5ba
David Moore <hey@iamdavidmoore.com>
2018-04-20 02:51:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+
+def prime?(int)
+  i = 2
+  if int < 2
+    return false
+  end
+  while i < int
+    if int % i == 0 || int == -1
+      return false
+      i += 1
+    end
+    i +=1
+  end
+  true
+end

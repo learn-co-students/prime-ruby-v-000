7b9354074009ea28b3c987b5a82196d2bfd25eba
Michael Boroski <mjboroski@gmail.com>
2018-04-20 22:31:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+          return false
+  elsif n <= 3
+          return true
+  elsif n%2 == 0 or n%3 == 0
+          return false
+  end
+
+  i = 5
+  while i * i <= n
+    if n % i == 0 or n % (i + 2) == 0
+        return false
+    end
+    i = i + 6
+  end
+
+  return true
+end

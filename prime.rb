e19d4ded875ffe8c184798ce4eac3cd729e716f1
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-22 20:49:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  t = number - 1
+  range = (2..t).to_a
+  if number < 2 
+    FALSE
+  elsif
+    (2..t).none? {|ea| number % ea == 0}
+    TRUE
+  else
+    FALSE
+  end
+end

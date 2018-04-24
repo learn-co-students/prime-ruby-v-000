55dc1dca43a0b189fba9f7fec45b240a3a783ab7
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-24 16:59:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -3,21 +3,9 @@ require'benchmark'
 def prime?(int)
   if int <= 1
     false
-  elsif int <= 3
-    true
-  elsif int % 2 == 0 || int % 3 == 0
-    false
-  
-  n = int**0.5
-  i = 0
-  test_range = (2..(n)).to_a
-  
-while i < test_range.length
-    if n % test_range[i] != 0
-      return false
-      i += 1
+  else
+    (2..(int**0.5)).to_a.all? do |possible_derivative|
+      int % possible_derivative != 0
     end
-  end
-else true
   end
 end

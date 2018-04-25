4ad2660919dc104bbe9592520e70b4c506fef45d
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-25 02:30:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,14 +1,3 @@
-# # Add  code here!
-# def prime?(n)
-#   primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-  
-# return primeNumbers.any?{|i|i==n}
-  
-  
-  
-  
-# end 
-
 
 def prime?(n)
   if n<2 

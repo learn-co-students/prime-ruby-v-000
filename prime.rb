f194dfef524441c07508420c07370d1e1cff3331
Melissa Real <melissa.real@hotmail.com>
2018-04-26 19:35:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(int)
+  collection =(1..1000000).to_a
+  array_of_ints = []  
+  
+  collection.each do |nr|
+    
+    if int % nr == 0
+     array_of_ints.push(int % nr)  
+    end 
+    
+  end 
+  
+  if array_of_ints.length == 2 
+   
+    return true 
+    
+  else 
+   
+    return false 
+  end 
+    
+    
+end

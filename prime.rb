e12841a35c81090be83ad70e11d516f7d3663b47
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-26 06:06:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+
+require 'pry'
+
+# def prime?(integer)
+  # while integer <
+# end
+
+def prime?(n)
+  if n > 2
+    range = (2..n-1).to_a
+      range.any? { |number| number % range.any? == 0 }
+    elsif n <= 2 && n > 0
+      range = (0..n).to_a
+    else n < 0
+      range = (n..0).to_a
+  end
+end
+#binding.pry
+    #if n % i == 0
+      #false
+    #end
+  #end

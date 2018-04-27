20f375b6cff163908abf4dcc5be8896e983f7d49
Karl Amoguis <ktamoguis@gmail.com>
2018-04-27 06:57:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  i = 2
+  if number < 2  then
+    false
+  else
+    while i < number
+      if number % i == 0
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+end

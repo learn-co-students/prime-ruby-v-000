710bfc8152e3fb36f3fac04026351e7983fdeb91
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-29 03:41:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,23 +1,15 @@
-# Add  code here!
-
 require 'pry'
 
-# def prime?(integer)
-  # while integer <
-# end
-
 def prime?(n)
-  if n > 2
-    range = (2..n-1).to_a
-      range.any? { |number| number % range.any? == 0 }
-    elsif n <= 2 && n > 0
-      range = (0..n).to_a
-    else n < 0
-      range = (n..0).to_a
+  if n <= 1
+    return false
   end
+  lowest = 2
+  while lowest < n
+    if n % lowest == 0
+      return false
+    end
+    lowest = lowest + 1
+  end
+  return true
 end
-#binding.pry
-    #if n % i == 0
-      #false
-    #end
-  #end

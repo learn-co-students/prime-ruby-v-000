5ef44182cf019c3892953beb445672f32e31b795
Paulina Salem <paulinasalem@yahoo.com>
2018-05-01 00:27:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+    if num <= 1
+      return false
+    end
+      n = 2
+  while n < num
+    if num % n == 0
+      return false
+    end
+    n += 1
+  end
+  true
+end

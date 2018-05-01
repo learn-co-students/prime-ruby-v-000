7d2aecefc89bda875d2a5d5691fb090e950edd41
Paul <pcherian10@gmail.com>
2018-05-01 15:58:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime? (num)
+  return false if num <= 1
+  return true if num == 2
+  i = 3
+  while (i < num)
+    return false if num % i == 0 || num.even?
+    i += 1
+  end
+  true
+end

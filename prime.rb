a01d7642e9118b9541ce13f1cb9e0c8f5ce1fb82
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-02 16:40:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+    n = 2
+  while n < num
+    if num % n == 0
+      return false
+    end
+    n += 1
+  end
+  true
+end
+  #(2...100).to_a
+  #(2..num/2).none?{|i| num % i == 0}
+
+  

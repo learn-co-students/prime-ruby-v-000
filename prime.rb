2273d2728407c01cb20d7d1b59dae9b4e08811bc
Eric Chrobak <eric@ericchrobak.com>
2018-05-03 02:59:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(number)
+  i = 2
+  root = number / i.to_f
+  if number == 2
+    return true
+  elsif number < 0 || number == 1 || number == 0
+    return false
+  else
+    while i <= root
+      if number % i == 0
+        return false
+      end
+      root = number / i.to_f
+      i += 1
+    end
+  end
+  return true
+end

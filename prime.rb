eb2cf2b3068fc175e765680306255306492dd2dc
corytveten <corytveten@gmail.com>
2018-05-03 15:46:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(integer)
+  return false if (integer.even? && integer > 2) || integer <= 1
+
+  number= integer / 2
+  x = 3
+  while x < number
+    return false if integer % x == 0
+    x += 2
+  end
+
+true
+end

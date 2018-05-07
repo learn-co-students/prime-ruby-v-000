52a590d5112cb2f6660ebf716a214398f94b4f0b
Dmitri Mendis <2dmendis@gmail.com>
2018-05-07 19:38:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ def prime?(num)
     if num == 2 
       return true 
     else 
-      if ((3..(num - 1)).to_a).any? {|i| num % i == 0}
+      if ((2..(num - 1)).to_a).any? {|i| num % i == 0}
         return false 
       else 
         return true 

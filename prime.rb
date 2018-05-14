ee8184f92bd12403f5bfa990e8d994f55454202d
Nathaniel Shuster <nshu521992@gmail.com>
2018-05-14 21:23:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    return false
+  else
+    i = 2
+    while i < n
+      if n % i == 0
+        return false
+      else
+        i += 1
+      end
+    end
+  end
+  true
+end

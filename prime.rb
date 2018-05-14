8a0f1985ec6898831005f5649187aa4cd64e3747
Shirley Collins <shirley@m104.us>
2018-05-14 23:36:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 1
+  return true if num.between?(2,3)
+  return false if num.even?
+
+   num_square = (num ** 0.5).floor
+   odd_range = (5..num_square).select {|int| int.odd?}
+
+   odd_range.each do |x|
+     if num % x == 0
+       return false
+     end
+   end
+   return true
+end

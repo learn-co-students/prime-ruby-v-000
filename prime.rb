afe427356bbaa9cf65293f8b4eb426f2a20b6bd1
Alex Wilson <fonzmp@gmail.com>
2018-05-15 19:28:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(int)
+  i = 2
+  storage = []
+  if int <= 1
+    return false
+  else
+    while i <= int
+      if int % i == 0
+        storage.push(i)
+      end
+      i += 1
+    end
+    array_length = storage.length
+    if array_length >= 2
+      return false
+    else
+      return true
+    end
+  end
+end

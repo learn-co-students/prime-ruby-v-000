2831a9347134bc617ce743522bb83038c9a32ce4
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-16 01:40:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(integer)
+  if integer.between?(2, 3)
+    return true
+  elsif integer < 2 || integer == 4
+    return false
+  end
+
+  check = 2
+  while check < integer
+    if integer % check == 0
+      return false
+    elsif check == integer / 2
+      return true
+    end
+    check += 1
+  end
+end

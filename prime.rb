d7865626a33330d7b0f7d4d48546831b1f2bc35f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-16 02:49:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,19 +1,17 @@
 def prime?(integer)
-  if integer % 2 === 0 && integer != 2
+  if integer % 2 === 0 && integer != 2 || integer < 2
     return false
   elsif integer.between?(2, 5)
     return true
-  elsif integer < 2
-    return false
   end
 
   check = 3
   while check < integer
     if integer % check === 0
       return false
-    elsif check === integer / 2
+    elsif check === integer / 2 || check === (integer / 2.0) + 0.5
       return true
     end
-    check += 1
+    check += 2
   end
 end

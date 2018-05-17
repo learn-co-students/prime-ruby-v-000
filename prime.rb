87ac4b48f831b6878cd85271d65584bcdc76b453
taryn yaeger <tsyaeger@gmail.com>
2018-05-17 02:38:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+
+
+def prime?(num)
+  
+  if num <= 1 
+    return false
+    
+  elsif num == 2 
+    return true
+
+  else
+  
+    range = (2..num/2 +1)
+    divs = range.collect{|x| num % x == 0}
+    puts divs
+    
+    divs.count(true) == 0 ? true : false
+
+  end
+end
+  
+ prime?(17)
+ # prime?(18)
+  

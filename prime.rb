ec6619b86c345031fc99bdfdf4b0718d42446887
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-18 20:37:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+x = 2
+    return false if integer < 2
+while x <= (integer/2)
+          if (integer % x) == 0
+             return false
+        end
+    x += 1
+  end
+  true
+end

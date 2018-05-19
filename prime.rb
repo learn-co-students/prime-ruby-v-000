430c29944da3f0c07b9635df192186120040eab0
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-19 21:17:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(int)
+  return false if int < 2 
+  return true if int == 3 || int == 2 
+  if (2...int-1).any?{|i| int % i == 0}
+    false
+  else
+    true
+  end
+end

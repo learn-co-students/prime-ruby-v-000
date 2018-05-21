2a66bb8a1839ec153598b674a5ad75ef292e17a2
Adam Barton <adam.barton333@gmail.com>
2018-05-21 03:04:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+ if num <= 1 
+   return false 
+ end
+
+  (2...num).each do |n|
+    if num % n == 0  
+      return false 
+    end
+  end
+  true 
+end
+  
+ 

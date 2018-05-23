849db45f54fe251ce574f7d748b76edb8b633f69
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-23 18:32:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+
+
+
+
+
+def prime?(number)
+  if number<=1
+    prime=false
+    return prime
+  else
+    squarert = number ** 0.5
+    int = 2.0
+    prime = true
+    while int <= squarert && prime == true
+        if number % int == 0
+            prime = false
+            return prime
+						end
+        int += 1
+				end
+    return prime
+	end
+end
+

897c2bf26f480e3c2b8f602ed572e36e4e4f3df0
Leticia Molina <letimolina@gmail.com>
2018-05-26 21:34:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  return false if (integer.even? && integer > 2) || integer <= 1
+
+  half_int = integer / 2
+  i = 3
+  while i < half_int
+    return false if integer % i == 0
+    i += 2
+  end
+
+  true
+end

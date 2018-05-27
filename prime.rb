188bde07cbbb80054411c6835a6e30d42d6aa520
Aziz Kosber <a.kosber@gmail.com>
2018-05-27 06:12:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'pry'
+def prime?(integer)
+  if integer < 0
+    false
+  elsif integer <= 1
+    false
+  elsif integer <= 3
+    true
+  else
+    (2..integer**0.5).to_a.none? {|i| integer % i == 0}
+  end
+end

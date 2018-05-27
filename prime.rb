f6d858f3b8bfeb4189d1d8cf229ed592b57f3f0c
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-27 14:29:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(test_number)
+  if test_number <= 0 || test_number == 1
+    return false
+  elsif test_number == 2
+    return true
+  else
+    i = 2
+    while i < test_number
+      if test_number % i == 0
+        return false
+      end
+      i +=1
+    end
+    return true
+  end
+end

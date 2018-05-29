abbee4fcab3c94615f760dc1e5490c1b9ab91d53
J Daniel <jdaniel1008@gmail.com>
2018-05-29 18:01:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
 require 'pry'
 # Add  code here!
 def prime?(num)
-  range = (2...num.abs).to_a
-  if [-1,0,1].include?(num)
+  range = (2...num).to_a
+  if num <= 0 || num == 1
     false
-  elsif range.empty?
-    true
   else
     range.all? do |den|
       num % den != 0

9159ee62851f3d54a9ff0683d395cf90706ebc3b
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-29 23:37:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+require 'benchmark'
+require 'bigdecimal/math'
+
+def prime?(num)
+  return false if num <= 1
+  (2..Math.sqrt(num)).each { |i| return false if num % i == 0 }
+  true
+end
+
+puts Benchmark.measure { BigMath.PI(10_000) }

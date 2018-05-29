0471c5a461512eea88a2d1a7cd0953f2cc82462b
J Daniel <jdaniel1008@gmail.com>
2018-05-29 17:52:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
+require 'pry'
 # Add  code here!
+def prime?(num)
+  range = (2...num.abs).to_a
+  if [-1,0,1].include?(num)
+    false
+  elsif range.empty?
+    true
+  else
+    range.all? do |den|
+      num % den != 0
+    end
+  end
+end

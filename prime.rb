afe3dc9999d06a52642d40f5e95e945ba6fe26cc
Richard Melchior <raustin.melchior@gmail.com>
2018-06-03 02:49:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,13 @@
-#get IsPrime = True
-#for PFactor ranges from 2 to Num-1 do
-#  begin block
-#     if Num divisible by PFactor then set IsPrime = False
-#  end block
-#if IsPrime = True then display Num is prime
-#else display Num is not prime
-
+##
+# => #prime? checks if the given number is prime. Psuedocode is below:
+# get IsPrime = True
+#  for PFactor ranges from 2 to Num-1 do
+#   begin block
+#      if Num divisible by PFactor then set IsPrime = False
+#   end block
+# if IsPrime = True then display Num is prime
+# else display Num is not prime
+##
 def prime?(number)
   isPrime = true;
   if(number <= 1)

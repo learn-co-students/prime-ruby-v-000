03d3050c414a8c66dc5ae270a1db19410e03362a
Richard Melchior <raustin.melchior@gmail.com>
2018-06-03 02:47:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+#get IsPrime = True
+#for PFactor ranges from 2 to Num-1 do
+#  begin block
+#     if Num divisible by PFactor then set IsPrime = False
+#  end block
+#if IsPrime = True then display Num is prime
+#else display Num is not prime
+
+def prime?(number)
+  isPrime = true;
+  if(number <= 1)
+    return false;
+  end
+  prime_factor = (2..(number - 1)).to_a
+  isPrime = prime_factor.none? { |factor| number % factor == 0}
+  return isPrime;
+end

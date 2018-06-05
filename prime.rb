e6a6e589c565c93eec81b9c49b7bb0e8216083a8
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-05 22:34:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+# check if an integer is prime, return true or false
+def prime?(n)
+  return false if n <= 1
+  Math.sqrt(n).floor.downto(2).each {|i| return false if n % i == 0}
+  true
+  
+end

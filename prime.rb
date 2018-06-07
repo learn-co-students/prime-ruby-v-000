4c952fed12b613518977708397952045b2889d6c
Marie Cain <mariecain27@gmail.com>
2018-06-07 01:57:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,39 @@
 # Add  code here!
+require 'pry'
+
+#def prime?(n)
+#  if n <= 0 || n == 1
+#    return false
+#  elsif n == 2
+#    return true
+#  elsif n > 2
+#    for i in 2..n do 
+#      if (n % i) == 0
+#        return false
+#      else
+#        return true
+#      end
+#    end
+#  end
+#end
+
+#binding.pry
+
+
+def prime?(n)
+  if n <= 0 || n == 1
+    return false
+  elsif n == 2
+    return true
+  end
+  
+  range = (2..(n - 1)).to_a
+  range.each do |i|
+    if (n % i) == 0
+     return false
+    end
+  end
+  true
+  #binding.pry
+
+end

853968e7ca9a89b4012ed8d9173d3b054d70601e
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-07 16:47:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,4 @@
 def prime?(number)
-#divisible by 1 and itself and nothing else
-#negative numbers are not prime
-# > than 2
-#divisible by every number from 1 to itself (1..number) will return an array of 2 ... number, then check every element within the array with condition of checking if prime
-
   if number <= 1
     return false
   elsif number == 2

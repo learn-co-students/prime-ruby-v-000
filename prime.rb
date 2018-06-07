5a5b3536a0718d499b39ceb28b47663d5dea32f9
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-07 16:30:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+#divisible by 1 and itself and nothing else
+#negative numbers are not prime
+# > than 2
+#divisible by every number from 1 to itself (1..number) will return an array of 2 ... number, then check every element within the array with condition of checking if prime
+
+  if number <= 1
+    return false
+  elsif  
+      range = (2..number)
+      range.each do |n|
+        number % n == 0
+        if number % n == 0
+          return false
+        end
+      end
+  else   
+    return true
+  end
+end

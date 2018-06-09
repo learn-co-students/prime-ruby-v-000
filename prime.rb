9b26e2aabf6d20a35f437e56b9137b044dc4381a
smithwebtek <brad@smithwebtek.com>
2018-06-09 18:58:14 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,11 @@
-def prime?(n)
-  if n.class != Fixnum
-      false
-    elsif n < 2
-      false 
-    elsif n <= 3
-      true
-    elsif n.modulo(2) == 0 || n.modulo(3) == 0
-      false
-    else
-      true  
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
 end
+
+puts prime?(105557)

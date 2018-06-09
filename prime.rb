f54fc19ed0283805c80712ced09173250f74399f
Joana Palero <joana.palero24@gmail.com>
2018-06-09 14:01:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(numbers)
+if numbers>1
+    i=2
+    while i < numbers
+      if numbers%i==0
+        return false
+      end
+      i+=1
+    end
+    return true
+  end
+  return false
+end

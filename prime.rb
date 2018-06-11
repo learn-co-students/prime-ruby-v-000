858bceb2e25d7fbdf414502246e273f12d7f7454
Will Lee <will.i.lee@outlook.com>
2018-06-11 18:51:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require 'pry'
+def prime?(integer)
+  if integer == 2
+    return true
+  elsif integer <= 1
+    return false
+  else
+    array_of_nums = (2...integer).to_a
+    !array_of_nums.any? do |number|
+      integer % number == 0
+    end
+  end
+end

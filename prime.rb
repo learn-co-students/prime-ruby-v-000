d101c52f018fcb228ca24ac4a97bd3b265b98baf
JohnMark <johnmarkludwick@gmail.com>
2018-06-13 18:15:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+
+require 'prime'
+
+def prime?(num)
+  if num < 0 || num == 0 || num == 1
+    false
+  else 
+    (2..num - 1).to_a.all? { |x| num % x != 0}
+  end
+  
+end
+
+
+
+
+# def is_prime?(num)
+#   return false if num <= 1
+#   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+#   true
+# end
+
+
+

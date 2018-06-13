f9a76e4f6cb56a6ded8065ab7ad2e49c19ddd1f4
Wendy Calderon <wendycalderon@gmail.com>
2018-06-13 03:30:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+def prime? (int)
+  #range = (2..int).to_a
+  if int <= 1 
+    return false
+  elsif
+    int == 2 || int  == 3
+      return true 
+  else 
+    (2..int/2).none? do |x|
+    int % x == 0
+    end
+  end
+end
+

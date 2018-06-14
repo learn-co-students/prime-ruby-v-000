33b266c31e705c8a08a4bbbda548f037f36dc20f
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-14 19:48:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -17,3 +17,13 @@ def prime?(number)
   true
 end
 
+#Flatiron solution different from mine 
+#def prime?(num)
+#  if num < 0 or num == 0 or num == 1
+#    return false
+#  else
+#    (2..num-1).to_a.all? do |possible_factor|
+#      num % possible_factor != 0
+#    end
+#  end
+#end

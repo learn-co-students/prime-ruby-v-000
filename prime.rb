aead6ce668d4178ebaa4d255c115fc55ae2dadfa
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 23:24:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+      if number > 1
+            array = (2..number-1).to_a
+            array.all? { |e|  number % e != 0} ? true : false
+      else
+            false
+      end
+end
+
+puts prime?(2)
+puts prime?(3)
+puts prime?(4)
+puts prime?(105557)

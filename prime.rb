df4df51b74178ef0ea227d9196e9f143f46da67a
Nate Miller <natekmiller96@gmail.com>
2018-06-23 17:27:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+
+def prime?(n)
+  if n < 2 && n > -2
+    return false
+  elsif n == 2 || n == -2
+    return true
+  else
+    if n % 2 == 0
+      return false
+    end
+    count = n/2
+    a = 1
+    while count != 0
+      a += 1
+      if n % a == 0
+        return false
+      else
+        count -= 1
+      end
+    end
+    return true
+  end
+end

1539278ab12a738e9e46e2ea8feca97f5577d811
Will Schultz <will.j.schultz@gmail.com>
2018-06-25 14:28:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(n)
+  if n<=1
+    return false
+  end
+
+   is_prime = true
+   for i in 2..n-1
+     if n % i == 0
+       is_prime = false
+     end
+   end
+   if is_prime
+     return true
+   else
+     return false
+   end
+ end

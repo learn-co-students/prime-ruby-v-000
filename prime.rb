3464dea51f198466fe422b7ab32aaf96d7adaac7
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 12:34:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer)
+  i = 2
+  if integer < 2
+    return false
+  end
+  while i <= Math.sqrt(integer)
+    return false if integer % i == 0
+    i += 1
+  end
+  return true
+end

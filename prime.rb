e76f2e1e429200007e9324614269483fba4eb5df
Priscilla H <priscillamhuynh@gmail.com>
2018-06-26 14:00:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  if (number.even? && number == 2) && number > -1
+    return true
+  elsif number.even?
+    return false
+  elsif number == 1 || number <0
+    return false
+  else
+    info = (2..number-1).any? {|num| number % num == 0}
+    return !info
+  end
+end
+
+puts prime?(3)

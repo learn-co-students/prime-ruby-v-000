f605f0dc2f4e1a387266486a0b11eb4db4435bd9
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 17:24:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+# using trial division
+def prime?(num)
+  return false if num.abs == 1 || num == 0
+
+  trial_divisors = (2..num - 1).to_a
+  attempted_divisions = trial_divisors.collect do |divisor|
+    num.abs % divisor == 0
+  end
+
+  attempted_divisions.none?
+end

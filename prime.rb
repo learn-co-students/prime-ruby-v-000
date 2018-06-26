f408026d4262512defde8f64ad45cf2cae2d8251
Justine Bricknell <justinegayton@gmail.com>
2018-06-26 18:43:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+#Prime Numbers are only divisible by themself and 1. 1 is neither prime nor composite. 2 is prime. 0 and neg are not.
+#Boolean Return (T or F)
+#string into array [].split
+
+def prime? (number)
+  if number < 2
+    return false
+  else (2..number/2).none? { |i| number % i == 0 }
+end
+end

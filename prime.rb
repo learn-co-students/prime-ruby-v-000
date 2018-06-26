9da5310d4fb60cffa7345a40301b3aa118b8dbd4
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-26 21:20:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  if number <=1
+    return false
+  end
+  if number === 2
+    return true
+  end
+  count =2
+  while count < number
+    if number % count  == 0
+      return false
+    end
+    count +=1
+  end
+  return true
+end

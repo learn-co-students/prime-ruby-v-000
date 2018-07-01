0717e0cbf647bd134a82d6346789d26cc07e7fee
Ana Bebic <anabebic.x@gmail.com>
2018-07-01 13:57:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,22 @@
 # Add  code here!
-prime?(n)
-Prime.each(100) do |prime|
-  p prime  #=> 2, 3, 5, 7, 11, ...., 97
+require 'benchmark'
+require 'bigdecimal/math'
+
+iterations = 10
+
+def prime?(integer)
+  return false if integer <= 1
+  Math.sqrt(integer).to_i.downto(2).each do |divisor|
+    return false if integer % divisor == 0
+  end
+  true
+end
+
+def prime_wo_sqrt?(integer)
+  return false if integer <= 1
+  worstcase = integer - 1
+  worstcase.downto(2).each do |divisor|
+    return false if integer % divisor == 0
+  end
+  true
 end

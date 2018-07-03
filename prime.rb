a5e3a3bd06c8109533c0cc9b3a7268438c414882
Jordan Harris <jharris@wework.com>
2018-07-03 20:49:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'pry'
+
+def prime?(n)
+    return false if n <= 1
+    return true  if n == 2 || n == 3
+    return false if n % 2 == 0 || n % 3 == 0
+    i = 5
+    w = 2
+    while i * i <= n
+      return false if n % i == 0
+      i += w
+      w = 6 - w
+    end
+    true
+  end

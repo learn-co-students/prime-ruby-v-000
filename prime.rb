77f6dce96d255440b7775bc72df0b85602374c0a
Eric Shiels <ericshiels2@gmail.com>
2018-07-04 17:36:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,4 +1,14 @@
-require 'prime'
 def prime?(integer)
-Prime.prime?(integer)
+range = (2..integer - 1).to_a
+if integer < 2
+  false
+else 
+  range.each do |number|
+    if (integer % number) == 0 
+      return false 
+    else 
+      return true 
+    end
+  end
+end
 end

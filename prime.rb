7873a249d6390a29dc8084d8af5811be068b5564
Anna Dybas <a.m.dybas@gmail.com>
2018-07-17 06:18:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  #prime nums are only divisable by themselves and 1 like 2,3,5,7,11,13
+  if number < 0 || number == 0 || number == 1
+    false
+  else
+    (2..number-1).to_a.all? {|i| number % i != 0}
+  end
+end
+
+
+# (2..n).to_a setting range 2 to n

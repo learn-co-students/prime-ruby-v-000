a9ccb34ffbd9cbcdbc5eed5fe813cbbf91c3ae1e
Dana Palmer <roiai4@hotmail.com>
2018-07-18 02:59:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
-#okay but why?
-
-def prime?(integer)
-  integer 
+def prime?(int)
+  if int == 0 || int == 1 || int < 0  
+    return false
+else
+  (2..int-1).to_a.all? do |num|
+      int % num != 0
+    end
+  end
 end
-

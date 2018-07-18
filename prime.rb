39f0af8ab89987b42f2a87ee8abb3e17ddcdcba6
Yana Ustsinovich <yustinovich@icloud.com>
2018-07-18 16:58:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+require "pry"
+def prime?(num)
+ num = num.abs
+  if num == 0 || num == 1
+   return false
+  elsif num == 2 || num == 3
+   return true
+  end
+ prevnum = (2..num-1).to_a
+ prevnum.all? { |del| num % del != 0 }
+end

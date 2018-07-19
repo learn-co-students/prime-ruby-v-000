5b0caa5511c7a872bc647be233dfdce9786470ea
Zain <zain@halai.us>
2018-07-19 14:53:31 -0500

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -1,13 +1,5 @@
 def prime?(n)
-	( n < 2 ) ? false : is_prime?(n)
-end
-
-def is_prime?(n)
-	i = 2
-	while i < n
-		return false if n % i == 0
-		i += 1
-	end
-
-	true
+  return false if n < 2
+  
+  (2...n).all? { |i| n % i != 0 }
 end

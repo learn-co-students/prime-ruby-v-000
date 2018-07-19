bc2dac770de0847254c35c1ee8191ea278a20048
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-19 20:22:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def check_prime_number(num)
+ num = num.to_i
+ n = 1
+ factors = []
+
+ while (num >= n ) do
+    if  (num % n == 0 )
+        factors << n unless factors.include?(n)
+    end
+    n += 1
+ end
+
+ if ( factors.size > 2)
+    return false
+ elsif num > 0
+    return true
+ end
+end
+end
+
+check_prime_number(num)

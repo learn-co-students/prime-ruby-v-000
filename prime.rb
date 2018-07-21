b510f9ad7858afa86e90be9fc86ff14a6a9fe400
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-21 18:28:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+
+def prime?(int)
+  if int <=1
+    return false
+  end
+if (2..int - 1).to_a.map do |i| int % i == 0
+    end.include?(true)
+    return false
+  else true
+  end
+end

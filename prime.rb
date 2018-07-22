b699d12610f61fd07734cff7444f6291fdaceb99
tc.thomascho@gmail.com <tc.thomascho@gmail.com>
2018-07-22 23:12:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require 'pry'
+def prime?(number)
+  if number > 1
+    factors = (1..number).select do |factor|
+      number % factor == 0
+    end
+  else
+    return false
+  end
+
+  factors.all? {|factor| factor == 1 || factor == number}
+
+end

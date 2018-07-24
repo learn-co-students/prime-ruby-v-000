0d3daf6a68841488c928d0f2996ba537c1455b7f
Rafael Lopez <lpz.raf@gmail.com>
2018-07-24 21:59:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,18 @@
-# Add  code here!
-def prime_num(n)
-if prime_num.prime?(n)
-  rerturn true 
-else
-  false
+
+# def prime_num(7)
+# if prime_num.prime?(7)
+#   rerturn true 
+# else
+#   false
+# end
+# end
+
+
+def prime?(number)
+(2..number -1).each do |num|
+  if number%num == 0 
+    return false
+  end
 end
+return true
 end

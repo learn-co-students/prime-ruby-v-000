435c983cb7904c35c846bc498305ff7486e09c83
Rafael Lopez <lpz.raf@gmail.com>
2018-07-25 00:22:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,12 @@
 def prime?(number)
-  if number == 2 || number == 1 || number % 3
-    return true
+  if number >= 2 
+    (2..number - 1).all? do |x| 
+      number % x != 0 
+    end
+  else
+    return false
+  end
+end
 
 
 

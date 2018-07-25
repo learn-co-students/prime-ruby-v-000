5d47842f0b96bfddcb1fbfffe1e6b3197c5fe3d5
Rafael Lopez <lpz.raf@gmail.com>
2018-07-25 00:03:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,18 +1,6 @@
-
-# def prime_num(7)
-# if prime_num.prime?(7)
-#   rerturn true 
-# else
-#   false
-# end
-# end
-
-
 def prime?(number)
-(2..number-1).each do |num|
-  if number%num == 0 
-    return false
-  end
-end
-return true
-end
+  if number == 2 || number == 1 || number % 3
+    return true
+
+
+

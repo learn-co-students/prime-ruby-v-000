8643a3855fdf75039ef39defe3c391d037fd2ee7
Eli Cho <eli.cho@gmail.com>
2018-07-28 22:42:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -17,17 +17,3 @@ def prime?(num)
     return true
   end
 end
-
-# function is_prime(n)
-#     if n ≤ 1
-#         return false
-#     else if n ≤ 3
-#         return true
-#     else if n mod 2 = 0 or n mod 3 = 0
-#         return false
-#     let i ← 5
-#     while i * i ≤ n
-#         if n mod i = 0 or n mod (i + 2) = 0
-#             return false
-#         i ← i + 6
-#     return true

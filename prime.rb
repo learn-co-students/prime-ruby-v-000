ecca1cde100e7631ea8b1ac7b7feec93e7d94fcd
Jae Yong Kim <kim900180@gmail.com>
2018-07-29 20:00:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+   return false if number < 2
+   dividers = (2..number - 1).to_a
+   dividers.each do |divider|
+    if number % divider == 0
+      return false
+    end
+  end
+  return true
+end

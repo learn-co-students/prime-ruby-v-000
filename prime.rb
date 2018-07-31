7766230fe1219a3601397d65a40ec1f3ef8d46be
jamieberrier <berrier.jamie@gmail.com>
2018-07-31 17:36:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+require 'pry'
+
+#takes in an integer argument and returns a boolean of whether or not that integer is a prime number
+def prime?(number)
+  if number >= 2
+     (2..number - 1).all? do |x|
+       number % x != 0
+     end
+   else
+     false
+   end
+end

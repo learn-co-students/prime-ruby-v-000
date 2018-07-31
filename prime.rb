095c08b2692df2243da3c3516a5487033f31b1bc
Alison Shin <alisonyshin@gmail.com>
2018-07-31 05:36:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(possibly_prime_number)
+  if possibly_prime_number > 1
+    numbers_range = (2..possibly_prime_number-1).to_a.all? {|number| possibly_prime_number % number != 0}
+  else
+    return false
+  end
+end

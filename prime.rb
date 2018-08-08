d08d588a0158f8bb9fc5ce357fa98ef8c2c1b253
Luis Herrera <herrera.lrh@gmail.com>
2018-08-08 23:07:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+require 'pry'
+
+def prime?(int)
+  if int < 0 or int == 0 or int == 1
+    return false
+  else
+    (2..int-1).to_a.all? do |factor|
+      int % factor != 0
+    end
+  end
+end

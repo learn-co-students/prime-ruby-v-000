7e15a869857620371942d8bf6c89ce22df5f8fac
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 03:03:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  lesser_integers = (1..integer).to_a
+  divisible_by = []
+  lesser_integers.map {|lesser_int| divisible_by << lesser_int if integer % lesser_int == 0}
+  if divisible_by.count > 2
+    return false
+  elsif integer < 2 
+    return false
+  else
+    return true
+  end
+end

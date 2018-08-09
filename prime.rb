5d9429f5d72bf5778dec0747355e73e45bb9453f
Joseph Senders <jasenders@gmail.com>
2018-08-09 20:46:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+    return false if (number.even? && number > 2) || number <= 1
+    half_num = number / 2
+      n = 3
+      while n < half_num
+        return false if number % n == 0
+        n += 2
+      end
+
+      true
+    end

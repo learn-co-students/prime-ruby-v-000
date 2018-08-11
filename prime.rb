62317a43148899a6257957e54c3517b6593a1440
Kenzie Morlock <k.morlock86@gmail.com>
2018-08-11 02:36:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime? x
+  if x < 0 or x == 0 or x == 1
+    return false
+  else
+    (2..x-1).to_a.all? do |possible_factor|
+      x % possible_factor != 0
+    end
+  end
+end

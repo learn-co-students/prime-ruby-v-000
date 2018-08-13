e37f1675623149cc02b551e7b3bad304ef764115
Brad McCollum <btmcco8@gmail.com>
2018-08-13 22:47:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require'benchmark'
+
+puts Benchmark.measure{
+
+def prime?(n)
+  if n <= 1
+    return false
+  else
+    (2..Math.sqrt(n)).none? { |n| (n % n).zero?}
+    Math.sqrt(n).floor.downto(2).each {|i| return false if n % i == 0}
+    true
+  end
+end
+}

dc68178a04176f19b185aefef2c5cc1d1ea22c3e
J Fern <jfern20@gmail.com>
2018-08-15 03:09:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+ if integer < 2 
+   return false 
+end    
+ (2..(integer/2.to_f).ceil).each do |x|
+   if integer % x == 0 
+     return false 
+     
+   end
+ end 
+ true 
+   
+
+end 

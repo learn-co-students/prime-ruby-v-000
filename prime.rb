5f9ea42111c56d58c460e58ff46517a4460f4ad8
Katrina Pritchard <pritckat@gmail.com>
2018-08-15 01:30:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,16 +1,12 @@
 # Add  code here!
 
 def prime?(maybe_prime)
-  numbers = [*2..1762]
-  numbers.each do |n|
-    if maybe_prime == -1 || maybe_prime == 0 || maybe_prime == 1
-      return false
-    elsif maybe_prime == 2 #|| maybe_prime == 3
-      return true
-    elsif maybe_prime % n == 0 || maybe_prime == 1763 || maybe_prime == 101013
+    if maybe_prime <= -1 || maybe_prime == 0 || maybe_prime == 1
       return false
     else
-      return true
+      numbers = (2..maybe_prime-1).to_a
+      numbers.all? do |num|
+        maybe_prime % num != 0
+      end
     end
-  end
 end

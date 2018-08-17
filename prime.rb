748dd95db7bf41d58203ec28c2b56dae7adb31f3
Daniel Schoch <d.schoch90@gmail.com>
2018-08-17 20:42:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(int)
+  poss_multiples = Array(2..int-1)
+  if int > 1
+    poss_multiples.none? do |mult|
+      int % mult == 0
+    end
+  else
+    false
+  end
+end

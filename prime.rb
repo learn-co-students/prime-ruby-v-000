51917e9323932fe0fc0136c6e13bc1976743a70f
MarSchlaefer <mschlaefer00@gmail.com>
2018-08-19 14:56:49 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
+def prime?(num)
+  i = 2
+
+  if num <= 1
+    return false
+  end
+
+  if num == 2
+    return true
+  end
+
+  while i < num do
+    if num % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end
 # Add  code here!

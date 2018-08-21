49a5cab810bc46e4051b4b5a8ff0d1c286558ece
taisaoko <ttd3calvin@gmail.com>
2018-08-21 21:25:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require "benchmark"
+def prime?(number)
+ if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
+    return false
+  end
+end 
+
+benchmark.measure {n*1000000}
+  

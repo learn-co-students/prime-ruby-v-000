d8574cfa1923f3afadc50a044a5093beca016922
Jonathan Chan <johnkchan252@gmail.com>
2018-08-21 03:38:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(integer)
+  i = 2
+  if integer < 0
+    integer *= -1
+  end
+  if integer == 1 || integer == 0
+    return false
+  end
+  while i < integer.abs
+    puts i
+    if integer % i == 0
+      return false
+    end
+    i += 1
+  end
+
+  return true
+end

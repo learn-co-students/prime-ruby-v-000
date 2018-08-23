ca685734ae6a3926edb604320f0bc14fca5bf8aa
Tristan Brown <tgbrown@stanford.edu>
2018-08-23 20:26:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime? (integer)
   if integer < 2
     return false
-  end 
-  (2..integer - 1).each do |x|
+  end
+  (2..integer - 1).to_a.each do |x|
    if (integer % x) == 0
     return false
   end

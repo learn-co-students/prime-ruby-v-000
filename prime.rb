d1076fc3fd707464a276845a6cd9c10a92aa7640
Marc Mills <mdmills1990@gmail.com>
2018-08-23 00:50:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+if num < 2
+  return false
+end
+  array = (2..num-1).to_a
+  array.each do |number|
+  if num % number == 0
+    return false
+  end
+end
+  return true
+end

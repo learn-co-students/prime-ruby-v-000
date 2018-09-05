31a1e881a2ff7ec33982bf1875d2bc9e35365122
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-05 02:28:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(number)
-if number >=2
-(2..number - 1).all? do |x|
+if number == 0 || number < 0 || number == 1
+  return false
+else
+(2..number - 1).to_a.all? do |x|
 number % x != 0
 end
-else
-false
 end
 end

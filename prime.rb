60e5a59c52b0e8281c9e0018a17b535fc4eac3b7
Heather Miller <heather.miller114@gmail.com>
2018-09-05 00:18:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  elsif number == 2 or number == 3
+    true
+  elsif (2...number-1).any?{|x| number % x == 0}
+    false
+  else
+    true
+  end
+end
+

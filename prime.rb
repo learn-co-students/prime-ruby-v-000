cc6e5fbf33c904d19e10a5aa524a6bd8b87e25fd
Ashley Bland <ashleyjbland@gmail.com>
2018-09-05 00:33:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+#basic idea here is to compare num with numbers that are smaller than itself (besides 1).
+#If it is divisible by any of them, then it's not prime
+
+
+def prime?(n)
+  if n <= 1
+    false
+  elsif n == 2
+    true
+  elsif (2..n/2).none?{|i| n % i == 0}
+    true
+  else
+    false
+  end
+end

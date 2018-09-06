59b35c750882d1b56a351cc34c23bc485bf5461e
alice T <alicewtien@gmail.com>
2018-09-06 18:15:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  elsif number == 2
+    true
+  else (2..number-1).none? { |x| number % x == 0 }
+  end
+end
+
+#else (2 to number-1) where the numbers are represented by |x|
+#none of the numbers are divisible by x then it's a prime number

940fa9b78c2a410d427ee6d46245cd1dd2554ecb
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-06 15:29:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 def prime?(integer)
   if integer < 0 || integer == 0 || integer == 1 
     return false
- 
+  else
+    (2..integer-1).to_a.all? do |possible|
+      integer % possible != 0 
+    end
+  end
 end
 
 

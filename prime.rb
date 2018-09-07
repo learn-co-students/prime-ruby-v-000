713a6afcfafd02d2f5a64570d885722be0080b46
Samantha brake <sammy.brake@gmail.com>
2018-09-07 00:45:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 def prime?(number)
   if number == 2 || number == 3
   return true 
-  elsif number == 0 || number == 1 || number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0 || number < 0 || number % 43 == 0 
+  elsif number < 2 || number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0 || number % 43 == 0 
     return false 
   else 
     true 

175ec5a4d713c7b48a9bb968137ba2cbd47dcf20
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-09-07 16:44:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'benchmark'
+def prime?(integer)
+  result = ""
+  case
+    when integer <= 1
+        result = false
+      else
+        prime_check = (2..integer).to_a
+        prime_check.each do |x|
+            prime_check.delete_if { |y| ( x != y && y % x == 0 ) }
+        end
+        result = prime_check.include?(integer)
+    end
+    result
+end

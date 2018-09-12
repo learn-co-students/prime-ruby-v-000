4af49dacd96d058e707014a97f0434f85c740166
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-12 22:54:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  range = (2..num - 1).to_a
+    if num == 2
+      return true
+    elsif num < 2
+      return false
+    elsif range.any? {|x|  num % x == 0}
+      return false
+    else
+      return true
+    end
+end

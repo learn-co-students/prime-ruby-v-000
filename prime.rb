2f39f8984fdf45cec8cb80206f250245f73606b2
Amil Masic <amil.masic@gmail.com>
2018-09-12 18:17:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+require "pry"
+def prime?(numbers)
+  if numbers < 2
+    return false
+  end
+
+  result = (2...numbers).any? do |n|
+    numbers % n == 0
+  end
+
+  return !result
+end
+
+  # numbers = (-120..120).to_a
+  # numbers = (2..numbers).to_a
+  # numbers.any? do |prime|
+  #
+  #   if prime == 2 || prime == 3 || prime == 5 || prime == 7
+  #       true
+  #     else prime % 2 != 0 || prime % 3 != 0 || prime % 5 != 0 || prime % 7 != 0
+  #       false
+  #     end
+  # end
+# end

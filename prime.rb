6a4206307042c09e119cf2377a3540ff752a0be4
Tim Knoll <timothy.knoll@gmail.com>
2018-09-13 23:14:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  start_num = 2
+  if number > 1
+    range = (start_num...number - 1).to_a
+
+    range.none? do |num_test|
+      number % num_test == 0
+    end
+  else
+    false
+  end
+end

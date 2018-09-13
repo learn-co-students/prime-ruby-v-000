41e56f232d144b852980584b08776afd3928a4ae
Tim Knoll <timothy.knoll@gmail.com>
2018-09-13 23:16:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -10,3 +10,13 @@ def prime?(number)
     false
   end
 end
+
+# def prime?(num)
+#   if num < 0 or num == 0 or num == 1
+#     return false
+#   else
+#     (2..num-1).to_a.all? do |possible_factor|
+#       num % possible_factor != 0
+#     end
+#   end
+# end

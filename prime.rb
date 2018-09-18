7e9069eccd263350da809eb4f2cd9405c34ebcce
Cheryl K <cherylkelson@icloud.com>
2018-09-18 19:10:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,15 +1,12 @@
 def prime?(number)
-  if number == 0 or number == 1 
+  if number == 0 or number == 1 or number < 0
     return false 
-  end 
-  i = 2  
-  limit = number / i 
-  while i < limit 
-    if number % i == 0 
-      return false 
+  else 
+    (2..number-1).to_a.all? do |checking_factor| 
+      number % checking_factor != 0
     end 
-  i += 1 
-  limit = number / i 
-  end 
-  return true 
+  end  
 end
+ 
+
+

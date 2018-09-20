d6028f93a5c525c0906cee4355b123e83b9ea577
Rachel Feldman <rachel.a.f.@Rachels-MacBook-Pro.local>
2018-09-20 19:52:31 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    elsif number === 2 || number === 3
+        return true
+    end
+
+    i = 2
+    while i <= number / 2
+        return false if number % i == 0
+        i += 1
+    end
+
+    return true
+
+end

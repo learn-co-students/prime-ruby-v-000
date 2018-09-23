5f836474bce360e9d7b31085eb2398dd50611e80
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-23 01:50:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  prime = Array(2..integer-1)
+  if integer == 2 || integer == 3
+    true 
+  elsif integer <= 1 
+    false 
+  elsif prime.any? {|i| integer % i == 0}
+    false 
+  else 
+    true 
+  end 
+end 

4277d5331b78fb3cb65a7fd1a30851e58472e749
Paul Fell <swellpf@gmail.com>
2018-09-26 17:25:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,17 @@
-# Add  code here!
+require 'pry' 
 def prime?(number)
-  divisors = [2,3,5,7,9,11]
-  divisors.each do |divisor|
-    if % number/divisor == 0 
+  binding.pry
+    test_array = (1..number).to_a
+    test_array.select odd
+    is_prime == FALSE
+  if number <= 1 
+    return is_prime
   end
+    test_array.each do |testnum|
+      if number < testnum 
+        is_prime = FALSE
+      elsif number % testnum = 0
+        
+  
+    
 end

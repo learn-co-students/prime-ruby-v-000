7559673a66a7ab9ba1a937a293ab1afd2d789ac7
Paul Fell <swellpf@gmail.com>
2018-09-26 18:46:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,24 +1,9 @@
-require 'pry' 
 def prime?(number)
-# binding.pry
-    test_array = (1..number-1).to_a
-    test_array = test_array.select do |number|
-  number.odd?
-    end
-#  binding.pry
-    is_prime = TRUE    # assume number is prime until proven otherwise
-  if number <= 1 
-     is_prime = FALSE
-     return is_prime
-   end
-   if number == 2 || number == 3
-     return is_prime
+  if number <= 1
+    false
+  else
+    (2..number-1).to_a.all? do |possible_factor|
+      number % possible_factor != 0
+      end
   end
- #  binding.pry
-    test_array.each do |testnum|
-      if number % testnum = 0
-          is_prime = FALSE   
-      end
-      end
-    is_prime
 end

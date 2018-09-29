36cb76c54e5ad8886ce29b801b69c692556f3d7e
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-29 01:37:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  array = (1..number.abs).to_a 
+  counter = 1 
+  divisible_by = []
+  while counter <= number.abs 
+    divisible_by << counter if number.abs % counter == 0 
+    counter += 1 
+  end 
+  if divisible_by.length == 2 
+    true 
+  else 
+    false 
+  end 
+  
+end 
+

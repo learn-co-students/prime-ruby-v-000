49bc136d80fa72191d31859012f97533a80bf742
Brian Birdwell <bbirdwell@gmail.com>
2018-10-01 06:57:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  array = (2..(num-1)).to_a
+  if array.any? { |n| num % n == 0 } 
+    return false
+  elsif num * (-1) >= 0 || num == 1
+    return false
+  elsif array.none? { |n| num % n == 0 }
+    return true
+  
+  end
+
+end

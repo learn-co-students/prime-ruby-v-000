26f2882f3145af843a790b01d596959b55fd8bda
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-04 22:37:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,9 @@
-
-
-def prime? (n)
-    if n <= 1
+def prime?(number)
+    if number <= 1
         false
-    elsif n == 2
+    elsif number == 2
         true
     else 
-        (2..n/2).none? { |i| n % i == 0}
+        (2..number/2).none? { |i| number % i == 0}
     end
 end
-
-#A prime number is any number that has no positive divisors other than itself and 1.

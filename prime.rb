248d3788aa9ecb40ff7c65aeb210a3559b2ab121
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-04 22:33:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,14 @@
 def prime?(numbers)
-  divider = [1..100]
-  numbers.any? do |number|
-    number % divider == 0
+  numbers.each do |numbers|
+    if (2..numbers/2).none?{|i| numbers % i == 0}
+      true
+    elsif numbers < 1
+     false
+    else
+      false
+    end
   end
 end
+
+
+#A prime number is any number that has no positive divisors other than itself and 1.

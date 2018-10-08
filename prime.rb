ecb69da3724120ec52646de7fa42a407724f7097
Tania Whiting <tanwhiting@aol.com>
2018-10-08 23:00:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -5,15 +5,16 @@ def prime?(number)
   if number < 2  
     return false
     else
-    range_numbers.each do |element| 
+      range_numbers.each do |element| 
       remainders.push(number%element) 
-     end
+    end
   end
   zero_remainders = remainders.select do |num|
       num == 0 
   end
   if zero_remainders.size == 2
       return true  
-    else return false 
-end
+    else 
+      return false 
+  end
 end

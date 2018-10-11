7fee360f56c31bf7f6646d4b4ede737f7d1b15b2
Sophie Gu <sophieqgu@gmail.com>
2018-10-11 09:28:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 def prime?(num)
-  if num < 2 
-    false 
-  elsif num == 2 
-    true 
-  else 
+  if num >= 2  
     (2..(num - 1)).to_a.none? {|divider| num % divider == 0}
+  else 
+    false 
   end 
 end 
   

f9c98d3e13fc1532ea5a4c5b3a0e1026eeb343e6
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-12 14:23:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 def prime?(number)
-  if number == 2
+  if number < 2
+    return false
+end
   array_of_numbers = (2...number).to_a
   array_of_numbers.any? do |n|
     if number % n == 0
       return false
   end
 end
+true
 end

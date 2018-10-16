ba925b96cdcb7fec4d9576a98c4558e068c5d87f
Sara Kantor <sara.kantor.nv@gmail.com>
2018-10-16 18:28:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+  final = false
+  if num <= 1 || (num.even? == true && num != 2)
+    final = false
+  elsif num == 2 || num ==3
+    final = true 
+  else 
+    collection = []
+    l = (2..(num-1)).to_a
+    l.each do |div|
+      if num%div==0
+        collection << false
+      else 
+        collection << true 
+      end
+    end 
+    final = !collection.any?{|item| item == false}
+  end
+  final
+end 

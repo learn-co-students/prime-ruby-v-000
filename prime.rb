8c46500aa16bd8da55d1f3185c2263d0567ea6ca
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-17 00:02:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,13 @@
-def prime?(number_given)
-  i = number_given - 1
-  while i > 1
-    (number_given / i).odd? || (nummber_given / i).even?
-  end
+require 'pry'
+def prime?(n)
+      if n <= 1
+         return false
+      elsif n <= 3
+         return true
+      elsif n % 2 == 0 || n % 3 == 0
+         return false
+      else
+        (4..n/5).to_a.all? {|f| n % f != 0}
+      end
+
 end

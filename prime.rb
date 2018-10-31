949322d990f4f8f213bb9d548920b5b56c1b1749
Alexei Lourie <alexeilourie@gmail.com>
2018-10-31 23:38:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+  if int <= 0
+    return false
+  elsif int == 1
+    return false
+  end
+  i = 2
+  check = 0
+  while i < int
+    if int % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end

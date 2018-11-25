0fce56dce58953464e0493ebdb2d48baafa780ca
James Nghiem <nghiem.j@hotmail.com>
2018-11-25 03:53:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+ def prime?(n)
+  if (n) == 2 
+    return true
+  elsif (n) <= 1
+    return false
+  else (2..n/2).none? do |num|
+    (n) % num == 0
+  end
+ end
+ end
+ 
+ 
+ 
+ 
+  #require 'prime'
+  #integer.each do |i|
+  #
+  

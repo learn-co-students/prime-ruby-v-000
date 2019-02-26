f4bbdd4868618b5bacff667df3ac2a379cf76111
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-26 00:12:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 # Add  code here!
-def prime?(int)
-  if (int % 1 == 0) && (int % int == 0)
-    true
-  elsif int == int * -1
-    false
-  end
+#require 'pry'
+def prime?(num)
+  num >= 2 && (2...num).none?{|int| num % int == 0}
+  #binding.pry
 end
+
+#none?
+#Imagine the opposite of #all?, a method #none?, where we are interested in none of the elements in a collection producing a true
+#expression within the block passed to #none?.
+#[1,3].none?{|i| i.even?} #=> true

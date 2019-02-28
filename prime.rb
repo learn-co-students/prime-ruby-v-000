4439723a96b05166ea5504d3db173d12d2ab45e2
Andranique Boone <andranique.boone@gmail.com>
2019-02-28 02:45:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer)
+ if integer <= 1 
+   return false
+ end
+ for n in 2..(integer - 1)
+   if integer%n == 0
+     return false
+   end
+ end
+ return true
+end

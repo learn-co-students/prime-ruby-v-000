085bfbab94a012bd5a6a27a95b4ea0430c7b60e9
Charles Okwechime <okwechimeamechi@gmail.com>
2019-04-01 20:08:40 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(number)
   if number >= 2
-    (2..number).to_a.all? {|nums| number % nums != 0}
+    (2...number).to_a.all? {|nums| number % nums != 0}
   else
     false
   end

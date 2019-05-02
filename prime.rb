1ff49b80bd0da8dabc5f32f95fb5e33c8a6ba007
Monica Tamaru Jones <mntamaru@gmail.com>
2019-05-02 23:51:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -19,3 +19,18 @@ def prime? (int)
     return true
   end
 end
+
+
+# The efficient solution for this problem
+
+# def prime?(num)
+#   if num < 0 or num == 0 or num == 1
+#     return false
+#   else
+#     (2..num-1).to_a.all? do |possible_factor|
+#       num % possible_factor != 0
+#     end
+#   end
+# end
+
+# puts prime?(105557)

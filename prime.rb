f95c229cc08b2831f165a58c1c0eebe13bf6c27c
Tiffany <ong.tifffany1503@gmail.com>
2019-05-20 21:02:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end

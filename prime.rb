5d5a4c88f2966e3d8aab655e4ee136c44e2a251b
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 22:43:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 require'pry'
 
 def prime?(number)
-  range= (2...number).to_a
-  answer = range.map {|d| (number % d == 0)}
-  if answer.include?(true)||answer==[]
-    return false
+  range= (2...number.abs).to_a
+  if number.abs <= 1
+    answer = true
   else
-    return true
+    answer = range.find {|d| (number.abs % d == 0)}
   end
+  return (answer.nil?)
 end

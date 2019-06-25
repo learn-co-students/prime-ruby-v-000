a95097b88ab145c072fa2355125c432a30d0d288
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 17:35:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
-def prime?(number)
-   start = 2 
-   if number > 1 
-     range = (start..number-1).to_a 
-     range.none? do |num_to_test| 
-       number % num_to_test == 0 
-     end 
-   else 
-     return false 
-  end 
+def prime?(n)
+  i = 2 
+  if n > 1 
+  array = (i..n-1).to_a 
+  array.none? do |num_to_test| 
+  n % num_to_test == 0
+    end
+  else
+  return false 
+  end
 end

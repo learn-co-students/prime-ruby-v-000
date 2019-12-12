accb9737f98ce2bc29a5947505d388a8cd403d14
github username <github email address>
2019-12-12 07:13:02 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,16 @@
 def prime?(n)
   return false if n < 2
   return true if n == 3 || n == 2
-    if (2...n-1).any?{|i| n % i == 0}
-      false
-    else
-      true
-    end
+  !(2...Math.sqrt(n)+1).any?{ |i| n % i == 0 }
 end
+
+
+# def prime?(n)
+#   return false if n < 2
+#   return true if n == 3 || n == 2
+#     if (2...n-1).any?{|i| n % i == 0}
+#       false
+#     else
+#       true
+#     end
+# end

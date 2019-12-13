1d9ed1c7494cc2dd6cbd33730f6274e0eaa57036
Darius Stewart <darius.stewart31@gmail.com>
2019-12-13 07:27:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  range = 2...integer
+  array_range = range.to_a
+
+if integer <= 1
+  false
+else
+  array_range.all? do |int|
+    integer % int != 0
+  end
+end
+end 

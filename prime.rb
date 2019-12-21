58563e18874364c2597181a852547206061d94f2
Kat Tochowicz <ktochow1@binghamton.edu>
2019-12-21 11:16:02 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+
+def prime?(number)
+  if number < 0 || number == 0 || number == 1
+    return false
+  else
+    (2..number-1).to_a.all? do |factor|
+      number % factor != 0
+    end
+  end
+end

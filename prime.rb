3191b768a17c856eb10ca8ec0dc658f87d583a63
github username <github email address>
2019-12-23 10:58:51 -0500

    Cut down to use none? instead of all, and fix rounding issues.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,18 @@
+# def prime?(n)
+#   return false if n < 2
+#   return true if n == 3 || n == 2
+#   (2...Math.sqrt(n)+1).none? { |i| n % i == 0 }
+# end
 def prime?(n)
   return false if n < 2
-  return true if n == 3 || n == 2
-  (2...Math.sqrt(n)+1).none? { |i| n % i == 0 }
+  (2..Math.sqrt(n).floor).none? {|i| n % i == 0 }
 end
+# floor is a bit faster that round or ceil
+# Benchmark.measure {
+#   100_000.times do
+#     10.1.round
+#   end
+# }.real
 
 # def prime?(n)
 #   return false if n < 2

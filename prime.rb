9979d6f6d013098cf1a82ce533acc870d4e1ae67
idith almog <almog.idith@gmail.com>
2020-04-30 21:25:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  i=2
+  while i <= number/2
+    if number % i == 0
+      return false
+    else
+      i += 1
+    end
+  end
+  true
+end

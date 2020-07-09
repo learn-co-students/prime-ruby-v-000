431eda3447efa44777818e3f5237647692d76186
hkato4188 <hkato@sas.upenn.edu>
2020-07-09 14:51:09 -0400

    refactored prime? method

--- prime.rb
+++ prime.rb
@@ -1,9 +1,7 @@
-def prime?(number)
-  if number < 0 or number == 0 or number == 1
+def prime?(numb)
+  if numb <= 1
     return false
   else
-    (2..number-1).to_a.all? do |possible_factor|
-      number % possible_factor != 0
-    end
+    (2..numb-1).to_a.all? {|possible_factor| numb % possible_factor != 0}
   end
 end

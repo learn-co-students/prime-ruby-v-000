1d6a2091e2f74ac420d82cdd25d552b2274ec015
Shasha Yan <shashayan@gmail.com>
2021-05-14 13:15:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(num)# Add  code here!
+  if num < 0 || num == 0 || num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |i|
+      num % i != 0
+    end
+  end
+end

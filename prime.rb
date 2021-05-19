140d6b67d0e4d00881538d44f93cca0400acc82a
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-19 17:35:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  
+  if (number <= 1)  
+      return false;
+  else
+   # Check from 2 to n-1 
+   (2..number-1).to_a.all? do |possible_factor|
+     number % possible_factor != 0
+      
+   end
+  end
+  
+end

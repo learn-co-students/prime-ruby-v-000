aebe0f528bb2d01a2c9b593db4f00b9972011a49
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 04:33:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  prime_number = true
+  if number < 2
+    prime_number = false
+  else
+    for n in 2..number-1
+      if number % n == 0
+        prime_number = false
+      end
+    end
+  end
+  prime_number
+end
